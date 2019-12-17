223ec33fd3f9f7c88f06a3a6c866e743072611e8
Blast <cjblanton2@gmail.com>
2019-12-17 01:12:50 -0500

    deathswap

--- src/main/java/me/pugabyte/bncore/features/minigames/models/mechanics/MechanicType.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/models/mechanics/MechanicType.java
@@ -1,15 +1,10 @@
 package me.pugabyte.bncore.features.minigames.models.mechanics;
 
-import me.pugabyte.bncore.features.minigames.mechanics.CaptureTheFlag;
-import me.pugabyte.bncore.features.minigames.mechanics.FourTeamDeathmatch;
-import me.pugabyte.bncore.features.minigames.mechanics.FreeForAll;
-import me.pugabyte.bncore.features.minigames.mechanics.OneFlagCaptureTheFlag;
-import me.pugabyte.bncore.features.minigames.mechanics.OneInTheQuiver;
-import me.pugabyte.bncore.features.minigames.mechanics.Paintball;
-import me.pugabyte.bncore.features.minigames.mechanics.TeamDeathmatch;
+import me.pugabyte.bncore.features.minigames.mechanics.*;
 
 public enum MechanicType {
 	CAPTURE_THE_FLAG(new CaptureTheFlag()),
+	DEATH_SWAP(new DeathSwap()),
 	FOUR_TEAM_DEATHMATCH(new FourTeamDeathmatch()),
 	FREE_FOR_ALL(new FreeForAll()),
 	ONE_FLAG_CAPTURE_THE_FLAG(new OneFlagCaptureTheFlag()),
