325d8d5642aba7fe2cf230c75810ddf60f3d17c1
BuildTools <WakkaFlocka239@gmail.com>
2020-01-03 00:40:55 -0600

    Thimble shit & ColorType Enum

---
+++ src/main/java/me/pugabyte/bncore/utils/ColorType.java
@@ -0,0 +1,160 @@
+package me.pugabyte.bncore.utils;
+
+import lombok.Getter;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.DyeColor;
+
+import java.util.Arrays;
+
+@Getter
+public enum ColorType {
+
+	WHITE(
+			"white",
+			Color.WHITE,
+			ChatColor.WHITE,
+			DyeColor.WHITE,
+			0
+	),
+	LIGHT_GRAY(
+			"light gray",
+			Color.SILVER,
+			ChatColor.GRAY,
+			DyeColor.SILVER,
+			8
+	),
+	GRAY(
+			"gray",
+			Color.GRAY,
+			ChatColor.DARK_GRAY,
+			DyeColor.GRAY,
+			7
+	),
+	BLACK(
+			"black",
+			Color.BLACK,
+			ChatColor.BLACK,
+			DyeColor.BLACK,
+			15
+	),
+	BROWN(
+			"brown",
+			Color.fromRGB(139, 69, 42),
+			null,
+			DyeColor.BROWN,
+			12
+	),
+	RED(
+			"red",
+			Color.RED,
+			ChatColor.DARK_RED,
+			DyeColor.RED,
+			14
+	),
+	ORANGE(
+			"orange",
+			Color.ORANGE,
+			ChatColor.GOLD,
+			DyeColor.ORANGE,
+			1
+	),
+	YELLOW(
+			"yellow",
+			Color.YELLOW,
+			ChatColor.YELLOW,
+			DyeColor.YELLOW,
+			4
+	),
+	LIME(
+			"lime",
+			Color.LIME,
+			ChatColor.GREEN,
+			DyeColor.LIME,
+			5
+	),
+	GREEN(
+			"green",
+			Color.GREEN,
+			ChatColor.DARK_GREEN,
+			DyeColor.GREEN,
+			13
+	),
+	CYAN(
+			"cyan",
+			Color.AQUA,
+			ChatColor.DARK_AQUA,
+			DyeColor.CYAN,
+			9
+	),
+	LIGHT_BLUE(
+			"light blue",
+			Color.TEAL,
+			ChatColor.AQUA,
+			DyeColor.LIGHT_BLUE,
+			3
+	),
+	BLUE(
+			"blue",
+			Color.BLUE,
+			ChatColor.BLUE,
+			DyeColor.BLUE,
+			11
+	),
+	PURPLE(
+			"purple",
+			Color.PURPLE,
+			ChatColor.DARK_PURPLE,
+			DyeColor.PURPLE,
+			10
+	),
+	MAGENTA(
+			"magenta",
+			Color.FUCHSIA,
+			null,
+			DyeColor.MAGENTA,
+			2
+	),
+	PINK(
+			"pink",
+			Color.fromRGB(255, 105, 180),
+			ChatColor.LIGHT_PURPLE,
+			DyeColor.PINK,
+			6
+	);
+
+	ColorType(String name, Color color, ChatColor chatColor, DyeColor dyeColor, int durability) {
+		this.name = name;
+		this.color = color;
+		this.chatColor = chatColor;
+		this.dyeColor = dyeColor;
+		this.durability = durability;
+	}
+
+	private String name;
+	private Color color;
+	private ChatColor chatColor;
+	private DyeColor dyeColor;
+	private Integer durability;
+
+	public static ColorType fromString(String name) {
+		return Arrays.stream(values()).filter(colorConfig -> name.equals(colorConfig.getName())).findFirst().get();
+	}
+
+	public static ColorType fromColor(Color color) {
+		return Arrays.stream(values()).filter(colorConfig -> color.equals(colorConfig.getColor())).findFirst().get();
+	}
+
+	public static ColorType fromChatColor(ChatColor chatColor) {
+		return Arrays.stream(values()).filter(colorConfig -> chatColor.equals(colorConfig.getChatColor())).findFirst().get();
+	}
+
+	public static ColorType fromDyeColor(DyeColor dyeColor) {
+		return Arrays.stream(values()).filter(colorConfig -> dyeColor.equals(colorConfig.getDyeColor())).findFirst().get();
+	}
+
+	public static ColorType fromDurability(int durability) {
+		return Arrays.stream(values()).filter(colorConfig -> durability == (colorConfig.getDurability())).findFirst().get();
+	}
+
+}
--- src/main/java/me/pugabyte/bncore/utils/Utils.java
+++ src/main/java/me/pugabyte/bncore/utils/Utils.java
@@ -28,7 +28,6 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
@@ -336,5 +335,6 @@ public class Utils {
 	public static Color getChatColorFromInt(int color){
 		return getChatColorFromString(getStringColorFromInt(color));
 	}
+
 
 }
