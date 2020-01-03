3cdbe580dc806ed84d55a360abc4bdcb30f4e2e3
Pugabyte <pugabyte@projecteden.gg>
2020-01-03 02:34:48 -0500

    Start ColorType tester

--- src/main/java/me/pugabyte/bncore/features/BNCoreCommand.java
+++ src/main/java/me/pugabyte/bncore/features/BNCoreCommand.java
@@ -7,10 +7,17 @@ import me.pugabyte.bncore.framework.commands.models.annotations.TabCompleterFor;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.models.nerds.Nerd;
 import me.pugabyte.bncore.skript.SkriptFunctions;
+import me.pugabyte.bncore.utils.ColorType;
+import me.pugabyte.bncore.utils.FireworkLauncher;
 import org.bukkit.Bukkit;
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.material.Dye;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,12 +36,52 @@ public class BNCoreCommand extends CustomCommand {
 	}
 
 	@Path("getPlayer {offlineplayer}")
-	void getPlayer(@Arg OfflinePlayer player) {
-		reply(player.getName());
+	void getPlayer(@Arg Nerd nerd) {
+		reply(nerd.toString());
 	}
 
 	@Path("redtint {double} {double} {player}")
 	void redTint(@Arg("0.5") double fadeTime, @Arg("10") double intensity, @Arg("self") Player player) {
 		SkriptFunctions.redTint(player, fadeTime, intensity);
+	}
+
+	@TabCompleterFor(ColorType.class)
+	List<String> colorTypeTabComplete(String filter) {
+		return Arrays.stream(ColorType.values())
+				.filter(value -> value.name().toLowerCase().startsWith(filter))
+				.map(Enum::name)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Object convert(String value, Class<?> type) {
+		if (ColorType.class == type)
+			try {
+				return ColorType.valueOf(value.toUpperCase());
+			} catch (IllegalArgumentException ignore) {
+				error("ColorType from " + value + " not found");
+			}
+		return super.convert(value, type);
+	}
+
+	@Path("getColor {color}")
+	void getColor(@Arg ColorType colorType) {
+
+		Location location = player().getLocation().add(2, 0, 0);
+
+		// Firework (Color)
+		new FireworkLauncher(location).color(colorType.getColor()).type(FireworkEffect.Type.BALL).detonateAfter(1).launch();
+
+		// Place block
+		location.getBlock().setType(Material.WOOL);
+		location.getBlock().setData(colorType.getDurability().byteValue());
+
+		// Give dyed chestplate
+		Dye dye = new Dye(Material.LEATHER_CHESTPLATE);
+		dye.setColor(colorType.getDyeColor());
+		location.getWorld().dropItemNaturally(location.add(0, 1, 0), dye.toItemStack(1));
+
+		// ChatColor
+		reply(colorType.getChatColor() + colorType.name());
 	}
 }
