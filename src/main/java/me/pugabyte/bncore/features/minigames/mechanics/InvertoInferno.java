efba001560ed356493a35557d39d49763d922d55
BuildTools <WakkaFlocka239@gmail.com>
2020-01-11 09:45:43 -0600

    InvertoInferno initial setup

---
+++ src/main/java/me/pugabyte/bncore/features/minigames/mechanics/InvertoInferno.java
@@ -0,0 +1,78 @@
+package me.pugabyte.bncore.features.minigames.mechanics;
+
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import me.pugabyte.bncore.features.minigames.Minigames;
+import me.pugabyte.bncore.features.minigames.models.Match;
+import me.pugabyte.bncore.features.minigames.models.Minigamer;
+import me.pugabyte.bncore.features.minigames.models.mechanics.multiplayer.teamless.TeamlessMechanic;
+import me.pugabyte.bncore.utils.WorldGuardUtils;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class InvertoInferno extends TeamlessMechanic {
+
+	@Override
+	public String getName() {
+		return "Inverto Inferno";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Put out the fire!";
+	}
+
+	@Override
+	public GameMode getGameMode() {
+		return GameMode.SURVIVAL;
+	}
+
+	@Override
+	public void onStart(Match match) {
+		super.onStart(match);
+		ProtectedRegion region = new WorldGuardUtils(Minigames.getGameworld()).getRegion("invertoinferno");
+//		ProtectedRegion region = Minigames.getWorldGuardUtils().getRegion("invertoinferno");
+		int vol = region.volume();
+		int percent = 5 / (100 * vol);
+		for (int i = 0; i < percent; i++) {
+//			Block block = Minigames.getWorldGuardUtils().getRandomBlock(region);
+			Block block = new WorldGuardUtils(Minigames.getGameworld()).getRandomBlock(region);
+			if (block.getType().isBurnable()) {
+				block.getRelative(0, 1, 0);
+				if (block.getType().equals(Material.AIR))
+					block.setType(Material.FIRE);
+			}
+		}
+	}
+
+	@Override
+	public void onPlayerInteract(Minigamer minigamer, PlayerInteractEvent event) {
+		super.onPlayerInteract(minigamer, event);
+		if (event.isCancelled()) return;
+
+		Block fire = event.getClickedBlock().getRelative(event.getBlockFace());
+		if (!fire.getType().equals(Material.FIRE)) {
+			event.setCancelled(true);
+			return;
+		}
+
+		minigamer.scored();
+	}
+
+	@EventHandler
+	public void onBlockBurn(BlockBurnEvent event) {
+		Location location = event.getBlock().getLocation();
+
+		for (ProtectedRegion region : Minigames.getWorldGuardUtils().getRegionsAt(location)) {
+			if (region.getId().equalsIgnoreCase("invertoinferno")) {
+				event.setCancelled(true);
+				break;
+			}
+		}
+	}
+
+}
