fca6538c5ed8bae40edddcdce8c5b4223241904c
Pugabyte <pugabyte@bear-nation.net>
2020-01-15 19:08:13 -0500

    Convert back to normal async process

--- src/main/java/me/pugabyte/bncore/features/chat/translator/TranslatorHandler.java
+++ src/main/java/me/pugabyte/bncore/features/chat/translator/TranslatorHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import lombok.SneakyThrows;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
@@ -11,7 +12,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.CompletableFuture;
 
 public class TranslatorHandler {
 	String apiKey;
@@ -20,56 +20,45 @@ public class TranslatorHandler {
 		this.apiKey = apiKey;
 	}
 
-	public CompletableFuture<String> translate(String message, Language from, Language to) {
+	@SneakyThrows
+	public String translate(String message, Language from, Language to) {
 		String link = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=%key%&text=%text%&lang=%language%";
 
 		String language = (from == Language.UNKNOWN) ? to.toString() : from.toString() + "-" + to.toString();
 
-		return CompletableFuture.supplyAsync(() -> {
-			try {
-				URL url = new URL(link
-						.replace("%language%", language.toLowerCase())
-						.replace("%key%", apiKey)
-						.replace("%text%", URLEncoder.encode(message, "UTF-8")));
+		URL url = new URL(link
+				.replace("%language%", language.toLowerCase())
+				.replace("%key%", apiKey)
+				.replace("%text%", URLEncoder.encode(message, "UTF-8")));
 
-				HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream(), StandardCharsets.UTF_8));
-				String input = reader.readLine();
-				reader.close();
+		HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream(), StandardCharsets.UTF_8));
+		String input = reader.readLine();
+		reader.close();
 
-				JsonArray array = new JsonParser().parse(input).getAsJsonObject().getAsJsonArray("text");
-				StringBuilder stringBuilder = new StringBuilder();
-				for (JsonElement member : array) {
-					stringBuilder.append(member.getAsString());
-				}
+		JsonArray array = new JsonParser().parse(input).getAsJsonObject().getAsJsonArray("text");
+		StringBuilder stringBuilder = new StringBuilder();
+		for (JsonElement member : array)
+			stringBuilder.append(member.getAsString());
 
-				return stringBuilder.toString().trim();
-			} catch (Exception ex) {
-				throw new RuntimeException(ex.getMessage());
-			}
-		});
+		return stringBuilder.toString().trim();
 	}
 
-	public CompletableFuture<Language> getLanguage(String message) {
+	@SneakyThrows
+	public Language getLanguage(String message) {
 		String link = "https://translate.yandex.net/api/v1.5/tr.json/detect?key=%key%&text=%text%";
 
-		return CompletableFuture.supplyAsync(() -> {
-			try {
-				URL url = new URL(link
-						.replace("%key%", apiKey)
-						.replace("%text%", URLEncoder.encode(message, "UTF-8")));
+		URL url = new URL(link
+				.replace("%key%", apiKey)
+				.replace("%text%", URLEncoder.encode(message, "UTF-8")));
 
-				HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
-				String input = reader.readLine();
-				reader.close();
+		HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
+		String input = reader.readLine();
+		reader.close();
 
-				JsonObject object = new JsonParser().parse(input).getAsJsonObject();
-				return Language.valueOf(object.get("lang").getAsString().toUpperCase());
-			} catch (Exception ex) {
-				throw new RuntimeException(ex.getMessage());
-			}
-		});
+		JsonObject object = new JsonParser().parse(input).getAsJsonObject();
+		return Language.valueOf(object.get("lang").getAsString().toUpperCase());
 	}
 
 }
