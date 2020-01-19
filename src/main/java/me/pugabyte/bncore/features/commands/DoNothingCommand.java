02d64e678111e9f3e62d9eebebf65bc7cb2234a1
BuildTools <WakkaFlocka239@gmail.com>
2020-01-19 00:00:37 -0600

    Converted commands: /donothing, /countdown, /unique & /whatworld

---
+++ src/main/java/me/pugabyte/bncore/features/commands/DoNothingCommand.java
@@ -0,0 +1,16 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+
+public class DoNothingCommand extends CustomCommand {
+
+	public DoNothingCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path
+	void nothing() {
+	}
+}
