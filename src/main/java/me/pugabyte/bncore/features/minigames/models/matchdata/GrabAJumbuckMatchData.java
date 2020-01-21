2021dddfc121cbaba1e40bf3b24eeeabf5ea9ad6
Pugabyte <pugabyte@projecteden.gg>
2020-01-20 20:30:34 -0500

    Remove unneeded properties on Grab-A-Jumbuck MatchData

--- src/main/java/me/pugabyte/bncore/features/minigames/models/matchdata/GrabAJumbuckMatchData.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/models/matchdata/GrabAJumbuckMatchData.java
@@ -1,7 +1,5 @@
 package me.pugabyte.bncore.features.minigames.models.matchdata;
 
-import com.sk89q.worldedit.BlockVector;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import lombok.Data;
 import me.pugabyte.bncore.features.minigames.models.Match;
 import me.pugabyte.bncore.features.minigames.models.MatchData;
@@ -11,10 +9,6 @@ import java.util.ArrayList;
 
 @Data
 public class GrabAJumbuckMatchData extends MatchData {
-
-	public ProtectedRegion region;
-	public BlockVector max;
-	public BlockVector min;
 	public ArrayList<Entity> sheeps = new ArrayList<>();
 	public ArrayList<Entity> items = new ArrayList<>();
 
