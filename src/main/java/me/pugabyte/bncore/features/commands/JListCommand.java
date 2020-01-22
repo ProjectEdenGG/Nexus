69c0dc338e01c6eef4cb94a1d552d889fc7f372f
BuildTools <WakkaFlocka239@gmail.com>
2020-01-22 17:43:20 -0600

    /staff color fix & fix syntax

--- src/main/java/me/pugabyte/bncore/features/commands/JListCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/JListCommand.java
@@ -1,20 +1,18 @@
 package me.pugabyte.bncore.features.commands;
 
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
-import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 
-@Aliases({"yt"})
-public class YoutubeCommand extends CustomCommand {
+//@Aliases({"ls", "who", "online", "players", "eonline", "elist", "ewho"})
+public class JListCommand extends CustomCommand {
 
-	public YoutubeCommand(CommandEvent event) {
+	public JListCommand(CommandEvent event) {
 		super(event);
 	}
 
 	@Path
-	void youtube() {
-		send("&ehttps://yt.bnn.gg");
-	}
+	void help() {
 
+	}
 }
