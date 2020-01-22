9ca57fa3a70372704fbff85e350d30cdd90caa67
BuildTools <WakkaFlocka239@gmail.com>
2020-01-22 16:46:04 -0600

    convert /suggestion & /website

---
+++ src/main/java/me/pugabyte/bncore/features/commands/WebsiteCommand.java
@@ -0,0 +1,20 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+
+@Aliases({"weburl", "web", "weblink"})
+public class WebsiteCommand extends CustomCommand {
+
+	public WebsiteCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path
+	void website() {
+		send("&5https://bnn.gg");
+	}
+
+}
