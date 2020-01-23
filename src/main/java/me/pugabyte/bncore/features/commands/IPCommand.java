2d038309755a70547c6dc78e7772e492f44b9654
Pugabyte <pugabyte@bear-nation.net>
2020-01-23 01:26:40 -0500

    Fix some casing

--- src/main/java/me/pugabyte/bncore/features/commands/IPCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/IPCommand.java
@@ -4,9 +4,9 @@ import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 
-public class IpCommand extends CustomCommand {
+public class IPCommand extends CustomCommand {
 
-	public IpCommand(CommandEvent event) {
+	public IPCommand(CommandEvent event) {
 		super(event);
 	}
 
