eda5d32f3ce161de5b499577ca36978ad54a5ccb
Blast <cjblanton2@gmail.com>
2020-01-24 17:51:43 -0500

    convert commands Skinify Remove debug from GoldRush

---
+++ src/main/java/me/pugabyte/bncore/features/commands/SkinifyCommand.java
@@ -0,0 +1,21 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import org.bukkit.entity.Player;
+
+@Aliases("skinifier")
+public class SkinifyCommand extends CustomCommand {
+
+	public SkinifyCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path("[player]")
+	void skin(@Arg("self") Player player) {
+		json("&eClick here &3to Bear Nation-ify your skin!" + "||url:http://bnn.gg/skins/uuid=" + player.getUniqueId());
+	}
+}
