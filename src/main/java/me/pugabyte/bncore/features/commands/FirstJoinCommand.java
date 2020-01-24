b31a4b3b4144c34e325ceb2df32c23de0983cc5e
Blast <cjblanton2@gmail.com>
2020-01-23 22:07:47 -0500

    convert commands FirstJoin, LastLogin, LastLogout

---
+++ src/main/java/me/pugabyte/bncore/features/commands/FirstJoinCommand.java
@@ -0,0 +1,20 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.models.nerds.Nerd;
+import me.pugabyte.bncore.utils.Utils;
+
+public class FirstJoinCommand extends CustomCommand {
+
+	public FirstJoinCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path("[player]")
+	void firstJoin(@Arg("self") Nerd nerd) {
+		send("&e&l" + nerd.getName() + " &3first joined Bear Nation at &e" + Utils.timespanDiff(nerd.getFirstJoin()) + " &3US Eastern Time");
+	}
+}
---
+++ src/main/java/me/pugabyte/bncore/features/commands/LastLoginCommand.java
@@ -0,0 +1,20 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.models.nerds.Nerd;
+import me.pugabyte.bncore.utils.Utils;
+
+public class LastLoginCommand extends CustomCommand {
+
+	public LastLoginCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path("[player]")
+	void lastLogin(@Arg("self") Nerd nerd) {
+		send("&e&l" + nerd.getName() + " &3last logged in &e" + Utils.timespanDiff(nerd.getLastJoin()));
+	}
+}
