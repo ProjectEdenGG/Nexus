88d5bf0a6cb666bebc1d3a72b52ce2b04665c55f
BuildTools <WakkaFlocka239@gmail.com>
2020-01-23 22:12:27 -0600

    fix whereis distance handling

--- src/main/java/me/pugabyte/bncore/features/commands/WhereIsCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/WhereIsCommand.java
@@ -4,12 +4,14 @@ import me.pugabyte.bncore.features.chat.Chat;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.utils.Utils;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.inventivetalent.glow.GlowAPI;
 
+@Permission("group.staff")
 public class WhereIsCommand extends CustomCommand {
 
 	public WhereIsCommand(CommandEvent event) {
@@ -19,6 +21,11 @@ public class WhereIsCommand extends CustomCommand {
 	@Path("<player>")
 	void whereIs(@Arg Player playerArg) {
 		Location playerArgLoc = playerArg.getLocation().clone();
+
+		if (!player().getWorld().equals(playerArg.getWorld())) {
+			error(playerArg.getName() + " is in " + Utils.camelCase(playerArg.getWorld().getName()));
+			return;
+		}
 
 		double distance = player().getLocation().distance(playerArgLoc);
 		if (distance > Chat.getLocalRadius()) {
