3b005f4c488107abceb4086dd2f56f0729fcbe28
BuildTools <WakkaFlocka239@gmail.com>
2020-01-25 17:11:35 -0600

    import cleanup

--- src/main/java/me/pugabyte/bncore/features/commands/EmptyCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/EmptyCommand.java
@@ -1,14 +1,12 @@
 package me.pugabyte.bncore.features.commands;
 
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
-import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.utils.Utils;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-
 
 public class EmptyCommand extends CustomCommand {
 
