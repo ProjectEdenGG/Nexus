5a6cbfff271190b217cda9cf9d3afd2a6b58d52b
Blast <cjblanton2@gmail.com>
2020-01-25 18:44:47 -0500

    fix DelVillage and FixFlat commands moved fixFlat method to WorldEditUtils

--- src/main/java/me/pugabyte/bncore/features/commands/worldedit/DelVillageCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/worldedit/DelVillageCommand.java
@@ -1,13 +1,7 @@
 package me.pugabyte.bncore.features.commands.worldedit;
 
 import com.sk89q.worldedit.LocalSession;
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-import com.sk89q.worldedit.regions.CuboidRegion;
-import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.util.Direction;
-import com.sk89q.worldedit.world.World;
 import lombok.SneakyThrows;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.DoubleSlash;
@@ -27,23 +21,13 @@ public class DelVillageCommand extends CustomCommand {
 	@SneakyThrows
 	@Path
 	void delVillage() {
-		World world = new BukkitWorld(player().getWorld());
 		WorldEditPlugin worldEditPlugin = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
 		LocalSession session = worldEditPlugin.getSession(player());
 		WorldEditUtils worldEditUtils = new WorldEditUtils(player().getWorld());
-		Vector pos = worldEditUtils.toVector(player().getLocation());
-		Region region = new CuboidRegion(pos, pos);
-		region = worldEditUtils.expandAll(region, 75);
-		session.getRegionSelector(world).learnChanges();
-		worldEditUtils.fill(region, Material.AIR);
-		region.expand(Direction.DOWN.toVector().multiply(500));
-		region.contract(Direction.DOWN.toVector().multiply(500));
-		session.getRegionSelector(region.getWorld()).learnChanges();
-		worldEditUtils.fill(region, Material.BEDROCK);
-		region.expand(Direction.UP.toVector().multiply(3));
-		region.contract(Direction.UP.toVector().multiply(1));
-		session.getRegionSelector(region.getWorld()).learnChanges();
-		worldEditUtils.fill(region, Material.GRASS);
+		worldEditUtils.setSelection(player(), player().getLocation());
+		ExpandAllCommand.expandAll(player(), 75);
+		worldEditUtils.fill(worldEditUtils.getPlayerSelection(player()), Material.AIR);
+		worldEditUtils.fixFlat(session, worldEditUtils.getPlayerSelection(player()));
 		send("&3Successfully removed the village");
 	}
 }
--- src/main/java/me/pugabyte/bncore/features/commands/worldedit/FixFlatCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/worldedit/FixFlatCommand.java
@@ -3,8 +3,6 @@ package me.pugabyte.bncore.features.commands.worldedit;
 import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.util.Direction;
 import com.sk89q.worldedit.world.World;
 import lombok.SneakyThrows;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
@@ -13,7 +11,6 @@ import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.utils.WorldEditUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 
 @DoubleSlash
 public class FixFlatCommand extends CustomCommand {
@@ -28,16 +25,7 @@ public class FixFlatCommand extends CustomCommand {
 		World world = new BukkitWorld(player().getWorld());
 		WorldEditPlugin worldEditPlugin = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
 		LocalSession session = worldEditPlugin.getSession(player());
-		Region region = session.getSelection(world);
-		WorldEditUtils worldEditUtils = new WorldEditUtils(player().getWorld());
-		region.expand(Direction.DOWN.toVector().multiply(500));
-		region.contract(Direction.DOWN.toVector().multiply(500));
-		session.getRegionSelector(region.getWorld()).learnChanges();
-		worldEditUtils.fill(region, Material.BEDROCK);
-		region.expand(Direction.UP.toVector().multiply(3));
-		region.contract(Direction.UP.toVector().multiply(1));
-		session.getRegionSelector(region.getWorld()).learnChanges();
-		worldEditUtils.fill(region, Material.GRASS);
+		new WorldEditUtils(player().getWorld()).fixFlat(session, session.getSelection(world));
 	}
 }
 
