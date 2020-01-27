48734da9577ab24d330b31fc23f4a24911fbb96c
Blast <cjblanton2@gmail.com>
2020-01-27 12:18:57 -0500

    add HiKoda Command with cooldown

---
+++ src/main/java/me/pugabyte/bncore/features/commands/HiKodaCommand.java
@@ -0,0 +1,20 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Cooldown;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+
+public class HiKodaCommand extends CustomCommand {
+
+	public HiKodaCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path
+	@Cooldown(value = 60 * 20, bypass = "group.staff")
+	void hiKoda() {
+		runCommand("ch qm g Hi Koda!");
+	}
+
+}
