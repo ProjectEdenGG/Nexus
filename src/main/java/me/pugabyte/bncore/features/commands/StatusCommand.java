fe014f64907c0ce47013a225fdd97b4729211eec
Blast <cjblanton2@gmail.com>
2020-01-29 23:18:08 -0500

    add runConsoleCommand convert command Status

---
+++ src/main/java/me/pugabyte/bncore/features/commands/StatusCommand.java
@@ -0,0 +1,51 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+
+public class StatusCommand extends CustomCommand {
+
+	public StatusCommand(CommandEvent event) {
+		super(event);
+	}
+
+	String perm = "featherboard.group.";
+	String userPerm = perm + "user";
+
+	@Path("(on|enable|true)")
+	void on() {
+		if (!perms()) addUserPerm();
+		else runCommand("fb on");
+	}
+
+	@Path("(off|disable|false)")
+	void off() {
+		if (!perms()) removeUserPerm();
+		else runCommand("fb off");
+	}
+
+	@Path
+	void toggle() {
+		if (!perms()) {
+			if (player().hasPermission(userPerm)) removeUserPerm();
+			else addUserPerm();
+		}
+		runCommand("rb off");
+	}
+
+	boolean perms() {
+		if (player().hasPermission(perm + "moderator")) return true;
+		if (player().hasPermission(perm + "operator")) return true;
+		return player().hasPermission(perm + "admin");
+	}
+
+	public void addUserPerm() {
+		runConsoleCommand("pex user " + player().getName() + " add " + userPerm);
+	}
+
+	public void removeUserPerm() {
+		runConsoleCommand("pex user " + player().getName() + " remove " + userPerm);
+	}
+
+}
