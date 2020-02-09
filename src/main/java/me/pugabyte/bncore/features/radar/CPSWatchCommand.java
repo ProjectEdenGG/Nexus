281642e031f280afe545bb9bf8b1b6e5f1061f07
BuildTools <WakkaFlocka239@gmail.com>
2020-02-08 19:22:13 -0600

    convert cpswatch and reachwatch

---
+++ src/main/java/me/pugabyte/bncore/features/radar/CPSWatchCommand.java
@@ -0,0 +1,92 @@
+package me.pugabyte.bncore.features.radar;
+
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import me.pugabyte.bncore.BNCore;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.utils.Tasks;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@NoArgsConstructor
+@Permission("group.staff")
+public class CPSWatchCommand extends CustomCommand implements Listener {
+
+	static Map<Player, Integer> cpsMap = new HashMap<>();
+	static Map<Player, List<Player>> watchMap = new HashMap<>();
+
+	public CPSWatchCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	static {
+		BNCore.registerListener(new CPSWatchCommand());
+	}
+
+	@Path("<player>")
+	void cpsWatch(@Arg Player player) {
+		watchMap.putIfAbsent(player, new ArrayList<>());
+		List<Player> watchList = watchMap.get(player);
+
+		if (watchList.contains(player())) {
+			watchList.remove(player());
+			send(PREFIX + "CPS Watcher &cdisabled &3for " + player.getName());
+		} else {
+			watchList.add(player());
+			send(PREFIX + "CPS Watcher &aenabled &3for " + player.getName());
+		}
+	}
+
+	@EventHandler
+	public void onClick(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+
+		if (watchMap.get(player) == null || watchMap.get(player).size() == 0)
+			return;
+
+		cpsMap.putIfAbsent(player, 0);
+		cpsMap.replace(player, cpsMap.get(player) + 1);
+
+		if (cpsMap.get(player) != 1)
+			return;
+
+		Tasks.wait(20, () -> {
+			Integer cps = cpsMap.get(player);
+			if (cps == null)
+				return;
+
+			if (cps > 2) {
+				final String color =
+						(cps > 20) ? "&c" :
+								(cps > 15) ? "&6" :
+										"&e";
+
+				watchMap.get(player).forEach(staff -> send(staff, PREFIX + player.getName() + "'s CPS is " + color + cps));
+			}
+
+			cpsMap.remove(player);
+		});
+	}
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		watchMap.get(player).forEach(staff ->
+				send(staff, PREFIX + "&c" + player.getName() + " went offline"));
+		watchMap.remove(player);
+	}
+
+
+}
---
+++ src/main/java/me/pugabyte/bncore/features/radar/ReachWatchCommand.java
@@ -0,0 +1,89 @@
+package me.pugabyte.bncore.features.radar;
+
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import me.pugabyte.bncore.BNCore;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static me.pugabyte.bncore.utils.Utils.camelCase;
+
+@NoArgsConstructor
+@Permission("group.staff")
+public class ReachWatchCommand extends CustomCommand implements Listener {
+
+	static Map<Player, List<Player>> watchMap = new HashMap<>();
+	private DecimalFormat nf = new DecimalFormat("#.00");
+
+	public ReachWatchCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	static {
+		BNCore.registerListener(new ReachWatchCommand());
+	}
+
+	@Path("<player>")
+	void reachWatch(@Arg Player player) {
+		watchMap.putIfAbsent(player, new ArrayList<>());
+		List<Player> watchList = watchMap.get(player);
+
+		if (watchList.contains(player())) {
+			watchList.remove(player());
+			send(PREFIX + "Reach Watcher &cdisabled &3for " + player.getName());
+		} else {
+			watchList.add(player());
+			send(PREFIX + "Reach Watcher &aenabled &3for " + player.getName());
+		}
+	}
+
+	@EventHandler
+	public void onDamage(EntityDamageByEntityEvent event) {
+		if (!(event.getDamager() instanceof Player))
+			return;
+
+		Player attacker = (Player) event.getDamager();
+
+		if (watchMap.get(attacker) == null || watchMap.get(attacker).size() == 0)
+			return;
+
+		if (!event.getCause().equals(EntityDamageEvent.DamageCause.ENTITY_ATTACK))
+			return;
+
+		Entity victim = event.getEntity();
+		String victimName = victim.getType().equals(EntityType.PLAYER) ? victim.getName() : camelCase(victim.getType().toString());
+		double distnace = attacker.getLocation().distance(victim.getLocation());
+
+		if (distnace > 3) {
+			String color = (distnace > 5) ? "&c" : (distnace > 3.7) ? "&6" : "&e";
+
+			watchMap.get(attacker).forEach(staff ->
+					send(staff, PREFIX + attacker.getName() + " --> " + victimName + " " + color + nf.format(distnace)));
+		}
+	}
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		watchMap.get(player).forEach(staff ->
+				send(staff, PREFIX + "&c" + player.getName() + " went offline"));
+		watchMap.remove(player);
+	}
+}
