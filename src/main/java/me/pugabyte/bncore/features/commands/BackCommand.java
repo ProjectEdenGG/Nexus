d0d934e54c739edfa3fb4276b96e3dcbcc5ce292
Pugabyte <pugabyte@projecteden.gg>
2020-02-12 19:47:48 -0500

    Finish back command

--- src/main/java/me/pugabyte/bncore/features/commands/BackCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/BackCommand.java
@@ -5,10 +5,13 @@ import me.pugabyte.bncore.BNCore;
 import me.pugabyte.bncore.features.minigames.Minigames;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.models.back.Back;
 import me.pugabyte.bncore.models.back.BackService;
+import me.pugabyte.bncore.utils.JsonBuilder;
 import me.pugabyte.bncore.utils.Utils;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -16,6 +19,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+
+import java.text.DecimalFormat;
 
 @Aliases("return")
 @NoArgsConstructor
@@ -33,8 +38,38 @@ public class BackCommand extends CustomCommand implements Listener {
 	}
 
 	@Path("[count]")
-	void help(int count) {
-		runCommand("essentials:back");
+	void help(@Arg("1") int count) {
+		if (!player().hasPermission("group.staff"))
+			count = 1;
+
+		Location location = back.getLocations().get(count - 1);
+		if (location == null)
+			error("You have no back location");
+
+		player().teleport(location, TeleportCause.COMMAND);
+	}
+
+	@Path("locations")
+	@Permission("group.staff")
+	void view() {
+		if (back.getLocations() == null || back.getLocations().size() == 0)
+			error("You have no back locations");
+
+		int i = 0;
+		JsonBuilder json = json(PREFIX + "Locations (&eClick to go&3):");
+
+		for (Location location : back.getLocations()) {
+			++i;
+			int x = (int) location.getX(), y = (int) location.getY(), z = (int) location.getZ(),
+					yaw = (int) location.getYaw(), pitch = (int) location.getPitch();
+			json.newline()
+					.next("&3" + new DecimalFormat("#00").format(i) + " &e" + location.getWorld().getName() +
+							" &7/ &e" + x + " &7/ &e" + y + " &7/ &e" + z)
+					.command("/tppos " + x + " " + y + " " + z + " " + yaw + " " + pitch + " " + location.getWorld().getName())
+					.hover("&eClick to go");
+		}
+
+		send(json);
 	}
 
 	@EventHandler
