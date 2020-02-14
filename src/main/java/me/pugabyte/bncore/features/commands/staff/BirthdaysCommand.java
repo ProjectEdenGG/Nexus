ea7c9ff8a985fa4f22eaced6ea67df4fdb28f3e1
Blast <cjblanton2@gmail.com>
2020-02-14 02:12:46 -0500

    fix ArrayIndex exception and negative birthdays

--- src/main/java/me/pugabyte/bncore/features/commands/staff/BirthdaysCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/BirthdaysCommand.java
@@ -26,7 +26,7 @@ public class BirthdaysCommand extends CustomCommand {
 			return (int) ChronoUnit.DAYS.between(now, getNextBirthday(nerd1)) - (int) ChronoUnit.DAYS.between(now, getNextBirthday(nerd2));
 		});
 		send("&3Upcoming birthdays:");
-		for (int i = 0; i < amount; i++) {
+		for (int i = 0; i < Math.min(amount, nerds.size()); i++) {
 			send("&3" + (i + 1) + " &e" + nerds.get(i).getName() + " &7- " + ChronoUnit.DAYS.between(LocalDate.now(), getNextBirthday(nerds.get(i))) + " days");
 		}
 	}
@@ -35,7 +35,8 @@ public class BirthdaysCommand extends CustomCommand {
 		LocalDate now = LocalDate.now();
 		LocalDate birthday = nerd.getBirthday();
 		boolean thisYear = true;
-		if (birthday.getMonth().getValue() < now.getMonth().getValue()) thisYear = false;
+		if (birthday.getMonth().getValue() < now.getMonth().getValue() && birthday.getDayOfMonth() < now.getDayOfMonth())
+			thisYear = false;
 		return birthday.withYear(thisYear ? now.getYear() : now.getYear() + 1);
 	}
 }
