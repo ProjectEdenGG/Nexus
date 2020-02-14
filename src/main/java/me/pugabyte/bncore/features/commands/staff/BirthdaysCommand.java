164f25cdfc6b6c8cfe1cf6d906c281fd02fc596c
Blast <cjblanton2@gmail.com>
2020-02-14 01:53:36 -0500

    convert birthdays command

--- src/main/java/me/pugabyte/bncore/features/commands/staff/BirthdaysCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/BirthdaysCommand.java
@@ -8,7 +8,7 @@ import me.pugabyte.bncore.models.nerds.Nerd;
 import me.pugabyte.bncore.models.nerds.NerdService;
 
 import java.time.LocalDate;
-import java.util.Comparator;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 public class BirthdaysCommand extends CustomCommand {
@@ -21,17 +21,21 @@ public class BirthdaysCommand extends CustomCommand {
 	void birthday(@Arg("5") int amount) {
 		NerdService service = new NerdService();
 		List<Nerd> nerds = service.getNerdsWithBirthdays();
-		nerds.sort(Comparator.comparing(Nerd::getBirthday));
+		nerds.sort((nerd1, nerd2) -> {
+			LocalDate now = LocalDate.now();
+			return (int) ChronoUnit.DAYS.between(now, getNextBirthday(nerd1)) - (int) ChronoUnit.DAYS.between(now, getNextBirthday(nerd2));
+		});
 		send("&3Upcoming birthdays:");
 		for (int i = 0; i < amount; i++) {
-			LocalDate now = LocalDate.now();
-			LocalDate birthday = nerds.get(i).getBirthday();
-			boolean thisYear = true;
-			if (birthday.getMonth().getValue() < now.getMonth().getValue()) thisYear = false;
-			LocalDate nextBirthday = birthday.withYear(thisYear ? now.getYear() : now.getYear() + 1);
-			send("&3" + (i + 1) + " &e" + nerds.get(i).getName() + " &7- " + now.until(nextBirthday).getDays() + " days");
+			send("&3" + (i + 1) + " &e" + nerds.get(i).getName() + " &7- " + ChronoUnit.DAYS.between(LocalDate.now(), getNextBirthday(nerds.get(i))) + " days");
 		}
 	}
 
-
+	public LocalDate getNextBirthday(Nerd nerd) {
+		LocalDate now = LocalDate.now();
+		LocalDate birthday = nerd.getBirthday();
+		boolean thisYear = true;
+		if (birthday.getMonth().getValue() < now.getMonth().getValue()) thisYear = false;
+		return birthday.withYear(thisYear ? now.getYear() : now.getYear() + 1);
+	}
 }
