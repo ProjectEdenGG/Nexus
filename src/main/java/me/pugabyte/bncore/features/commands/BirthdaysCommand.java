aeab2873e42c0593ae3d4dd6a9cb070931765b55
Pugabyte <pugabyte@projecteden.gg>
2020-02-14 19:47:37 -0500

    Fix birthdays

--- src/main/java/me/pugabyte/bncore/features/commands/BirthdaysCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/BirthdaysCommand.java
@@ -1,4 +1,4 @@
-package me.pugabyte.bncore.features.commands.staff;
+package me.pugabyte.bncore.features.commands;
 
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
@@ -10,6 +10,7 @@ import me.pugabyte.bncore.models.nerds.NerdService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -17,19 +18,21 @@ import java.util.Locale;
 
 @Aliases("birthday")
 public class BirthdaysCommand extends CustomCommand {
+	NerdService service = new NerdService();
 
 	public BirthdaysCommand(CommandEvent event) {
 		super(event);
 	}
 
 	@Path("[amount]")
-	void birthday(@Arg("5") int amount) {
-		NerdService service = new NerdService();
+	void birthday(@Arg("10") int amount) {
 		List<Nerd> nerds = service.getNerdsWithBirthdays();
 		nerds.sort((nerd1, nerd2) -> {
 			LocalDate now = LocalDate.now();
 			return (int) ChronoUnit.DAYS.between(now, getNextBirthday(nerd1)) - (int) ChronoUnit.DAYS.between(now, getNextBirthday(nerd2));
 		});
+		send("Size: " + nerds.size());
+
 		line();
 		send("&3Upcoming birthdays:");
 		line();
@@ -37,29 +40,30 @@ public class BirthdaysCommand extends CustomCommand {
 			if (LocalDate.now().getDayOfYear() == getNextBirthday(nerds.get(i)).getDayOfYear())
 				send("&3" + (i + 1) + " &e" + nerds.get(i).getName() + " &7- Today");
 			else
-				json("&3" + (i + 1) + " &e" + nerds.get(i).getName() + " &7- " +
-						ChronoUnit.DAYS.between(LocalDate.now(), getNextBirthday(nerds.get(i))) + " days").hover(nerds.get(i).getBirthday().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+				send(json()
+						.next("&3" + (i + 1) + " &e" + nerds.get(i).getName() + " &7- " +
+								ChronoUnit.DAYS.between(LocalDate.now(), getNextBirthday(nerds.get(i))) + " days")
+						.hover(nerds.get(i).getBirthday().format(DateTimeFormatter.ofPattern("MM/dd/yyyy"))));
 		}
 	}
 
-	@Path("[set]")
-	void set() {
-		if (arg(2) == null) format();
-		NerdService service = new NerdService();
-		Nerd nerd = new Nerd();
-		nerd.fromPlayer(player());
+	@Path("set <birthday>")
+	void set(String input) {
+		if (isNullOrEmpty(input))
+			format();
+
+		Nerd nerd = service.get(player());
 		try {
-			LocalDate birthday = LocalDate.parse(arg(2), DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+			LocalDate birthday = LocalDate.parse(input, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
 			nerd.setBirthday(birthday);
 			service.save(nerd);
 			send(PREFIX + "Your birthday has been set to &e" + birthday.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()) + " " + birthday.getDayOfMonth() + ", " + birthday.getYear());
-		} catch (Exception e) {
+		} catch (DateTimeParseException e) {
 			format();
 		}
-
 	}
 
-	@Path("[format]")
+	@Path("format")
 	void format() {
 		error("Correct birthday format: MM/dd/yyyy");
 	}
@@ -67,10 +71,7 @@ public class BirthdaysCommand extends CustomCommand {
 	public LocalDate getNextBirthday(Nerd nerd) {
 		LocalDate now = LocalDate.now();
 		LocalDate birthday = nerd.getBirthday();
-		boolean thisYear = true;
-		if (birthday.getMonth().getValue() < now.getMonth().getValue() ||
-				(birthday.getMonth().getValue() == now.getMonth().getValue() && birthday.getDayOfMonth() <= now.getDayOfMonth()))
-			thisYear = false;
+		boolean thisYear = !birthday.withYear(now.getYear()).isBefore(now);
 		return birthday.withYear(thisYear ? now.getYear() : now.getYear() + 1);
 	}
 }
