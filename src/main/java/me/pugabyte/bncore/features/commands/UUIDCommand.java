21b93c1c291ea2582b8eb345641f058cf1efe80a
Blast <cjblanton2@gmail.com>
2020-02-16 01:29:19 -0500

    convert commands name & uuid

---
+++ src/main/java/me/pugabyte/bncore/features/commands/UUIDCommand.java
@@ -0,0 +1,20 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import org.bukkit.entity.Player;
+
+public class UUIDCommand extends CustomCommand {
+
+	public UUIDCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path("[player]")
+	void uuid(@Arg("self") Player player) {
+		send(json("&e" + player.getUniqueId()).hover("&3Click to copy").suggest(player.getUniqueId().toString()));
+	}
+
+}
