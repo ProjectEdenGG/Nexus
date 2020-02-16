5f4b3d2abba8445e39f30af6a40d09086fbc2c74
Blast <cjblanton2@gmail.com>
2020-02-15 23:58:43 -0500

    convert command suicide

---
+++ src/main/java/me/pugabyte/bncore/features/commands/SuicideCommand.java
@@ -0,0 +1,25 @@
+package me.pugabyte.bncore.features.commands;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.utils.WorldGroup;
+import org.bukkit.GameMode;
+
+public class SuicideCommand extends CustomCommand {
+
+	public SuicideCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path
+	void suicide() {
+		if (!WorldGroup.get(player().getWorld()).equals(WorldGroup.SURVIVAL))
+			error("You can only do this command in the survival world.");
+		runCommand("god off");
+		player().setGameMode(GameMode.SURVIVAL);
+		player().setHealth(0.0);
+	}
+
+
+}
