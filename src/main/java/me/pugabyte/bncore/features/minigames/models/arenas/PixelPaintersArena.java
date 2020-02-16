0282932d2724179bdb25d8aa58fdfa7e38515163
BuildTools <WakkaFlocka239@gmail.com>
2020-02-16 01:07:55 -0600

    PixelPainters work

--- src/main/java/me/pugabyte/bncore/features/minigames/models/arenas/PixelPaintersArena.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/models/arenas/PixelPaintersArena.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import me.pugabyte.bncore.features.minigames.models.Arena;
 import org.bukkit.configuration.serialization.SerializableAs;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Data
@@ -16,12 +17,19 @@ public class PixelPaintersArena extends Arena {
 	private Region logoRegion = getRegion("logo");
 	private Region lobbyAnimationRegion = getRegion("lobbyanimation");
 
+	public PixelPaintersArena(Map<String, Object> map) {
+		super(map);
+	}
+
 	@Override
 	public String getRegionBaseName() {
 		return "pixelpainters";
 	}
 
-	public PixelPaintersArena(Map<String, Object> map) {
-		super(map);
+	@Override
+	public Map<String, Object> serialize() {
+		LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) super.serialize();
+		return map;
 	}
+
 }
