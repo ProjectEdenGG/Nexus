a7b431bb59413ebddcdac8eed17f5a004cb094a9
Blast <cjblanton2@gmail.com>
2020-02-16 16:46:28 -0500

    Refractor MSCBCommand to mcmmo package, rename JMcMMOPrestigeCommand

--- src/main/java/me/pugabyte/bncore/features/mcmmo/MCSBCommand.java
+++ src/main/java/me/pugabyte/bncore/features/mcmmo/MCSBCommand.java
@@ -1,12 +1,12 @@
-package me.pugabyte.bncore.features.commands.aliases;
+package me.pugabyte.bncore.features.mcmmo;
 
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 
-public class MCSKCommand extends CustomCommand {
+public class MCSBCommand extends CustomCommand {
 
-	public MCSKCommand(CommandEvent event) {
+	public MCSBCommand(CommandEvent event) {
 		super(event);
 	}
 
