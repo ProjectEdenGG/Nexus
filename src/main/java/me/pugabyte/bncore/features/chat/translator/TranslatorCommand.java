8394b55c267113758671fcac59b7d64e76b356f5
Pugabyte <pugabyte@bear-nation.net>
2020-02-16 22:18:06 -0500

    Path parser bugfix

--- src/main/java/me/pugabyte/bncore/features/chat/translator/TranslatorCommand.java
+++ src/main/java/me/pugabyte/bncore/features/chat/translator/TranslatorCommand.java
@@ -3,7 +3,6 @@ package me.pugabyte.bncore.features.chat.translator;
 import me.pugabyte.bncore.features.chat.Chat;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
-import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
