b4f5993d762a8b31ff68a673d6ce2a502b581f71
BuildTools <WakkaFlocka239@gmail.com>
2020-02-21 13:51:15 -0600

    properly setup UEMatchData: was causing errors to games without matchdata

--- src/main/java/me/pugabyte/bncore/features/minigames/models/matchdata/UncivilEngineersMatchData.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/models/matchdata/UncivilEngineersMatchData.java
@@ -1,7 +1,10 @@
 package me.pugabyte.bncore.features.minigames.models.matchdata;
 
 import lombok.Data;
+import me.pugabyte.bncore.features.minigames.mechanics.UncivilEngineers;
+import me.pugabyte.bncore.features.minigames.models.Match;
 import me.pugabyte.bncore.features.minigames.models.MatchData;
+import me.pugabyte.bncore.features.minigames.models.annotations.MatchDataFor;
 import org.bukkit.entity.Entity;
 
 import java.util.HashMap;
@@ -9,6 +12,11 @@ import java.util.Map;
 import java.util.UUID;
 
 @Data
+@MatchDataFor(UncivilEngineers.class)
 public class UncivilEngineersMatchData extends MatchData {
 	public Map<Entity, UUID> entities = new HashMap<>();
+
+	public UncivilEngineersMatchData(Match match) {
+		super(match);
+	}
 }
