7d9ad3a488643e7c8b3cf4529108d8591b0786d2
Pugabyte <pugabyte@bear-nation.net>
2020-02-23 19:20:35 -0500

    respect arena max players

--- src/main/java/me/pugabyte/bncore/features/minigames/models/MatchData.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/models/MatchData.java
@@ -2,11 +2,16 @@ package me.pugabyte.bncore.features.minigames.models;
 
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import me.pugabyte.bncore.features.minigames.Minigames;
+import me.pugabyte.bncore.utils.WorldEditUtils;
+import me.pugabyte.bncore.utils.WorldGuardUtils;
 
 @NoArgsConstructor
 public class MatchData {
 	@ToString.Exclude
 	private Match match;
+	protected WorldGuardUtils WGUtils = Minigames.getWorldGuardUtils();
+	protected WorldEditUtils WEUtils = Minigames.getWorldEditUtils();
 
 	public MatchData(Match match) {
 		this.match = match;
