bd4be6f1c45ae79a5030d9bf0711f18a3a60c4d6
Pugabyte <pugabyte@projecteden.gg>
2020-02-29 14:03:55 -0500

    Battleship grid work + bugfixes

--- src/main/java/me/pugabyte/bncore/features/minigames/commands/BattleshipCommand.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/commands/BattleshipCommand.java
@@ -3,7 +3,9 @@ package me.pugabyte.bncore.features.minigames.commands;
 import lombok.NonNull;
 import me.pugabyte.bncore.features.minigames.managers.PlayerManager;
 import me.pugabyte.bncore.features.minigames.mechanics.Battleship;
+import me.pugabyte.bncore.features.minigames.mechanics.Battleship.ShipType;
 import me.pugabyte.bncore.features.minigames.models.Minigamer;
+import me.pugabyte.bncore.features.minigames.models.matchdata.BattleshipMatchData;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
@@ -17,16 +19,25 @@ import java.util.Arrays;
 @Permission("group.staff")
 public class BattleshipCommand extends CustomCommand {
 	private Minigamer minigamer;
+	BattleshipMatchData matchData;
 
 	public BattleshipCommand(@NonNull CommandEvent event) {
 		super(event);
-		if (isPlayer())
+		if (isPlayer()) {
 			minigamer = PlayerManager.get(player());
+			if (minigamer.isPlaying(Battleship.class))
+				matchData = minigamer.getMatch().getMatchData();
+		}
 	}
 
 	@Path("kit")
 	void kit() {
-		Arrays.asList(Battleship.Ship.values()).forEach(ship -> Utils.giveItem(player(), ship.getItem()));
+		Arrays.asList(ShipType.values()).forEach(shipType -> Utils.giveItem(player(), shipType.getItem()));
+	}
+
+	@Path("getChatGrid")
+	void getChatGrid() {
+		matchData.getGrid(minigamer.getTeam()).getChatGrid().forEach(this::send);
 	}
 
 }
