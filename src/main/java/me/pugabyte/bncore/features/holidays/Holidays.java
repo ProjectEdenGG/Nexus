d88f1dc947cbfc360f3c8f034969ffedf0eb50ef
BuildTools <WakkaFlocka239@gmail.com>
2020-03-08 21:17:33 -0500

    Holi work

--- src/main/java/me/pugabyte/bncore/features/holidays/Holidays.java
+++ src/main/java/me/pugabyte/bncore/features/holidays/Holidays.java
@@ -1,11 +1,13 @@
 package me.pugabyte.bncore.features.holidays;
 
 import me.pugabyte.bncore.features.holidays.bearfair20.BearFair20;
+import me.pugabyte.bncore.features.holidays.holi.Holi20;
 
 public class Holidays {
 
 	public Holidays() {
 		new BearFair20();
+		new Holi20();
 	}
 
 }
