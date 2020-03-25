9f19a1d2ae183072e7b55edd163d98018df3478f
BuildTools <WakkaFlocka239@gmail.com>
2020-03-25 06:00:57 -0500

    finish LaunchPads

--- src/main/java/me/pugabyte/bncore/features/listeners/LaunchPads.java
+++ src/main/java/me/pugabyte/bncore/features/listeners/LaunchPads.java
@@ -1,150 +1,166 @@
 package me.pugabyte.bncore.features.listeners;
 
-import me.pugabyte.bncore.BNCore;
-import me.pugabyte.bncore.framework.annotations.Disabled;
 import me.pugabyte.bncore.utils.Tasks;
 import me.pugabyte.bncore.utils.Utils;
 import me.pugabyte.bncore.utils.WorldGroup;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.material.MaterialData;
 
-@Disabled
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 public class LaunchPads implements Listener {
-	private int taskId;
-	private final static double DECELERATION_RATE = 0.98D;
-	private final static double GRAVITY_CONSTANT = 0.08D;
-	private final static double VANILLA_ANTICHEAT_THRESHOLD = 9.5D; // actual 10D
+	private static Map<Player, Integer> taskIDs = new HashMap<>();
+	private static Map<Player, FallingBlock> jumpPadPlayers = new HashMap<>();
+	private static List<UUID> jumpPadBlockUUIDs = new ArrayList<>();
 
 	@EventHandler
-	public void onRedstoneBlockActivate(PlayerInteractEvent event) {
+	public void onPressurePlatePress(PlayerInteractEvent event) {
 		Block block = event.getClickedBlock();
-		if (block == null)
-			return;
-
-		if (!block.getType().equals(Material.GLOWING_REDSTONE_ORE))
-			return;
-
-		if (!event.getAction().equals(Action.PHYSICAL))
-			return;
-
-		if (event.getPlayer().isSneaking())
-			return;
+		if (block == null) return;
+		if (!event.getAction().equals(Action.PHYSICAL)) return;
+		if (!block.getType().equals(Material.GOLD_PLATE)) return;
+		if (event.getPlayer().isSneaking()) return;
 
 		Block below = block.getRelative(0, -1, 0).getLocation().getBlock();
-		if (Utils.isNullOrAir(below.getType()))
+		if (!(below.getType().equals(Material.GLOWING_REDSTONE_ORE) || below.getType().equals(Material.REDSTONE_ORE)))
 			return;
 
-		if (Utils.isSign(below.getType())) {
-			Sign sign = (Sign) below.getState();
+		Block belowBelow = below.getRelative(0, -1, 0).getLocation().getBlock();
+		if (Utils.isNullOrAir(belowBelow.getType())) return;
+
+		event.setCancelled(true);
+
+		if (Utils.isSign(belowBelow.getType())) {
+			Sign sign = (Sign) belowBelow.getState();
 			String[] lines = sign.getLines();
 
-			if (!lines[1].equalsIgnoreCase("[LaunchPad]"))
-				return;
+			if (!lines[0].equalsIgnoreCase("[LaunchPad]")) return;
+			if (lines[1].equalsIgnoreCase("") || lines[2].equalsIgnoreCase("")) return;
 
-			if (lines[2].equalsIgnoreCase("") || lines[3].equalsIgnoreCase(""))
-				return;
+			double power = Double.parseDouble(lines[1]);
+			double angle = Double.parseDouble(lines[2]);
 
-			double height = Double.parseDouble(lines[2]);
-			double distance = Double.parseDouble(lines[3]);
+			if (!lines[3].equalsIgnoreCase("")) {
+				double direction = Double.parseDouble(lines[3]);
+				launchPlayer(event.getPlayer(), power, angle, direction);
+			}
 
-			launchPlayer(event.getPlayer(), height, distance);
+			launchPlayer(event.getPlayer(), power, angle);
 		} else if (WorldGroup.get(block.getLocation().getWorld()).equals(WorldGroup.MINIGAMES))
 			launchPlayer(event.getPlayer());
 	}
 
 	public void launchPlayer(Player player) {
-		launchPlayer(player, 5.0, 5.0);
+		launchPlayer(player, 10.0, 45.0, -1.0);
 	}
 
 	public void launchPlayer(Player player, double power, double angle) {
-		launchPlayer(player, power, angle, -1);
+		launchPlayer(player, power, angle, -1.0);
 	}
 
-	// <Power>
-	// <Angle>
-	// [Direction]
 	public void launchPlayer(Player player, double power, double angle, double direction) {
-		if (direction == -1) {
+		if (jumpPadPlayers.get(player) != null)
+			return;
 
-		} else {
+		if (direction == -1.0)
+			direction = player.getLocation().getYaw();
+		power *= 2;
 
-		}
+		Location launchLocation = player.getLocation();
+		launchLocation.setPitch((float) -angle);
+		launchLocation.setYaw((float) direction);
 
+		// Piston Moving Piece is invisible?
+		MaterialData PISTON = new MaterialData(Material.PISTON_MOVING_PIECE);
+
+		FallingBlock fallingBlock = launchLocation.getWorld().spawnFallingBlock(launchLocation, PISTON.getItemType(), PISTON.getData());
+		jumpPadPlayers.put(player, fallingBlock);
+		jumpPadBlockUUIDs.add(fallingBlock.getUniqueId());
+		fallingBlock.setVelocity(launchLocation.getDirection().normalize().multiply(power / 15.0));
+
+		playerVelTask(player);
+		player.getWorld().createExplosion(player.getLocation(), -1);
 	}
 
 	private void playerVelTask(Player player) {
+		taskIDs.put(player, Tasks.repeat(0, 1, () -> {
+			boolean endFlight = false;
+			FallingBlock fBlock = jumpPadPlayers.get(player);
+			fBlock.setDropItem(false);
 
-	}
+			if (fBlock != null) {
+				if ((fBlock.getWorld() != null && !player.getLocation().getWorld().equals(fBlock.getWorld()))
+						|| fBlock.getLocation().clone().add(fBlock.getLocation().getDirection().clone().multiply(0.5)).getBlock().isLiquid())
+					endFlight = true;
 
-	private void cancelPlayerVelTask() {
-		Tasks.cancel(taskId);
-		BNCore.log("Canceled Task: " + taskId);
-	}
+				if (!fBlock.isDead() && !endFlight)
+					player.setVelocity(fBlock.getVelocity());
+			}
 
-	/*
-			double y = height * 0.5;
-			playerVelTask(player, y, distance);
+			if (fBlock == null || fBlock.isOnGround() || fBlock.isDead() || fBlock.getLocation().getY() < -10.0
+					|| fBlock.getVelocity().length() == 0.0 || endFlight || jumpPadPlayers.get(player) == null) {
 
-		private void playerVelTask(Player player, double height, double distance){
-			final double[] velY = {height};
-			Location locCached = new Location(null,0,0,0);
-			Vector direction = player.getLocation().getDirection().multiply(distance);
-
-			taskId = Tasks.repeat(0, 1, () -> {
-				if (velY[0] > VANILLA_ANTICHEAT_THRESHOLD) {
-					player.getLocation(locCached).setY(locCached.getY() + velY[0]);
-					player.teleport(locCached);
-					player.setVelocity(new Vector(direction.getX(), VANILLA_ANTICHEAT_THRESHOLD, direction.getZ()));
-				} else {
-					player.setVelocity(new Vector(direction.getX(), velY[0], direction.getZ()));
-					cancelPlayerVelTask();
+				if (fBlock != null) {
+					fBlock.remove();
+					jumpPadBlockUUIDs.remove(fBlock.getUniqueId());
 				}
 
-				velY[0] -= GRAVITY_CONSTANT;
-				velY[0] *= DECELERATION_RATE;
-			});
+				jumpPadPlayers.remove(player);
+				cancelPlayerVelTask(player);
+			}
+		}));
 	}
-	 */
 
-	/*
-		double y = height * 0.5;
-		Vector upwards = player.getVelocity().setY(y);
-		player.setVelocity(upwards);
-
-		Vector direction = player.getLocation().getDirection().multiply(distance);
-		direction.setY(upwards.getY());
-
-		player.setVelocity(direction);
-		player.getWorld().createExplosion(player.getLocation(), -1);
+	private void cancelPlayerVelTask(Player player) {
+		Tasks.cancel(taskIDs.get(player));
 	}
-	 */
 
-	/* Pstones Player Launch
+	@EventHandler
+	public void onEntityChangeBlockEvent(final EntityChangeBlockEvent event) {
+		if (event == null) return;
 
-	final float height = field.getVelocity() > 0 ? field.getVelocity() : field.getSettings().getLaunchHeight();
-	double speed = 8;
+		if (jumpPadBlockUUIDs.contains(event.getEntity().getUniqueId()))
+			event.setCancelled(true);
 
-	Vector loc = player.getLocation().toVector();
-	Vector target = new Vector(field.getX(), field.getY(), field.getZ());
+	}
 
-	final Vector velocity = target.clone().subtract(new Vector(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
-	velocity.multiply(speed / velocity.length());
-	velocity.setY(height > 0 ? height : (((player.getLocation().getPitch() * -1) + 90) / 35));
-	Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-		plugin.getPermissionsManager().allowFly(player);
-		player.setVelocity(velocity);
+	@EventHandler
+	public void onDamage(final EntityDamageEvent event) {
+		if (event.getEntity() == null) return;
+		if (!(event.getEntity() instanceof Player)) return;
+		Player player = (Player) event.getEntity();
+		if (jumpPadPlayers.get(player) != null)
+			event.setCancelled(true);
+	}
 
-		plugin.getCommunicationManager().showLaunch(player);
-		startFallImmunity(player);
-		player.getWorld().createExplosion(player.getLocation(), -1);
-	}, 0L);
+	@EventHandler
+	public void onFlightToggle(final PlayerToggleFlightEvent event) {
+		if (jumpPadPlayers.get(event.getPlayer()) != null)
+			event.setCancelled(true);
+	}
 
-	*/
+	@EventHandler
+	public void onTeleport(final PlayerTeleportEvent event) {
+		if (jumpPadPlayers.get(event.getPlayer()) != null) {
+			event.setCancelled(true);
+		}
+	}
 
 }
