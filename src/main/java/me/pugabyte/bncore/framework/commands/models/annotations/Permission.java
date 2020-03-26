55626008401b9e0c64cddf48b0b9df895605ff67
BuildTools <WakkaFlocka239@gmail.com>
2020-03-26 12:34:14 -0500

    permissions comment

--- src/main/java/me/pugabyte/bncore/framework/commands/models/annotations/Permission.java
+++ src/main/java/me/pugabyte/bncore/framework/commands/models/annotations/Permission.java
@@ -9,6 +9,36 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Permission {
 	String value();
-	boolean absolute() default false;
 
+	boolean absolute() default false;
 }
+
+/*
+	Groups:
+		group.admin			- admin+
+		group.seniorstaff	- operator+
+		group.moderator		- moderator+
+		group.builder		- builder/architect
+		group.staff			- staff + builders
+
+	Rank Exclusive:
+		rank.<rankname>
+
+	Ex: ladder.trusted = trusted and above
+	Rank Ladder:
+		ladder.guest 		- guest
+		ladder.member		- member+
+		ladder.trusted		- trusted+
+		ladder.elite		- elite+
+		ladder.veteran		- veteran+
+
+		ladder.builder		- builder & veteran+
+		ladder.architect	- architect+
+
+		ladder.moderator	- moderator & veteran+
+
+		ladder.operator		- operator & architect+ & moderator+
+		ladder.admin		- admin+
+		ladder.owner		- owner+
+
+ */
