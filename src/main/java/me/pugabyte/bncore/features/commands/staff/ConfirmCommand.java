f71cad1d3cfe7253e548601e57373c0ceb916e91
Blast <cjblanton2@gmail.com>
2020-03-26 04:31:38 -0400

    convert confirm and cancel

---
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/ConfirmCommand.java
@@ -0,0 +1,20 @@
+package me.pugabyte.bncore.features.commands.staff;
+
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+
+@Permission("group.staff")
+public class ConfirmCommand extends CustomCommand {
+
+	public ConfirmCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path
+	void run() {
+		runCommand("pr pv apply");
+	}
+
+}
