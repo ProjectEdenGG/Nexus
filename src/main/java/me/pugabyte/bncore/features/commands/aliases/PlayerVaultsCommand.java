0e74f049485abd5f1dda9557ddc04d6f8aa41f84
Pugabyte <pugabyte@bear-nation.net>
2020-03-28 12:16:39 -0400

    Bugfixes, convert vaults handler

---
+++ src/main/java/me/pugabyte/bncore/features/commands/aliases/PlayerVaultsCommand.java
@@ -0,0 +1,26 @@
+package me.pugabyte.bncore.features.commands.aliases;
+
+import lombok.NonNull;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
+import me.pugabyte.bncore.framework.commands.models.annotations.Fallback;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.utils.WorldGroup;
+
+@Fallback("playervaults")
+@Aliases({"pv", "chest", "vault"})
+public class PlayerVaultsCommand extends CustomCommand {
+
+	public PlayerVaultsCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	@Path
+	void run() {
+		if (WorldGroup.get(player().getWorld()) != WorldGroup.SURVIVAL && !player().hasPermission("group.seniorstaff"))
+			error("You can't open vaults here");
+		fallback();
+	}
+
+}
