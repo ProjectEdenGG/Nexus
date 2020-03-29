0b7bf54cde981eee930c439e057fa8bc7b44c73a
Pugabyte <pugabyte@projecteden.gg>
2020-03-29 00:12:59 -0400

    Add region tools command, fix hall of history listener

---
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/RegionToolsCommand.java
@@ -0,0 +1,25 @@
+package me.pugabyte.bncore.features.commands.staff;
+
+import lombok.NonNull;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.utils.WorldGuardUtils;
+
+@Permission("group.staff")
+public class RegionToolsCommand extends CustomCommand {
+	WorldGuardUtils wgUtils;
+
+	public RegionToolsCommand(@NonNull CommandEvent event) {
+		super(event);
+		wgUtils = new WorldGuardUtils(player().getWorld());
+	}
+
+	@Path("getRegionsLikeAt <filter>")
+	void getRegionsLikeAt(String filter) {
+		send(PREFIX + "Found regions:");
+		wgUtils.getRegionsLikeAt(player().getLocation(), filter).forEach(region -> send(region.getId()));
+	}
+
+}
