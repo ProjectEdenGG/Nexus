7c68516d528aebabe91cbacfaaf68c775fee403f
Blast <cjblanton2@gmail.com>
2020-03-30 00:31:57 -0400

    murder work

--- src/main/java/me/pugabyte/bncore/features/minigames/models/matchdata/MurderMatchData.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/models/matchdata/MurderMatchData.java
@@ -1,18 +1,14 @@
 package me.pugabyte.bncore.features.minigames.models.matchdata;
 
 import lombok.Data;
-import me.pugabyte.bncore.features.minigames.mechanics.Battleship;
+import me.pugabyte.bncore.features.minigames.mechanics.Murder;
 import me.pugabyte.bncore.features.minigames.models.Match;
 import me.pugabyte.bncore.features.minigames.models.MatchData;
 import me.pugabyte.bncore.features.minigames.models.annotations.MatchDataFor;
-import org.bukkit.entity.Player;
-
-import java.util.HashMap;
 
 @Data
-@MatchDataFor(Battleship.class)
+@MatchDataFor(Murder.class)
 public class MurderMatchData extends MatchData {
-	private static HashMap<Player, Integer> locators = new HashMap<>();
 
 	public MurderMatchData(Match match) {
 		super(match);
