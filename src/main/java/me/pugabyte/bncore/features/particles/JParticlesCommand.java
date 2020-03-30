b513e05b66192594d7709e41f479663a566253b6
BuildTools <WakkaFlocka239@gmail.com>
2020-03-30 09:59:49 -0500

    add VectorUtils for particles and misc changes

--- src/main/java/me/pugabyte/bncore/features/particles/JParticlesCommand.java
+++ src/main/java/me/pugabyte/bncore/features/particles/JParticlesCommand.java
@@ -15,8 +15,8 @@ import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.utils.Tasks;
 import me.pugabyte.bncore.utils.Utils;
 import org.bukkit.Location;
-import org.bukkit.Particle;
 import org.bukkit.event.Listener;
+import org.bukkit.util.Vector;
 
 @NoArgsConstructor
 @Permission("group.admin")
@@ -31,18 +31,46 @@ public class JParticlesCommand extends CustomCommand implements Listener {
 		LineEffect.builder().player(player()).distance(distance).density(density).rainbow(true).build();
 	}
 
-	@Path("circle [radius] [density]")
-	void circle(@Arg("1.0") double radius, @Arg("20") int density) {
-		CircleEffect.builder().player(player()).location(player().getLocation()).density(10).radius(0.333).ticks(20 * 20).whole(true).rainbow(true).updateLoc(true).build();
-		Tasks.wait(20, () -> CircleEffect.builder().player(player()).location(player().getLocation()).density(20).radius(0.666).ticks(20 * 20).whole(true).rainbow(true).updateLoc(true).build());
-		Tasks.wait(40, () -> CircleEffect.builder().player(player()).location(player().getLocation()).density(40).radius(0.999).ticks(20 * 20).whole(true).rainbow(true).updateLoc(true).build());
-		Tasks.wait(60, () -> CircleEffect.builder().player(player()).location(player().getLocation()).density(60).radius(1.333).ticks(20 * 20).whole(true).rainbow(true).updateLoc(true).build());
+	@Path("circles")
+	void circles() {
+		CircleEffect.builder().player(player()).location(player().getLocation()).density(10).radius(0.333).ticks(20 * 20).whole(true).updateLoc(true).rainbow(true).build();
+		Tasks.wait(20, () -> CircleEffect.builder().player(player()).location(player().getLocation()).density(20).radius(0.666).ticks(20 * 20).whole(true).updateLoc(true).rainbow(true).build());
+		Tasks.wait(40, () -> CircleEffect.builder().player(player()).location(player().getLocation()).density(40).radius(0.999).ticks(20 * 20).whole(true).updateLoc(true).rainbow(true).build());
+		Tasks.wait(60, () -> CircleEffect.builder().player(player()).location(player().getLocation()).density(60).radius(1.333).ticks(20 * 20).whole(true).updateLoc(true).rainbow(true).build());
+	}
+
+	@Path("circle")
+	void circle() {
+		Vector vector = new Vector(0, 1, 0);
+		Location loc = player().getLocation().add(vector);
+		CircleEffect.builder().player(player()).location(loc).updateVector(vector).density(100).radius(1.5).ticks(20 * 20).rainbow(true).build();
+	}
+
+	@Path("halo")
+	void halo() {
+		Vector vector = new Vector(0, 2.1, 0);
+		Location loc = player().getLocation().add(vector);
+		CircleEffect.builder().player(player()).location(loc).updateVector(vector).density(20).radius(0.5).ticks(20 * 20).rainbow(true).build();
+	}
+
+	@Path("slowsphere")
+	void slowsphere() {
+		Vector vector = new Vector(0, 1.5, 0);
+		Location loc = player().getLocation().add(vector);
+		CircleEffect.builder().player(player()).location(loc).updateVector(vector).density(100).radius(1.5).ticks(20 * 20).randomRotation(true).rainbow(true).build();
+	}
+
+	@Path("fastsphere")
+	void fastsphere() {
+		Vector vector = new Vector(0, 1.5, 0);
+		Location loc = player().getLocation().add(vector);
+		CircleEffect.builder().player(player()).location(loc).updateVector(vector).density(100).radius(1.5).ticks(20 * 20).randomRotation(true).rainbow(true).fast(true).build();
 	}
 
 	@Path("dot")
 	void dot() {
-		Location loc = Utils.getCenteredLocation(player().getLocation()).add(0, 1.5, 0);
-		DotEffect.builder().player(player()).loc(loc).particle(Particle.REDSTONE).ticks(10 * 20).rainbow(true).build();
+		Location loc = Utils.getCenteredLocation(player().getLocation()).add(0, 1, 0);
+		DotEffect.builder().player(player()).loc(loc).ticks(10 * 20).rainbow(true).build();
 	}
 
 	@Path("band")
