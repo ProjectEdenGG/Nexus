23bb7c4b58a0735a7dbb78ec8e00e18f997279b7
Pugabyte <pugabyte@projecteden.gg>
2020-04-01 00:21:33 -0400

    Add back /mcmmo reset

--- src/main/java/me/pugabyte/bncore/features/mcmmo/McMMOResetCommand.java
+++ src/main/java/me/pugabyte/bncore/features/mcmmo/McMMOResetCommand.java
@@ -3,9 +3,11 @@ package me.pugabyte.bncore.features.mcmmo;
 import me.pugabyte.bncore.features.mcmmo.menus.McMMOResetMenu;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Redirects.Redirect;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.utils.WorldGroup;
 
+@Redirect(from = "/mcmmo reset", to = "/mcmmoreset")
 public class McMMOResetCommand extends CustomCommand {
 	public McMMOResetCommand(CommandEvent event) {
 		super(event);
