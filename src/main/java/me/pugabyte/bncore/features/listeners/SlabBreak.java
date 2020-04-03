1c695a4b36411e0001ea1aded4ec0d7c263705a1
Blast <cjblanton2@gmail.com>
2020-04-03 10:10:29 -0400

    cleanup slabbreak

--- src/main/java/me/pugabyte/bncore/features/listeners/SlabBreak.java
+++ src/main/java/me/pugabyte/bncore/features/listeners/SlabBreak.java
@@ -13,43 +13,41 @@ public class SlabBreak implements Listener {
 
 	@EventHandler
 	public void onSlabBreak(BlockBreakEvent event) {
-		if (event.isCancelled()) {
-			return;
-		}
-		if (!event.getPlayer().hasPermission("group.staff")) {
-			return;
-		}
+		if (event.isCancelled()) return;
+		if (!event.getPlayer().hasPermission("group.staff")) return;
 		if (isDoubleSlab(event.getBlock())) {
-			event.setCancelled(true);
+			if (event.getPlayer().getInventory().getItemInMainHand().getType().name().contains("slab")) {
+				event.setCancelled(true);
 
-			Vector direction = event.getPlayer().getLocation().getDirection();
-			Vector blockVector = null;
-			for (double d = 1; d < 16; d += .06) {
-				Vector multiplied = direction.clone().multiply(d).add(new Vector(0, event.getPlayer().getEyeHeight(), 0)).add(event.getPlayer().getLocation().toVector());
-				Location multipliedLocation = multiplied.toLocation(event.getPlayer().getWorld());
-				if (multipliedLocation.getBlock().getType() == event.getBlock().getType()) {
-					blockVector = multiplied;
-					break;
+				Vector direction = event.getPlayer().getLocation().getDirection();
+				Vector blockVector = null;
+				for (double d = 1; d < 16; d += .06) {
+					Vector multiplied = direction.clone().multiply(d).add(new Vector(0, event.getPlayer().getEyeHeight(), 0)).add(event.getPlayer().getLocation().toVector());
+					Location multipliedLocation = multiplied.toLocation(event.getPlayer().getWorld());
+					if (multipliedLocation.getBlock().getType() == event.getBlock().getType()) {
+						blockVector = multiplied;
+						break;
+					}
 				}
-			}
 
-			if (blockVector != null) {
-				double blockY = blockVector.getY() - ((int) blockVector.getY());
-				if (blockY > .5) {
+				if (blockVector != null) {
+					double blockY = blockVector.getY() - ((int) blockVector.getY());
+					if (blockY > .5) {
+						event.getBlock().setTypeIdAndData(event.getBlock().getTypeId() + 1, event.getBlock().getData(), true);
+						if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
+							event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType(), 1, event.getBlock().getData()));
+						}
+					} else {
+						event.getBlock().setTypeIdAndData(event.getBlock().getTypeId() + 1, (byte) (event.getBlock().getData() + 8), true);
+						if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
+							event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType(), 1, (byte) (event.getBlock().getData() & 7)));
+						}
+					}
+				} else {
 					event.getBlock().setTypeIdAndData(event.getBlock().getTypeId() + 1, event.getBlock().getData(), true);
 					if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
 						event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType(), 1, event.getBlock().getData()));
 					}
-				} else {
-					event.getBlock().setTypeIdAndData(event.getBlock().getTypeId() + 1, (byte) (event.getBlock().getData() + 8), true);
-					if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
-						event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType(), 1, (byte) (event.getBlock().getData() & 7)));
-					}
-				}
-			} else {
-				event.getBlock().setTypeIdAndData(event.getBlock().getTypeId() + 1, event.getBlock().getData(), true);
-				if (event.getPlayer().getGameMode() == GameMode.SURVIVAL) {
-					event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType(), 1, event.getBlock().getData()));
 				}
 			}
 		}
