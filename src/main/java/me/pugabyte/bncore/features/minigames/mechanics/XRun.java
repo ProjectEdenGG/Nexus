74225bf471611da4c2e6d9439560bcf8656c5030
Pugabyte <pugabyte@projecteden.gg>
2020-04-04 11:32:43 -0400

    Hide death messages on parkour/xrun

--- src/main/java/me/pugabyte/bncore/features/minigames/mechanics/XRun.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/mechanics/XRun.java
@@ -1,6 +1,7 @@
 package me.pugabyte.bncore.features.minigames.mechanics;
 
 import me.pugabyte.bncore.features.minigames.mechanics.common.CheckpointMechanic;
+import me.pugabyte.bncore.features.minigames.models.events.matches.minigamers.MinigamerDeathEvent;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,6 +20,12 @@ public class XRun extends CheckpointMechanic {
 	@Override
 	public ItemStack getMenuItem() {
 		return new ItemStack(Material.SUGAR);
+	}
+
+	@Override
+	public void onDeath(MinigamerDeathEvent event) {
+		event.setDeathMessage(null);
+		super.onDeath(event);
 	}
 
 }
