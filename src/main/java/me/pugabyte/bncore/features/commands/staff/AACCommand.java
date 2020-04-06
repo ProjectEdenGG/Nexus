8022e58f39e3568b4bb72c0b8d25374215be35b0
BuildTools <WakkaFlocka239@gmail.com>
2020-04-06 15:21:23 -0500

    add /aac notify command

---
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/AACCommand.java
@@ -0,0 +1,22 @@
+package me.pugabyte.bncore.features.commands.staff;
+
+import me.pugabyte.bncore.features.chat.Chat;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Fallback;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+
+@Fallback("aac")
+@Permission("group.admin")
+public class AACCommand extends CustomCommand {
+	public AACCommand(CommandEvent event) {
+		super(event);
+	}
+
+	@Path("notify <message...>")
+	void notify(@Arg String message) {
+		Chat.broadcast(message, "staff");
+	}
+}
