4a41be4591caa46534cecd6ac8e0d06a7e6bc13e
Pugabyte <pugabyte@bear-nation.net>
2020-04-07 17:29:35 -0400

    Prevent t/command cause i cant type

--- src/main/java/me/pugabyte/bncore/features/chat/ChatListener.java
+++ src/main/java/me/pugabyte/bncore/features/chat/ChatListener.java
@@ -2,6 +2,7 @@ package me.pugabyte.bncore.features.chat;
 
 import lombok.NoArgsConstructor;
 import me.pugabyte.bncore.features.chat.events.ChatEvent;
+import me.pugabyte.bncore.framework.commands.Commands;
 import me.pugabyte.bncore.models.chat.ChatService;
 import me.pugabyte.bncore.models.chat.Chatter;
 import me.pugabyte.bncore.utils.Tasks;
@@ -11,13 +12,24 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.regex.Pattern;
+
+import static me.pugabyte.bncore.utils.StringUtils.right;
+import static me.pugabyte.bncore.utils.Utils.runCommand;
+
 @NoArgsConstructor
 public class ChatListener implements Listener {
 
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent event) {
 		Chatter chatter = new ChatService().get(event.getPlayer());
-		Tasks.sync(() -> chatter.say(event.getMessage()));
+		Tasks.sync(() -> {
+			// Prevents "t/command"
+			if (Pattern.compile("^t" + Commands.getCommandPattern() + ".*").matcher(event.getMessage()).matches())
+				runCommand(event.getPlayer(), right(event.getMessage(), event.getMessage().length() - 2));
+			else
+				chatter.say(event.getMessage());
+		});
 		event.setCancelled(true);
 	}
 
