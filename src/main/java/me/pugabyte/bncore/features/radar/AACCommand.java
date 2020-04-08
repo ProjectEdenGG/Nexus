e8b0fb9a0fb54ea8bd2394e890d60484b381a0e1
BuildTools <WakkaFlocka239@gmail.com>
2020-04-08 16:09:12 -0500

    misc changes

--- src/main/java/me/pugabyte/bncore/features/radar/AACCommand.java
+++ src/main/java/me/pugabyte/bncore/features/radar/AACCommand.java
@@ -1,4 +1,4 @@
-package me.pugabyte.bncore.features.commands.staff;
+package me.pugabyte.bncore.features.radar;
 
 import me.pugabyte.bncore.features.chat.Chat;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
@@ -6,6 +6,8 @@ import me.pugabyte.bncore.framework.commands.models.annotations.Fallback;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.utils.WorldGroup;
+import org.bukkit.entity.Player;
 
 @Fallback("aac")
 @Permission("group.admin")
@@ -14,8 +16,11 @@ public class AACCommand extends CustomCommand {
 		super(event);
 	}
 
-	@Path("notify <message...>")
-	void notify(String message) {
+	@Path("notify <player> <message...>")
+	void notify(Player player, String reason) {
+		String name = player.getName();
+		String worldGroup = WorldGroup.get(player).name();
+		String message = name + " " + reason.replace("{worldgroup}", worldGroup);
 		Chat.broadcast(message, "staff");
 	}
 }
