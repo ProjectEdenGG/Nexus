c7b0ff19581b25acd561eb849635cce66d97362c
Pugabyte <pugabyte@bear-nation.net>
2020-04-09 12:10:38 -0400

    Exchanges working

--- src/main/java/me/pugabyte/bncore/features/shops/providers/PlayerShopProvider.java
+++ src/main/java/me/pugabyte/bncore/features/shops/providers/PlayerShopProvider.java
@@ -3,9 +3,8 @@ package me.pugabyte.bncore.features.shops.providers;
 import fr.minuskube.inv.ClickableItem;
 import fr.minuskube.inv.SmartInventory;
 import fr.minuskube.inv.content.InventoryContents;
-import me.pugabyte.bncore.BNCore;
 import me.pugabyte.bncore.models.shop.Shop;
-import me.pugabyte.bncore.models.shop.Shop.ShopItem;
+import me.pugabyte.bncore.models.shop.Shop.Product;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -34,12 +33,15 @@ public class PlayerShopProvider extends _ShopProvider {
 		super.init(player, contents);
 
 		int index = 9;
-		for (ShopItem shopItem : shop.getItems()) {
-			ItemStack item = shopItem.getItem().clone();
+		for (Product product : shop.getItems()) {
+			ItemStack item = product.getItem().clone();
 			// lore and stuff
 			contents.set(index++, ClickableItem.from(item, e -> {
-				BNCore.log("Class of exchange: " + shopItem.getExchange().getClass().getSimpleName());
-				shopItem.getExchange().process(shopItem, player);
+				try {
+					product.getExchange().process(product, player);
+				} catch (Exception ex) {
+					player.sendMessage(colorize(ex.getMessage()));
+				}
 			}));
 		}
 	}
--- src/main/java/me/pugabyte/bncore/models/shop/Shop.java
+++ src/main/java/me/pugabyte/bncore/models/shop/Shop.java
@@ -1,6 +1,7 @@
 package me.pugabyte.bncore.models.shop;
 
 import dev.morphia.annotations.Converters;
+import dev.morphia.annotations.Embedded;
 import dev.morphia.annotations.Entity;
 import dev.morphia.annotations.Id;
 import lombok.AllArgsConstructor;
@@ -10,15 +11,20 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import me.pugabyte.bncore.BNCore;
+import me.pugabyte.bncore.framework.exceptions.postconfigured.InvalidInputException;
 import me.pugabyte.bncore.framework.persistence.serializer.mongodb.ItemStackConverter;
 import me.pugabyte.bncore.framework.persistence.serializer.mongodb.UUIDConverter;
 import me.pugabyte.bncore.models.PlayerOwnedObject;
+import me.pugabyte.bncore.utils.Utils;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static me.pugabyte.bncore.utils.StringUtils.colorize;
 
 @Data
 @Builder
@@ -32,15 +38,18 @@ public class Shop extends PlayerOwnedObject {
 	@NonNull
 	private UUID uuid;
 	private String description;
-	private List<ShopItem> items = new ArrayList<>();
+	@Embedded
+	private List<Product> items = new ArrayList<>();
+	@Embedded
+	private List<ItemStack> holding = new ArrayList<>();
 
 	@Data
 	@NoArgsConstructor
 	@AllArgsConstructor
-	public static class ShopItem {
+	public static class Product {
 		private UUID uuid;
 		private ItemStack item;
-		private int stock;
+		private double stock;
 		private Exchange exchange;
 
 		public Shop getShop() {
@@ -52,47 +61,106 @@ public class Shop extends PlayerOwnedObject {
 
 		<T> T getPrice();
 
-		void process(ShopItem item, OfflinePlayer customer);
+		void process(Product product, Player customer);
 
 	}
 
 	@Data
+	@Builder
 	@NoArgsConstructor
 	@AllArgsConstructor
+	// Customer buying an item from the shop owner for money
+	public static class ItemForMoneyExchange implements Exchange {
+		@NonNull
+		private Double price;
+
+		@Override
+		public void process(Product product, Player customer) {
+			BNCore.log("Processing ItemForMoneyExchange");
+			if (product.getStock() == 0)
+				throw new InvalidInputException("This item is out of stock");
+			if (!BNCore.getEcon().has(customer, price))
+				throw new InvalidInputException("You do not have enough money to purchase this item");
+
+			BNCore.getEcon().withdrawPlayer(customer, price);
+			BNCore.getEcon().depositPlayer(product.getShop().getOfflinePlayer(), price);
+			giveItem(customer, product.getItem());
+			product.setStock(product.getStock() - product.getItem().getAmount());
+			new ShopService().save(product.getShop());
+			customer.sendMessage(colorize("You purchased " + product.getItem().getType() + " x" + product.getItem().getAmount() + " for $" + price));
+		}
+	}
+
+	@Data
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	// Customer buying an item from the shop owner for other items
 	public static class ItemForItemExchange implements Exchange {
 		@NonNull
 		private ItemStack price;
 
 		@Override
-		public void process(ShopItem item, OfflinePlayer customer) {
+		public void process(Product product, Player customer) {
 			BNCore.log("Processing ItemForItemExchange");
+			if (product.getStock() == 0)
+				throw new InvalidInputException("This item is out of stock");
+			if (product.getStock() < product.getItem().getAmount())
+				throw new InvalidInputException("There is not enough stock to fulfill your purchase");
+			if (!customer.getInventory().contains(price))
+				throw new InvalidInputException("You do not have enough " + pretty(price) + " to purchase this item");
+
+			customer.getInventory().remove(price);
+			giveItem(product.getShop().getOfflinePlayer(), price);
+			giveItem(customer, product.getItem());
+			product.setStock(product.getStock() - product.getItem().getAmount());
+			new ShopService().save(product.getShop());
+			customer.sendMessage(colorize("You purchased " + pretty(product.getItem()) + " for " + pretty(price)));
 		}
 	}
 
 	@Data
+	@Builder
 	@NoArgsConstructor
 	@AllArgsConstructor
-	public static class ItemForMoneyExchange implements Exchange {
-		@NonNull
-		private Integer price;
-
-		@Override
-		public void process(ShopItem item, OfflinePlayer customer) {
-			BNCore.log("Processing ItemForMoneyExchange");
-		}
-	}
-
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
+	// Customer selling an item to the shop owner for money
 	public static class MoneyForItemExchange implements Exchange {
 		@NonNull
-		private ItemStack price;
+		private Double price;
 
 		@Override
-		public void process(ShopItem item, OfflinePlayer customer) {
-			BNCore.log("Processing MoneyForItemExchange");
+		public void process(Product product, Player customer) {
+			BNCore.log("Processing ItemForMoneyExchange");
+			OfflinePlayer shopOwner = product.getShop().getOfflinePlayer();
+			if (product.getStock() == 0)
+				throw new InvalidInputException("This item is out of stock");
+			if (product.getStock() > 0 && product.getStock() < price)
+				throw new InvalidInputException("There is not enough stock to fulfill your purchase");
+			if (!BNCore.getEcon().has(shopOwner, price))
+				throw new InvalidInputException(shopOwner.getName() + " does not have enough money to purchase this item from you");
+			if (!customer.getInventory().contains(product.getItem()))
+				throw new InvalidInputException("You do not have enough " + pretty(product.getItem()) + " to sell");
+
+			BNCore.getEcon().withdrawPlayer(shopOwner, price);
+			BNCore.getEcon().depositPlayer(customer, price);
+			giveItem(shopOwner, product.getItem());
+			customer.getInventory().remove(product.getItem());
+			product.setStock(product.getStock() - price);
+			new ShopService().save(product.getShop());
+			customer.sendMessage(colorize("You sold " + product.getItem().getType() + " x" + product.getItem().getAmount() + " for $" + price));
 		}
+	}
+
+	// TODO move
+	public static void giveItem(OfflinePlayer player, ItemStack item) {
+		if (player.isOnline())
+			Utils.giveItem(player.getPlayer(), item);
+		else
+			((Shop) new ShopService().get(player)).getHolding().add(item);
+	}
+
+	private static String pretty(ItemStack price) {
+		return price.getType() + " x" + price.getAmount();
 	}
 
 }
