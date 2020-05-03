c8b8e742b618492fee7dde19be74d4f6d1718fb7
Shannontc <shannongaming10@gmail.com>
2020-05-03 04:51:32 -0500

    Update SWS/SWL, add BlockUtils, and add LOGS and WOOD to MaterialTag.

--- src/main/java/me/pugabyte/bncore/features/commands/SidewaysStairsCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/SidewaysStairsCommand.java
@@ -1,12 +1,16 @@
 package me.pugabyte.bncore.features.commands;
 
+import com.sk89q.worldedit.registry.state.DirectionalProperty;
+import com.sk89q.worldedit.util.Direction;
 import lombok.NoArgsConstructor;
+import me.pugabyte.bncore.features.particles.effects.DiscoEffect;
 import me.pugabyte.bncore.framework.annotations.Disabled;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
-import me.pugabyte.bncore.framework.exceptions.postconfigured.InvalidInputException;
+import me.pugabyte.bncore.utils.BlockUtils;
+import me.pugabyte.bncore.utils.MaterialTag;
 import me.pugabyte.bncore.utils.StringUtils;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -17,19 +21,19 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Camaros
  */
 
-@Disabled
 @Aliases("sws")
 @NoArgsConstructor
 public class SidewaysStairsCommand extends CustomCommand implements Listener {
 	public static final String PREFIX = StringUtils.getPrefix("SidewaysStairs");
 	static Map<Player, SidewaysStairsPlayer> playerData = new HashMap<>();
+
+	private static final List<String> validAngles = (List<String>)Arrays.asList("north",  "east", "south", "west");
 
 	private SidewaysStairsPlayer swsPlayer;
 
@@ -45,10 +49,11 @@ public class SidewaysStairsCommand extends CustomCommand implements Listener {
 	void help() {
 		send(PREFIX + "Commands:");
 		send("&c/sws toggle &7- Turn SWS on or off");
-		send("&c/sws angle <number> &7- Set the angle of the stairs to be placed (0-7)");
-		send("&c/sws rotate &7- Rotate the stair angle");
-		send("&c/sws copy &7- Copy the angle of an existing stair");
-		send("&c/sws upsidedown [true|false] &7- Toggle upsidedown stairs");
+		send("&c/sws set <north|south|east|west> &7- Set the direction of the stairs to be placed.");
+		send("&c/sws setupsidedown <true|false> &7- Toggle upsidedown stairs in set placement mode.");
+		send("&c/sws rotate &7- Rotate the stair angle.");
+		send("&c/sws copy &7- Copy the angle of an existing stair.");
+		send("&c/sws upsidedown [true|false] &7- Toggle whether stairs can be placed upsidedown or not.");
 	}
 
 	@Path("toggle")
@@ -62,41 +67,67 @@ public class SidewaysStairsCommand extends CustomCommand implements Listener {
 		send(PREFIX + (swsPlayer.isEnabled() ? "Enabled" : "Disabled"));
 	}
 
-	@Path("(set|angle|setangle)")
+	@Path("(set|angle|setangle)") //this method isn't called when no argument is given.
 	void setAngle() {
-		send(PREFIX + "/sws angle <number> &7- Set the angle of the stairs to be placed (0-7)");
+		send(PREFIX + "/sws angle <north/south/east/west> -&7 Set the angle to place stair blocks.");
 	}
 
-	@Path("(set|angle|setangle) <angle>")
-	void setAngle(byte angle) {
-		swsPlayer.trySetAngle(angle);
-		swsPlayer.setAction("set_angle");
+	@Path("(set|angle|setangle) <north|south|east|west>")
+	void setAngle(String angle) {
+		if(validAngles.contains(angle.toLowerCase())) {
+			swsPlayer.setEnabled(true);
+			swsPlayer.setAction(SwsAction.SET_ANGLE);
+			swsPlayer.setDirection(angle.toLowerCase());
+			send(PREFIX + "Angle successfully set to " + angle);
+		} else
+			send(PREFIX + "Invalid angle. Angle must be North, South, East, or West");
+	}
+
+	@Path("(setupsidedown)") //this doesn't work either. I'd like this to be called if no argument is provided.
+	void setUpsidedown() {
+		setUpsidedown(swsPlayer.getHalf().equals("top") ? false : true);
+	}
+
+	@Path("(setupsidedown) <true/false>")
+	void setUpsidedown(Boolean value){
 		swsPlayer.setEnabled(true);
-		send(PREFIX + "Angle successfully set to " + angle);
+		swsPlayer.setAction(SwsAction.SET_ANGLE);
+		swsPlayer.setHalf(value ? "top" : "bottom");
+		send(PREFIX + String.format("Upsidedown stairs has now been %s.", value ? "enabled" : "disabled"));
 	}
 
 	@Path("copy")
 	void copy() {
-		swsPlayer.setAction("copy");
+		swsPlayer.setEnabled(true);
+		swsPlayer.setAction(SwsAction.COPY);
 		send(PREFIX + "Right click a stair block to copy its angle.");
 	}
 
 	@Path("rotate")
 	void rotate() {
 		swsPlayer.setEnabled(true);
-		send(PREFIX + "Angle changed to " + swsPlayer.rotate());
+		swsPlayer.setAction(SwsAction.SET_ANGLE);
+		Iterator i = validAngles.iterator();
+		while(i.hasNext())
+			if(((String)i.next()).equals(swsPlayer.direction))
+				if(i.hasNext())
+					swsPlayer.setDirection((String)i.next());
+				else
+					swsPlayer.setDirection(validAngles.get(0));
+		send(PREFIX + String.format("Angle changed to %s.", swsPlayer.getDirection()));
 	}
 
 	@Path("upsidedown")
 	void upsideDown() {
-		upsideDown(swsPlayer.getAction().equals("disable_upsidedown_placement"));
+		swsPlayer.setEnabled(true);
+		upsideDown(swsPlayer.getAction() == SwsAction.DISABLE_UPSIDEDOWN_PLACEMENT);
 	}
 
 	@Path("upsidedown <true|false>")
 	void upsideDown(boolean allow) {
-		swsPlayer.setAction(allow ? "" : "disable_upsidedown_placement");
-		swsPlayer.setEnabled(!allow);
-		send(PREFIX + "Upsidedown stair placement " + (allow ? "enabled" : "disabled."));
+		swsPlayer.setEnabled(true);
+		swsPlayer.setAction(allow ? SwsAction.NONE : SwsAction.DISABLE_UPSIDEDOWN_PLACEMENT);
+		send(PREFIX + String.format("Upsidedown stair placement  %s.",  allow ? "enabled" : "disabled"));
 	}
 
 	@EventHandler
@@ -107,19 +138,23 @@ public class SidewaysStairsCommand extends CustomCommand implements Listener {
 		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
 
 			Block block = event.getClickedBlock();
-			String blockName = block.getType().toString();
 			Player player = event.getPlayer();
 			SidewaysStairsPlayer swsPlayer = playerData.get(player);
 
+
 			if (swsPlayer != null && swsPlayer.isEnabled())
-				if (swsPlayer.getAction().equals("copy"))
-					if (blockName.toLowerCase().endsWith("stairs")) {
-						swsPlayer.setAction("set_angle");
-						swsPlayer.setAngle(block.getData());
+				if (swsPlayer.getAction() == SwsAction.COPY)
+					if (MaterialTag.STAIRS.isTagged(block.getType())) {
+						swsPlayer.setAction(SwsAction.SET_ANGLE);
+						String direction = BlockUtils.getBlockProperty(block, "facing");
+						String half = BlockUtils.getBlockProperty(block, "half");
+						swsPlayer.setDirection(direction);
+						swsPlayer.setHalf(half);
 						swsPlayer.setEnabled(true);
-						player.sendMessage(PREFIX + "Angle succesfully copied (" + block.getData() + ")");
-					} else
+						player.sendMessage(PREFIX + "Angle succesfully copied (" + direction + (!half.equals("bottom") ? "/upsidedown" : "") + ").");
+					} else {
 						player.sendMessage(PREFIX + "Can only copy angle of a stair block.");
+					}
 		}
 	}
 
@@ -127,24 +162,31 @@ public class SidewaysStairsCommand extends CustomCommand implements Listener {
 	public void onStairPlace(BlockPlaceEvent event) {
 		Player player = event.getPlayer();
 		Block block = event.getBlock();
-		String blockName = block.getType().toString();
 		SidewaysStairsPlayer swsPlayer = playerData.get(player);
 
-		// TODO 1.13ify
-//		if (swsPlayer != null && swsPlayer.isEnabled())
-//			if (swsPlayer.getAction().equals("set_angle"))
-//				if (blockName.toLowerCase().endsWith("stairs"))
-//					block.setData(swsPlayer.getAngle());
-//			else if (swsPlayer.getAction().equals("disable_upsidedown_placement"))
-//				if ((int) (block.getData()) > 3)
-//					block.setData((byte) (block.getData() - 4));
+		if (MaterialTag.STAIRS.isTagged(block.getType()))
+			if (swsPlayer != null && swsPlayer.isEnabled())
+				if (swsPlayer.getAction() == SwsAction.SET_ANGLE) {
+					BlockUtils.updateBlockProperty(block, "facing", swsPlayer.direction);
+					BlockUtils.updateBlockProperty(block, "half", swsPlayer.half);
+				} else if (swsPlayer.getAction() == SwsAction.DISABLE_UPSIDEDOWN_PLACEMENT) {
+					BlockUtils.updateBlockProperty(block, "half", "bottom");
+				}
+	}
+
+	enum SwsAction{
+		NONE, COPY, SET_ANGLE, DISABLE_UPSIDEDOWN_PLACEMENT;
 	}
 
 	public class SidewaysStairsPlayer {
+
 		private final Player player;
 		private boolean enabled = false;
-		private String action = "";
-		private byte angle = 0;
+		private SwsAction action = SwsAction.NONE;
+		private String direction = "north";
+		private String half = "bottom";
+
+
 
 		public SidewaysStairsPlayer(Player player) {
 			this.player = player;
@@ -162,32 +204,28 @@ public class SidewaysStairsCommand extends CustomCommand implements Listener {
 			this.enabled = enabled;
 		}
 
-		public String getAction() {
+		public SwsAction getAction() {
 			return action;
 		}
 
-		public void setAction(String action) {
+		public void setAction(SwsAction action) {
 			this.action = action;
 		}
 
-		public byte getAngle() {
-			return angle;
+		public String getDirection() {
+			return direction;
 		}
 
-		public void setAngle(byte angle) {
-			this.angle = angle;
+		public void setDirection(String direction) {
+			this.direction = direction;
 		}
 
-		public void trySetAngle(byte angle) {
-			if (angle > 7 || angle < 0)
-				throw new InvalidInputException("Invalid angle (Must be a number between 0-7)");
-
-			this.angle = angle;
+		public String getHalf() {
+			return half;
 		}
 
-		public int rotate() {
-			this.angle = (byte) ((angle + 1) % 8);
-			return angle;
+		public void setHalf(String half) {
+			this.half = half;
 		}
 
 	}
