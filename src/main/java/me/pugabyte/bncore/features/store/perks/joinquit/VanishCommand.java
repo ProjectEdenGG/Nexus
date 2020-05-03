f2fde5f1e0c0ad0a197216b14aef5ec66adb6f8c
BuildTools <WakkaFlocka239@gmail.com>
2020-05-03 17:19:38 -0500

    add toggleable interaction and pickup commands to vanish

--- src/main/java/me/pugabyte/bncore/features/store/perks/joinquit/VanishCommand.java
+++ src/main/java/me/pugabyte/bncore/features/store/perks/joinquit/VanishCommand.java
@@ -1,6 +1,7 @@
 package me.pugabyte.bncore.features.store.perks.joinquit;
 
 import lombok.NonNull;
+import me.pugabyte.bncore.BNCore;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Fallback;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
@@ -12,6 +13,8 @@ import me.pugabyte.bncore.utils.Utils;
 @Fallback("premiumvanish")
 @Redirect(from = {"/fj", "/fakejoin"}, to = "/vanish fj")
 @Redirect(from = {"/fq", "/fakequit"}, to = "/vanish fq")
+@Redirect(from = {"/ni", "/nointeract"}, to = "/vanish ni")
+@Redirect(from = {"/np", "/nopickup"}, to = "/vanish np")
 public class VanishCommand extends CustomCommand {
 
 	public VanishCommand(@NonNull CommandEvent event) {
@@ -31,6 +34,40 @@ public class VanishCommand extends CustomCommand {
 	void fakeQuit() {
 		runCommand("vanish on");
 		JoinQuit.quit(player());
+	}
+
+	@Path("(ni|nointeract)")
+	@Permission("vanish.use")
+	void toggleInteract() {
+		if (player().hasPermission("pv.interact")) {
+			BNCore.getPerms().playerRemove(player(), "pv.interact");
+			BNCore.getPerms().playerRemove(player(), "pv.useblocks");
+			BNCore.getPerms().playerRemove(player(), "pv.damage");
+			BNCore.getPerms().playerRemove(player(), "pv.breakblocks");
+			BNCore.getPerms().playerRemove(player(), "pv.placeblocks");
+			BNCore.getPerms().playerRemove(player(), "pv.dropitems");
+			send("interaction enabled");
+		} else {
+			BNCore.getPerms().playerAdd(player(), "pv.interact");
+			BNCore.getPerms().playerAdd(player(), "pv.useblocks");
+			BNCore.getPerms().playerAdd(player(), "pv.damage");
+			BNCore.getPerms().playerAdd(player(), "pv.breakblocks");
+			BNCore.getPerms().playerAdd(player(), "pv.placeblocks");
+			BNCore.getPerms().playerAdd(player(), "pv.dropitems");
+			send("interaction disabled");
+		}
+	}
+
+	@Path("(np|nopickup)")
+	@Permission("vanish.use")
+	void togglePickup() {
+		if (player().hasPermission("pv.interact")) {
+			BNCore.getPerms().playerRemove(player(), "pv.toggleitems");
+			send("pickup disabled");
+		} else {
+			BNCore.getPerms().playerAdd(player(), "pv.toggleitems");
+			send("pickup enabled");
+		}
 	}
 
 }
