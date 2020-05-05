7974c7da81ce23ec0aa9636b8231ac720eb7055b
Pugabyte <pugabyte@bear-nation.net>
2020-05-05 14:32:58 -0400

    Add crossbow to iron tools in mcmmo (remove in 2.2)

--- src/main/java/me/pugabyte/bncore/features/mcmmo/McMMO.java
+++ src/main/java/me/pugabyte/bncore/features/mcmmo/McMMO.java
@@ -1,8 +1,31 @@
 package me.pugabyte.bncore.features.mcmmo;
 
+import com.gmail.nossr50.mcMMO;
+import com.gmail.nossr50.util.MaterialMapStore;
+import me.pugabyte.bncore.BNCore;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+
 public class McMMO {
 	public McMMO() {
 		new McMMOListener();
+
+		// Remove when updating to mcmmo 2.2
+		addCrossbowToIronTools();
+	}
+
+	private void addCrossbowToIronTools() {
+		try {
+			MaterialMapStore materialMapStore = mcMMO.getMaterialMapStore();
+			Field field = materialMapStore.getClass().getDeclaredField("ironTools");
+			field.setAccessible(true);
+			HashSet<String> ironTools = (HashSet<String>) field.get(materialMapStore);
+			ironTools.add("crossbow");
+		} catch (Exception ex) {
+			BNCore.log("Could not add crossbow to iron tools");
+			ex.printStackTrace();
+		}
 	}
 
 }
