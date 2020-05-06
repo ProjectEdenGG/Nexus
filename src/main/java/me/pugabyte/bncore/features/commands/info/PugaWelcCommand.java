a6aa958049b3ebbda28031640cb86d763e28401f
Pugabyte <pugabyte@projecteden.gg>
2020-05-06 09:22:55 -0400

    Add automatic converter and tab completer for enum classes

--- src/main/java/me/pugabyte/bncore/features/commands/info/PugaWelcCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/info/PugaWelcCommand.java
@@ -14,8 +14,6 @@ public class PugaWelcCommand extends CustomCommand {
 	void run() {
 		line(2);
 		send("&eHello there, and welcome to the server!");
-		send("&3Most likely you are asking yourself the question '&eWhy should I choose this server?&3'");
-		send("&3I can promise you that if you &estick around &3long enough and &egive us a chance&3, you won't be disappointed");
 		send("&3Here's a few resources you may find helpful as you explore our server. Just &e&lclick &3to open them");
 		send(json("&3[+] &eFAQ").command("/faq"));
 		send(json("&3[+] &eRules").command("/rules"));
--- src/main/java/me/pugabyte/bncore/features/commands/staff/SpawnLeaderboardsCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/SpawnLeaderboardsCommand.java
@@ -3,14 +3,9 @@ package me.pugabyte.bncore.features.commands.staff;
 import lombok.NonNull;
 import me.pugabyte.bncore.features.listeners.Leaderboards.Leaderboard;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
-import me.pugabyte.bncore.framework.commands.models.annotations.ConverterFor;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
-import me.pugabyte.bncore.framework.commands.models.annotations.TabCompleterFor;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
-import me.pugabyte.bncore.framework.exceptions.postconfigured.InvalidInputException;
-
-import java.util.List;
 
 @Permission("group.staff")
 public class SpawnLeaderboardsCommand extends CustomCommand {
@@ -23,20 +18,6 @@ public class SpawnLeaderboardsCommand extends CustomCommand {
 	void update(Leaderboard leaderboard) {
 		leaderboard.update();
 		send(PREFIX + "Updated");
-	}
-
-	@ConverterFor(Leaderboard.class)
-	Leaderboard convertToLeaderboard(String value) {
-		try {
-			return Leaderboard.valueOf(value.toUpperCase());
-		} catch (IllegalArgumentException ignore) {
-			throw new InvalidInputException("Leaderboard from " + value + " not found");
-		}
-	}
-
-	@TabCompleterFor(Leaderboard.class)
-	List<String> tabCompleteLeaderboard(String filter) {
-		return tabCompleteEnum(Leaderboard.class, filter);
 	}
 
 }
