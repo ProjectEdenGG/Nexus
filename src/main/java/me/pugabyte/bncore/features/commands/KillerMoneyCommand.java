1819542d504392d8d6024f886ed38bee4068fca7
Pugabyte <pugabyte@projecteden.gg>
2020-05-06 08:28:15 -0400

    Fix KillerMoney working on spawner mobs, turn off killermoney from suffocation

--- src/main/java/me/pugabyte/bncore/features/commands/KillerMoneyCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/KillerMoneyCommand.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.pugabyte.bncore.BNCore;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.models.setting.Setting;
@@ -13,15 +14,23 @@ import me.pugabyte.bncore.utils.Utils;
 import me.pugabyte.bncore.utils.WorldGroup;
 import org.bukkit.GameMode;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 
 import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
+@Aliases("km")
 @NoArgsConstructor
 public class KillerMoneyCommand extends CustomCommand implements Listener {
 	SettingService service = new SettingService();
@@ -40,23 +49,58 @@ public class KillerMoneyCommand extends CustomCommand implements Listener {
 	}
 
 	@EventHandler
-	public void onEntityKill(EntityDeathEvent event) {
-		Player player = event.getEntity().getKiller();
-		if (player == null) return;
-		if (!player.getGameMode().equals(GameMode.SURVIVAL)) return;
-		try {
-			MobMoney mob = MobMoney.valueOf(event.getEntityType().name());
-			if (!mob.getActiveWorlds().contains(WorldGroup.get(player.getWorld()))) return;
-			if (event.getEntityType() == EntityType.ENDERMAN && player.getWorld().getName().contains("the_end")) return;
+	public void onEntitySpawn(CreatureSpawnEvent event) {
+		if (event.getSpawnReason() == SpawnReason.SPAWNER || event.getSpawnReason() == SpawnReason.SPAWNER_EGG)
+			event.getEntity().setMetadata("killermoney-spawner", new FixedMetadataValue(BNCore.getInstance(), true));
+	}
 
-			double money = mob.getRandomValue() * BOOST;
-			BNCore.getEcon().depositPlayer(player, money);
-			DecimalFormat formatter = new DecimalFormat("#.##");
-			if (!new SettingService().get(player, "killerMoneyMute").getBoolean())
-				player.sendMessage(StringUtils.colorize("&3You killed a " + mob.name().toLowerCase().replace("_", " ") +
-						"&3 and received &e$" + formatter.format(money)));
-		} catch (IllegalArgumentException ignore) {
+	@EventHandler
+	public void onEntityKill(EntityDamageByEntityEvent event) {
+		LivingEntity victim;
+		if (!(event.getEntity() instanceof LivingEntity)) return;
+
+		victim = (LivingEntity) event.getEntity();
+		Player player = null;
+
+		if (event.getDamager() instanceof Player) {
+			player = (Player) event.getDamager();
+		} else if (event.getDamager() instanceof Projectile) {
+			Projectile projectile = (Projectile) event.getDamager();
+			if (projectile.getShooter() instanceof Player)
+				player = (Player) projectile.getShooter();
 		}
+		if (player == null) return;
+
+		if (victim.getHealth() - event.getFinalDamage() > 0)
+			return;
+
+		if (!player.getGameMode().equals(GameMode.SURVIVAL))
+			return;
+
+		if (event.getCause() == DamageCause.SUFFOCATION)
+			return;
+		for (MetadataValue meta : victim.getMetadata("killermoney-spawner"))
+			if (meta.asBoolean())
+				return;
+
+		MobMoney mob;
+		try {
+			mob = MobMoney.valueOf(event.getEntityType().name());
+		} catch (IllegalArgumentException ignore) {
+			return;
+		}
+
+		if (!mob.getActiveWorlds().contains(WorldGroup.get(player.getWorld()))) return;
+
+		// TODO make this enum config driven
+		if (event.getEntityType() == EntityType.ENDERMAN && player.getWorld().getName().contains("the_end")) return;
+
+		double money = mob.getRandomValue() * BOOST;
+		BNCore.getEcon().depositPlayer(player, money);
+		DecimalFormat formatter = new DecimalFormat("#.##");
+		if (!new SettingService().get(player, "killerMoneyMute").getBoolean())
+			player.sendMessage(StringUtils.colorize("&3You killed a " + mob.name().toLowerCase().replace("_", " ") +
+					"&3 and received &e$" + formatter.format(money)));
 	}
 
 	@Getter
@@ -100,8 +144,7 @@ public class KillerMoneyCommand extends CustomCommand implements Listener {
 		}
 
 		double getRandomValue() {
-			Double random = Utils.randomDouble(min, max);
-			return random;
+			return Utils.randomDouble(min, max);
 		}
 
 	}
