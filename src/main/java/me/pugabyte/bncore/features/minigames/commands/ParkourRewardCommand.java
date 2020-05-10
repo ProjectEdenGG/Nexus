505995b1566f5521a6b04cf417e9e047507f0506
BuildTools <WakkaFlocka239@gmail.com>
2020-05-10 03:53:50 -0500

    parkourreward now needs to be run as op from interactioncommands

--- src/main/java/me/pugabyte/bncore/features/minigames/commands/ParkourRewardCommand.java
+++ src/main/java/me/pugabyte/bncore/features/minigames/commands/ParkourRewardCommand.java
@@ -6,9 +6,11 @@ import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Cooldown;
 import me.pugabyte.bncore.framework.commands.models.annotations.Cooldown.Part;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.utils.Time;
 
+@Permission("group.admin")
 @Cooldown(@Part(Time.WEEK))
 public class ParkourRewardCommand extends CustomCommand {
 
