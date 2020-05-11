3a0deff4617c3f30fb0da31538c61f5d8e670811
BuildTools <WakkaFlocka239@gmail.com>
2020-05-11 11:28:29 -0500

    add staff perms on /igosleep

--- src/main/java/me/pugabyte/bncore/features/commands/staff/IGoSleepCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/IGoSleepCommand.java
@@ -4,10 +4,12 @@ import lombok.NonNull;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.utils.Tasks;
 import me.pugabyte.bncore.utils.Time;
 
+@Permission("group.staff")
 public class IGoSleepCommand extends CustomCommand {
 
 	public IGoSleepCommand(@NonNull CommandEvent event) {
