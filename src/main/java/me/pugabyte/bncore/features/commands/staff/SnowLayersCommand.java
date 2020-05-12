53bbcd5c2f838ba6e4e79c086b0373a7b76f81ff
Pugabyte <pugabyte@projecteden.gg>
2020-05-12 18:21:39 -0400

    Add /snowlayers

---
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/SnowLayersCommand.java
@@ -0,0 +1,25 @@
+package me.pugabyte.bncore.features.commands.staff;
+
+import lombok.NonNull;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.Snow;
+
+public class SnowLayersCommand extends CustomCommand {
+
+	public SnowLayersCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	@Path("[layers]")
+	void layers(int layers) {
+		Block block = player().getLocation().getBlock();
+		block.setType(Material.SNOW);
+		Snow snow = (Snow) block.getBlockData();
+		snow.setLayers(layers);
+		block.setBlockData(snow);
+	}
+}
