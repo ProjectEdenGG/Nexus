fd13abaca09ff7c3ecfb515160fa2492c27a1137
Pugabyte <pugabyte@projecteden.gg>
2020-05-12 18:21:58 -0400

    Add permission

--- src/main/java/me/pugabyte/bncore/features/commands/staff/SnowLayersCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/SnowLayersCommand.java
@@ -3,11 +3,13 @@ package me.pugabyte.bncore.features.commands.staff;
 import lombok.NonNull;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.type.Snow;
 
+@Permission("group.staff")
 public class SnowLayersCommand extends CustomCommand {
 
 	public SnowLayersCommand(@NonNull CommandEvent event) {
