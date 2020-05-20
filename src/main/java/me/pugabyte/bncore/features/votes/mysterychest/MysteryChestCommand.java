42bb90713263427291e67374539d3a26133f9a2b
BuildTools <unconfigured@null.spigotmc.org>
2020-05-20 18:40:50 -0400

    add admin command to give mystery chests

--- src/main/java/me/pugabyte/bncore/features/votes/mysterychest/MysteryChestCommand.java
+++ src/main/java/me/pugabyte/bncore/features/votes/mysterychest/MysteryChestCommand.java
@@ -2,6 +2,7 @@ package me.pugabyte.bncore.features.votes.mysterychest;
 
 import fr.minuskube.inv.SmartInventory;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
@@ -9,11 +10,11 @@ import me.pugabyte.bncore.models.setting.Setting;
 import me.pugabyte.bncore.models.setting.SettingService;
 import me.pugabyte.bncore.utils.Utils;
 import me.pugabyte.bncore.utils.WorldGroup;
+import org.bukkit.OfflinePlayer;
 
 public class MysteryChestCommand extends CustomCommand {
 
 	SettingService service = new SettingService();
-	Setting setting = service.get(player(), "mysteryChest");
 
 	public MysteryChestCommand(CommandEvent event) {
 		super(event);
@@ -28,12 +29,13 @@ public class MysteryChestCommand extends CustomCommand {
 
 	@Path()
 	void use() {
+		Setting setting = service.get(player(), "mysteryChest");
 		int chests = 0;
 		try {
 			chests = Integer.parseInt(setting.getValue());
 		} catch (Exception ignore) {
 		}
-		if (chests == 0)
+		if (chests > 0)
 			error("You do not have any mystery chest to open");
 		if (!WorldGroup.get(player()).equals(WorldGroup.SURVIVAL))
 			error("You must be in the survival world to run this command.");
@@ -43,9 +45,25 @@ public class MysteryChestCommand extends CustomCommand {
 		INV.open(player());
 	}
 
+	@Path("give <player> [amount]")
+	@Permission("group.admin")
+	void give(OfflinePlayer player, @Arg("1") int amount) {
+		Setting setting = service.get(player, "mysteryChest");
+		int chests = 0;
+		try {
+			chests = Integer.parseInt(setting.getValue());
+		} catch (Exception ignore) {
+		}
+		chests += amount;
+		setting.setValue("" + chests);
+		service.save(setting);
+		send(PREFIX + "&e" + player.getName() + " &3now has &e" + chests + "&3 Mystery Chests");
+	}
+
 	@Path("test")
 	@Permission("group.admin")
 	void test() {
+		Setting setting = service.get(player(), "mysteryChest");
 		setting.setValue("1");
 		service.save(setting);
 		use();
