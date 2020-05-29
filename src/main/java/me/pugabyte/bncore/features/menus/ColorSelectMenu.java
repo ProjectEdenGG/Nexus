b86775afeebe4a468c5b130a4eae43e7fc71f5ae
Pugabyte <pugabyte@bear-nation.net>
2020-05-29 01:04:43 -0400

    Use null check on dye color to skip

--- src/main/java/me/pugabyte/bncore/features/menus/ColorSelectMenu.java
+++ src/main/java/me/pugabyte/bncore/features/menus/ColorSelectMenu.java
@@ -24,14 +24,13 @@ public class ColorSelectMenu extends MenuUtils implements InventoryProvider {
 
 	@Override
 	public void init(Player player, InventoryContents contents) {
-
 		addCloseItem(contents);
 
 		int row = 1;
 		int column = 0;
 
 		for (ColorType color : ColorType.values()) {
-			if (color == ColorType.LIGHT_RED) continue;
+			if (color.getDyeColor() == null) continue;
 			ItemStack item = nameItem(color.switchColor(type), "&e" + StringUtils.camelCase(color.getName()));
 			contents.set(row, column, ClickableItem.from(item, e -> onClick.accept(e)));
 			if (column == 8) {
