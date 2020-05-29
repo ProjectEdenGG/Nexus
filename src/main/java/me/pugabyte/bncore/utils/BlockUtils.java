a3df64c23a13f4606c5bfd9376ae4deb30517eed
Pugabyte <pugabyte@bear-nation.net>
2020-05-29 00:52:51 -0400

    Style and keyword fixes

--- src/main/java/me/pugabyte/bncore/utils/BlockUtils.java
+++ src/main/java/me/pugabyte/bncore/utils/BlockUtils.java
@@ -10,48 +10,49 @@ import java.util.Iterator;
 
 public class BlockUtils {
 
-	public static void updateBlockProperty(Block block, String key, String newValue){
+	public static void updateBlockProperty(Block block, String key, String newValue) {
 		block.setBlockData(getBlockDataWithNewValue(block, key, newValue));
 	}
 
-	public static String getBlockProperty(Block block, String key){
-		HashMap<String, String> properties  = getBlockProperties(block);
-		return properties.containsKey(key) ? properties.get(key) : null;
+	public static String getBlockProperty(Block block, String key) {
+		HashMap<String, String> properties = getBlockProperties(block);
+		return properties.getOrDefault(key, null);
 	}
 
-	public static boolean containsBlockProperty(Block block, String key){
-		return getBlockProperty(block, key) !=null;
+	public static boolean containsBlockProperty(Block block, String key) {
+		return getBlockProperty(block, key) != null;
 	}
 
-	public static HashMap<String, String> getBlockProperties(Block block){
+	public static HashMap<String, String> getBlockProperties(Block block) {
 		return getBlockProperties(block.getState().getBlockData().getAsString());
 	}
 
-	public static HashMap<String, String> getBlockProperties(BlockData blockData){
+	public static HashMap<String, String> getBlockProperties(BlockData blockData) {
 		return getBlockProperties(blockData.getAsString());
 	}
 
-	public static HashMap<String, String> getBlockProperties(String blockDataString){
+	public static HashMap<String, String> getBlockProperties(String blockDataString) {
 		HashMap<String, String> blockDataVariables = new HashMap<>();
 		String[] variables = blockDataString.replace("]", "").split("\\[");
 		String[] variableList = variables.length > 1 ? variables[1].split(",") : null;
-		if(variableList!=null) for(String s:variableList) blockDataVariables.put(s.split("=")[0], s.split("=")[1]);
+		if (variableList != null)
+			for (String s : variableList) blockDataVariables.put(s.split("=")[0], s.split("=")[1]);
 		return blockDataVariables;
 	}
 
-	public static BlockData getBlockDataWithNewValue(Block block, String key, String newValue){
+	public static BlockData getBlockDataWithNewValue(Block block, String key, String newValue) {
 		HashMap<String, String> variables = getBlockProperties(block);
-		if(variables.containsKey(key.toLowerCase()))variables.put(key, newValue.toLowerCase());
+		if (variables.containsKey(key.toLowerCase())) variables.put(key, newValue.toLowerCase());
 		return getBlockDataFromList(block, variables);
 	}
 
-	public static BlockData getBlockDataFromList(Block block, HashMap<String, String> variables){
+	public static BlockData getBlockDataFromList(Block block, HashMap<String, String> variables) {
 		return getBlockDataFromList(block.getType(), variables);
 	}
 
-	public static BlockData getBlockDataFromList(Material material, HashMap<String, String> variables){
-		if(material==null) return null;
-		if(variables!=null&&!variables.isEmpty()){
+	public static BlockData getBlockDataFromList(Material material, HashMap<String, String> variables) {
+		if (material == null) return null;
+		if (variables != null && !variables.isEmpty()) {
 			return Bukkit.createBlockData(generateBlockDataString(material, variables));
 		} else {
 			return Bukkit.createBlockData(material);
@@ -59,17 +60,17 @@ public class BlockUtils {
 
 	}
 
-	public static String generateBlockDataString(Material material, HashMap<String, String> values){
-		if(values!=null&&!values.isEmpty()){
+	public static String generateBlockDataString(Material material, HashMap<String, String> values) {
+		if (values != null && !values.isEmpty()) {
 			StringBuilder vsb = new StringBuilder();
-			Iterator i = values.keySet().iterator();
-			while(i.hasNext()){
-				String v = (String)i.next();
+			Iterator<String> i = values.keySet().iterator();
+			while (i.hasNext()) {
+				String v = i.next();
 				vsb.append(String.format("%s=%s", v, i.hasNext() ? values.get(v) + "," : values.get(v)));
 			}
 			return String.format("minecraft:%s[%s]", material.toString().toLowerCase(), vsb.toString());
 		} else {
-			return String.format("minecraft:%s",material.toString()).toLowerCase();
+			return String.format("minecraft:%s", material.toString()).toLowerCase();
 		}
 	}
 
