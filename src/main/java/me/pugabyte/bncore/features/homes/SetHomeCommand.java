1d01c928e2a96b8373c3af9f7714bb9c9a3e1131
Pugabyte <pugabyte@bear-nation.net>
2020-05-29 19:21:42 -0400

    Add staff home commands

--- src/main/java/me/pugabyte/bncore/features/homes/SetHomeCommand.java
+++ src/main/java/me/pugabyte/bncore/features/homes/SetHomeCommand.java
@@ -4,10 +4,12 @@ import lombok.NonNull;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.models.home.Home;
 import me.pugabyte.bncore.models.home.HomeOwner;
 import me.pugabyte.bncore.models.home.HomeService;
+import org.bukkit.OfflinePlayer;
 
 import java.util.Optional;
 
@@ -22,11 +24,11 @@ public class SetHomeCommand extends CustomCommand {
 	}
 
 	@Path("[name]")
-	void sethome(@Arg("home") String homeName) {
+	void setHome(@Arg("home") String homeName) {
 		int homes = homeOwner.getHomes().size();
 		int max = homeOwner.getMaxHomes();
 		int left = Math.max(0, max - homes);
-		if (left == 0)
+		if (left <= 0)
 			error("You have used all of your available homes! &3To set more homes, you will need to either &erank up &3or &c/donate");
 
 		Optional<Home> home = homeOwner.getHome(homeName);
@@ -41,6 +43,31 @@ public class SetHomeCommand extends CustomCommand {
 					.location(player().getLocation())
 					.build());
 			message = "Home \"&e" + homeName + "&3\" set to current location. Return with &c/h" + (homeName.equalsIgnoreCase("home") ? "" : " " + homeName);
+		}
+
+		service.save(homeOwner);
+		send(PREFIX + message);
+	}
+
+	@Permission("group.staff")
+	@Path("<player> <name>")
+	void setHome(OfflinePlayer player, String homeName) {
+		homeOwner = service.get(player);
+		int homes = homeOwner.getHomes().size();
+		int max = homeOwner.getMaxHomes();
+
+		Optional<Home> home = homeOwner.getHome(homeName);
+		String message;
+		if (home.isPresent()) {
+			home.get().setLocation(player().getLocation());
+			message = "Updated location of home \"&e" + homeName + "&3\"";
+		} else {
+			homeOwner.add(Home.builder()
+					.uuid(player().getUniqueId())
+					.name(homeName)
+					.location(player().getLocation())
+					.build());
+			message = "Home \"&e" + homeName + "&3\" set to current location";
 		}
 
 		service.save(homeOwner);
