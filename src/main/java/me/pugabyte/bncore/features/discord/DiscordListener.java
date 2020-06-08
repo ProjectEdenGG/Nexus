e4cd7258275398195a2787ffe07437cad77cdb60
Pugabyte <pugabyte@projecteden.gg>
2020-06-08 01:08:28 -0400

    Add .pug back

--- src/main/java/me/pugabyte/bncore/features/discord/DiscordListener.java
+++ src/main/java/me/pugabyte/bncore/features/discord/DiscordListener.java
@@ -1,11 +1,14 @@
 package me.pugabyte.bncore.features.discord;
 
 import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.pugabyte.bncore.BNCore;
 import me.pugabyte.bncore.features.discord.DiscordId.Channel;
 import me.pugabyte.bncore.features.discord.DiscordId.Role;
 import me.pugabyte.bncore.features.discord.DiscordId.User;
+import me.pugabyte.bncore.framework.exceptions.BNException;
 import me.pugabyte.bncore.models.discord.DiscordService;
 import me.pugabyte.bncore.models.discord.DiscordUser;
 import me.pugabyte.bncore.models.setting.Setting;
@@ -16,9 +19,14 @@ import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
+
+import static me.pugabyte.bncore.utils.StringUtils.stripColor;
 
 @NoArgsConstructor
 public class DiscordListener extends ListenerAdapter {
@@ -31,6 +39,12 @@ public class DiscordListener extends ListenerAdapter {
 
 			BNCore.fileLog("discord", name + " joined " + channel);
 		});
+	}
+
+	@Data
+	private class RandomPugClubResponse {
+		private String image;
+		private String link;
 	}
 
 	@Override
@@ -48,6 +62,21 @@ public class DiscordListener extends ListenerAdapter {
 				message += " " + attachment.getUrl();
 
 			BNCore.fileLog("discord", "[#" + channel + "] " + name + ": " + message.trim());
+
+			if (Channel.BOT_COMMANDS.getId().equals(event.getChannel().getId())) {
+				if (message.toLowerCase().startsWith(".pug")) {
+					try {
+						try (Response response = new OkHttpClient().newCall(new Request.Builder().url("http://randompug.club/loaf").build()).execute()) {
+							RandomPugClubResponse result = new Gson().fromJson(response.body().string(), RandomPugClubResponse.class);
+							Discord.koda(result.getImage(), Channel.BOT_COMMANDS);
+						}
+					} catch (Exception ex) {
+						event.getChannel().sendMessage(stripColor(ex.getMessage())).queue();
+						if (!(ex instanceof BNException))
+							ex.printStackTrace();
+					}
+				}
+			}
 		});
 	}
 
