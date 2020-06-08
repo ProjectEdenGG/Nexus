b9fc1aa530b57d177bb98a39ff5e0571cba21fdd
Blast <cjblanton2@gmail.com>
2020-06-08 19:34:39 -0400

    add custom recipes menu

--- src/main/java/me/pugabyte/bncore/features/recipes/CustomRecipesCommand.java
+++ src/main/java/me/pugabyte/bncore/features/recipes/CustomRecipesCommand.java
@@ -1,12 +1,10 @@
 package me.pugabyte.bncore.features.recipes;
 
-import fr.minuskube.inv.SmartInventory;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 public class CustomRecipesCommand extends CustomCommand {
 
@@ -17,7 +15,7 @@ public class CustomRecipesCommand extends CustomCommand {
 
 	@Path()
 	void open() {
-		openMenu(CraftingRecipeMenu.MAIN, player());
+		CraftingRecipeMenu.open(CraftingMenuType.MAIN, player());
 	}
 
 	@Path("reload")
@@ -29,15 +27,6 @@ public class CustomRecipesCommand extends CustomCommand {
 		CustomRecipes.amount = 0;
 		new CustomRecipes();
 		send(PREFIX + "Successfully reloaded &e" + CustomRecipes.amount + "&3 custom recipes");
-	}
-
-	public static void openMenu(CraftingRecipeMenu menu, Player player) {
-		SmartInventory.builder()
-				.size(3, 9)
-				.title("Custom Recipes")
-				.provider(new CraftingRecipeProvider(menu))
-				.build()
-				.open(player);
 	}
 
 
