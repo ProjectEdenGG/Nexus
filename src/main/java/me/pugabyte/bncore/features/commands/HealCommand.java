434915fc10b44e39c34cb8e1666bd8e1bb6ad1aa
Pugabyte <pugabyte@projecteden.gg>
2020-06-10 04:17:23 -0400

    Add /feed and /heal

---
+++ src/main/java/me/pugabyte/bncore/features/commands/HealCommand.java
@@ -0,0 +1,28 @@
+package me.pugabyte.bncore.features.commands;
+
+import lombok.NonNull;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+
+@Permission("group.staff")
+public class HealCommand extends CustomCommand {
+
+	public HealCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	@Path("[player]")
+	void run(@Arg("self") Player player) {
+		player.setHealth(20);
+		player.setFoodLevel(20);
+		player.setFireTicks(0);
+		for (PotionEffect effect : player.getActivePotionEffects())
+			player.removePotionEffect(effect.getType());
+	}
+
+}
