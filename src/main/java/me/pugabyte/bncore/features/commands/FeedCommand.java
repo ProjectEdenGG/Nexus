440df9dcf99a0a238cebace86ed605d79610bd20
Pugabyte <pugabyte@bear-nation.net>
2020-06-10 04:17:23 -0400

    Add /feed and /heal

---
+++ src/main/java/me/pugabyte/bncore/features/commands/FeedCommand.java
@@ -0,0 +1,26 @@
+package me.pugabyte.bncore.features.commands;
+
+import lombok.NonNull;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import org.bukkit.entity.Player;
+
+@Permission("group.staff")
+public class FeedCommand extends CustomCommand {
+
+	public FeedCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	@Path("[player]")
+	void run(@Arg("self") Player player) {
+		player.setFoodLevel(20);
+		player.setSaturation(10);
+		player.setExhaustion(0);
+		send(PREFIX + "Fed " + player.getName());
+	}
+
+}
