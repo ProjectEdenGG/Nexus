20f301fd0e6eaa1de1516d90fdfcc1356e03dc06
Pugabyte <pugabyte@projecteden.gg>
2020-06-10 01:46:43 -0400

    Add /item and /give

---
+++ src/main/java/me/pugabyte/bncore/features/commands/ItemCommand.java
@@ -0,0 +1,26 @@
+package me.pugabyte.bncore.features.commands;
+
+import lombok.NonNull;
+import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
+import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
+import me.pugabyte.bncore.utils.Utils;
+import org.bukkit.Material;
+
+@Aliases("i")
+@Permission("essentials.item")
+public class ItemCommand extends CustomCommand {
+
+	public ItemCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	@Path("<type> [amount]")
+	void run(Material material, @Arg("1") int amount) {
+		Utils.giveItem(player(), material, amount);
+	}
+
+}
