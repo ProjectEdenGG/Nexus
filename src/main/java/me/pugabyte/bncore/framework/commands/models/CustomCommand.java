ab65fa6b4ffcda25f6415e1648d4f3e52904571f
Pugabyte <pugabyte@projecteden.gg>
2020-06-20 00:34:22 -0400

    Update tameables to better handle foxes, add /tameables move

--- src/main/java/me/pugabyte/bncore/framework/commands/models/CustomCommand.java
+++ src/main/java/me/pugabyte/bncore/framework/commands/models/CustomCommand.java
@@ -38,6 +38,7 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static me.pugabyte.bncore.utils.StringUtils.trimFirst;
@@ -145,6 +146,13 @@ public abstract class CustomCommand extends ICustomCommand {
 			throw new MustBeIngameException();
 
 		return (Player) event.getSender();
+	}
+
+	protected UUID uuid() {
+		if (!isPlayer())
+			throw new MustBeIngameException();
+
+		return ((Player) event.getSender()).getUniqueId();
 	}
 
 	protected ConsoleCommandSender console() {
