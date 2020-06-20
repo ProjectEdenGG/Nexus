4e8e8217c97e2145f5dc8731188e07608918b6e9
Pugabyte <pugabyte@projecteden.gg>
2020-06-20 19:21:50 -0400

    Add numbers to banners command

--- src/main/java/me/pugabyte/bncore/features/votes/BannerCommand.java
+++ src/main/java/me/pugabyte/bncore/features/votes/BannerCommand.java
@@ -3,20 +3,27 @@ package me.pugabyte.bncore.features.votes;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Arg;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
+import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import me.pugabyte.bncore.framework.exceptions.postconfigured.InvalidInputException;
 import me.pugabyte.bncore.models.vote.VoteService;
+import me.pugabyte.bncore.utils.ColorType;
 import me.pugabyte.bncore.utils.MaterialTag;
 import me.pugabyte.bncore.utils.StringUtils;
 import me.pugabyte.bncore.utils.Utils;
 import me.pugabyte.bncore.utils.WorldGuardUtils;
+import org.bukkit.DyeColor;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static me.pugabyte.bncore.utils.StringUtils.colorize;
 
@@ -25,6 +32,89 @@ public class BannerCommand extends CustomCommand implements Listener {
 
 	public BannerCommand(@NonNull CommandEvent event) {
 		super(event);
+	}
+
+	private final static String A = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:rs,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:ms,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String B = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:rs,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:cbo,Color:<baseInt>},{Pattern:ls,Color:<patternInt>},{Pattern:ms,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String C = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ts,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:ms,Color:<baseInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String D = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:rs,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:cbo,Color:<baseInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String E = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ls,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:ms,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String F = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ms,Color:<patternInt>},{Pattern:rs,Color:<baseInt>},{Pattern:ts,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String G = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:rs,Color:<patternInt>},{Pattern:hh,Color:<baseInt>},{Pattern:bs,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String H = "<patternBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ts,Color:<baseInt>},{Pattern:bs,Color:<baseInt>},{Pattern:ls,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String I = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:cs,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String J = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ls,Color:<patternInt>},{Pattern:hh,Color:<baseInt>},{Pattern:bs,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String K = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:drs,Color:<patternInt>},{Pattern:hh,Color:<baseInt>},{Pattern:dls,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String L = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:bs,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String M = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:tt,Color:<patternInt>},{Pattern:tts,Color:<baseInt>},{Pattern:ls,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String N = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ls,Color:<patternInt>},{Pattern:tt,Color:<baseInt>},{Pattern:drs,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String O = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ls,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String P = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:rs,Color:<patternInt>},{Pattern:hhb,Color:<baseInt>},{Pattern:ms,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String Q = "<patternBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:mr,Color:<baseInt>},{Pattern:rs,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:br,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String R = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ms,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:hhb,Color:<baseInt>},{Pattern:ls,Color:<patternInt>},{Pattern:drs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String S = "<patternBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:mr,Color:<baseInt>},{Pattern:ms,Color:<baseInt>},{Pattern:drs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String T = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ts,Color:<patternInt>},{Pattern:cs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String U = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:bs,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String V = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:dls,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bt,Color:<baseInt>},{Pattern:dls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String W = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:bt,Color:<patternInt>},{Pattern:bts,Color:<baseInt>},{Pattern:ls,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String X = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:cr,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String Y = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:drs,Color:<patternInt>},{Pattern:hhb,Color:<baseInt>},{Pattern:dls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String Z = "<baseBanner>{BlockEntityTag:{Base:<baseInt>,Patterns:[{Pattern:ts,Color:<patternInt>},{Pattern:dls,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _0 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:bs,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:dls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _1 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:cs,Color:<patternInt>},{Pattern:tl,Color:<patternInt>},{Pattern:cbo,Color:<baseInt>},{Pattern:bs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _2 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:ts,Color:<patternInt>},{Pattern:mr,Color:<baseInt>},{Pattern:bs,Color:<patternInt>},{Pattern:dls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _3 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:bs,Color:<patternInt>},{Pattern:ms,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:cbo,Color:<baseInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _4 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:ls,Color:<patternInt>},{Pattern:hhb,Color:<baseInt>},{Pattern:rs,Color:<patternInt>},{Pattern:ms,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _5 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:bs,Color:<patternInt>},{Pattern:mr,Color:<baseInt>},{Pattern:ts,Color:<patternInt>},{Pattern:drs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _6 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:bs,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:hh,Color:<baseInt>},{Pattern:ms,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _7 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:dls,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _8 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:ts,Color:<patternInt>},{Pattern:ls,Color:<patternInt>},{Pattern:ms,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String _9 = "<baseBanner>{BlockEntityTag:{Patterns:[{Pattern:ls,Color:<patternInt>},{Pattern:hhb,Color:<baseInt>},{Pattern:ms,Color:<patternInt>},{Pattern:ts,Color:<patternInt>},{Pattern:rs,Color:<patternInt>},{Pattern:bs,Color:<patternInt>},{Pattern:bo,Color:<baseInt>}]}}";
+	private final static String[] alphabet = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z};
+	private final static String[] numbers = {_0, _1, _2, _3, _4, _5, _6, _7, _8, _9};
+	private final static String[] all = new ArrayList<String>() {{
+		addAll(Arrays.asList(alphabet));
+		addAll(Arrays.asList(numbers));
+	}}.toArray(new String[0]);
+
+	@Permission("group.staff")
+	@Path("<baseColor> <patternColor> [input...]")
+	void all(DyeColor baseColor, DyeColor patternColor, @Arg("-") String input) {
+		String baseBanner = "minecraft:" + baseColor.name().toLowerCase().replace(" ", "_") + "_banner";
+		String patternBanner = "minecraft:" + patternColor.name().toLowerCase().replace(" ", "_") + "_banner";
+		String baseInt = ColorType.fromDyeColor(baseColor).getDurability() + "";
+		String patternInt = ColorType.fromDyeColor(patternColor).getDurability() + "";
+
+		String give = "minecraft:give " + player().getName();
+		if (input.equalsIgnoreCase("*")) {
+			for (String letter : all) {
+				String banner = letter
+						.replaceAll("<baseBanner>", baseBanner)
+						.replaceAll("<patternBanner>", patternBanner)
+						.replaceAll("<baseInt>", baseInt)
+						.replaceAll("<patternInt>", patternInt);
+
+				runCommandAsConsole(give + " " + banner);
+			}
+		} else {
+			char[] chars = input.toUpperCase().replaceAll(" ", "").toCharArray();
+			for (int i = 0; i < chars.length; i++) {
+				char character = input.charAt(i);
+				String banner;
+				if (isInt(String.valueOf(character)))
+					banner = numbers[Integer.parseInt(String.valueOf(character))];
+				else
+					banner = alphabet[Character.getNumericValue(character) - Character.getNumericValue('A')];
+
+				banner = banner
+						.replaceAll("<baseBanner>", baseBanner)
+						.replaceAll("<patternBanner>", patternBanner)
+						.replaceAll("<baseInt>", baseInt)
+						.replaceAll("<patternInt>", patternInt);
+
+				runCommandAsConsole(give + " " + banner);
+			}
+		}
 	}
 
 	@Path("textures")
