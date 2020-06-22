6355d6666dd8e21dd69ff6c1813dbef94d79325a
Pugabyte <pugabyte@bear-nation.net>
2020-06-22 00:07:42 -0400

    Enable worldedit pasting (saving doesnt work, use WE commands)

--- src/main/java/me/pugabyte/bncore/BNCore.java
+++ src/main/java/me/pugabyte/bncore/BNCore.java
@@ -106,10 +106,6 @@ public class BNCore extends JavaPlugin {
 		}
 	}
 
-	public static boolean disableWorldEditPasting() {
-		return true;
-	}
-
 	public static void log(String message) {
 		getInstance().getLogger().info(stripColor(message));
 	}
