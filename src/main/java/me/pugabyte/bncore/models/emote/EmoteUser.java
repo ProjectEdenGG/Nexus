a38e51e2f27003ce58a05845bfe8fe9feb53a4b6
Pugabyte <pugabyte@bear-nation.net>
2020-07-04 00:31:32 -0400

    Add delay at end of match

---
+++ src/main/java/me/pugabyte/bncore/models/emote/EmoteUser.java
@@ -0,0 +1,30 @@
+package me.pugabyte.bncore.models.emote;
+
+import dev.morphia.annotations.Converters;
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import me.pugabyte.bncore.framework.persistence.serializer.mongodb.UUIDConverter;
+import me.pugabyte.bncore.models.PlayerOwnedObject;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity("emote_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Converters(UUIDConverter.class)
+public class EmoteUser extends PlayerOwnedObject {
+	@Id
+	@NonNull
+	private UUID uuid;
+	private boolean enabled = true;
+
+}
