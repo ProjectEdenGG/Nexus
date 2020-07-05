eeed19a84fc192e5a5c1a015835f2259e1be0965
Pugabyte <pugabyte@bear-nation.net>
2020-07-04 23:04:42 -0400

    Add flag converter and tab completer

--- src/main/java/me/pugabyte/bncore/features/commands/staff/WorldGuardEditCommand.java
+++ src/main/java/me/pugabyte/bncore/features/commands/staff/WorldGuardEditCommand.java
@@ -1,17 +1,24 @@
 package me.pugabyte.bncore.features.commands.staff;
 
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.Flag;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import me.pugabyte.bncore.BNCore;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
 import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
+import me.pugabyte.bncore.framework.commands.models.annotations.ConverterFor;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
+import me.pugabyte.bncore.framework.commands.models.annotations.TabCompleterFor;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Aliases("wgedit")
 @NoArgsConstructor
@@ -48,6 +55,19 @@ public class WorldGuardEditCommand extends CustomCommand implements Listener {
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		if (event.getPlayer().hasPermission(permission))
 			BNCore.getPerms().playerRemove(event.getPlayer(), permission);
+	}
+
+	@ConverterFor(Flag.class)
+	Flag<?> convertToWorldGuardFlag(String value) {
+		return WorldGuard.getInstance().getFlagRegistry().get(value);
+	}
+
+	@TabCompleterFor(Flag.class)
+	List<String> tabCompleteWorldGuardFlag(String filter) {
+		return WorldGuard.getInstance().getFlagRegistry().getAll().stream()
+				.map(Flag::getName)
+				.filter(name -> name.toLowerCase().startsWith(filter.toLowerCase()))
+				.collect(Collectors.toList());
 	}
 
 }
