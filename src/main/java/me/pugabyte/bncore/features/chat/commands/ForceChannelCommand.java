82eaee11e6224631f6b46df977d04a9ed1673c05
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2020-08-11 00:11:50 -0500

    add alias /fc from /forcechannel

--- src/main/java/me/pugabyte/bncore/features/chat/commands/ForceChannelCommand.java
+++ src/main/java/me/pugabyte/bncore/features/chat/commands/ForceChannelCommand.java
@@ -2,6 +2,7 @@ package me.pugabyte.bncore.features.chat.commands;
 
 import lombok.NonNull;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
+import me.pugabyte.bncore.framework.commands.models.annotations.Aliases;
 import me.pugabyte.bncore.framework.commands.models.annotations.Path;
 import me.pugabyte.bncore.framework.commands.models.annotations.Permission;
 import me.pugabyte.bncore.framework.commands.models.events.CommandEvent;
@@ -9,6 +10,7 @@ import me.pugabyte.bncore.models.chat.Chatter;
 import me.pugabyte.bncore.models.chat.PublicChannel;
 
 @Permission("group.staff")
+@Aliases("fc")
 public class ForceChannelCommand extends CustomCommand {
 
 	public ForceChannelCommand(@NonNull CommandEvent event) {
