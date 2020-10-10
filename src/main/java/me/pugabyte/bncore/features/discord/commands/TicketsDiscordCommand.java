8eb6795950760a7111ee57d8a3a3dced28585dc7
Pugabyte <pugabyte@projecteden.gg>
2020-10-10 17:37:30 -0400

    More ticket fixes

--- src/main/java/me/pugabyte/bncore/features/discord/commands/TicketsDiscordCommand.java
+++ src/main/java/me/pugabyte/bncore/features/discord/commands/TicketsDiscordCommand.java
@@ -30,50 +30,58 @@ public class TicketsDiscordCommand extends Command {
 			return;
 
 		Tasks.async(() -> {
-			DiscordUser user = new DiscordService().checkVerified(event.getAuthor().getId());
-			OfflinePlayer player = Utils.getPlayer(user.getUuid());
+			final String PREFIX = "**[Tickets]** ";
+			try {
+				DiscordUser user = new DiscordService().checkVerified(event.getAuthor().getId());
+				OfflinePlayer player = Utils.getPlayer(user.getUuid());
 
-			String[] args = event.getArgs().split(" ");
-			if (args.length == 0 || !args[0].toLowerCase().matches("(view|close|reopen)"))
-				throw new InvalidInputException("Correct usage: `/tickets <view|close|reopen> <ticketId>`");
+				String[] args = event.getArgs().split(" ");
+				if (args.length == 0 || !args[0].toLowerCase().matches("(view|close|reopen)"))
+					throw new InvalidInputException("Correct usage: `/tickets <view|close|reopen> <ticketId>`");
 
-			String id = args[1];
-			if (!Utils.isInt(id))
-				throw new InvalidInputException("Ticket ID must be a number");
+				String id = args[1];
+				if (!Utils.isInt(id))
+					throw new InvalidInputException("Ticket ID must be a number");
 
-			final String nl = System.lineSeparator();
-			final TicketService service = new TicketService();
-			final Ticket ticket = service.get(Integer.parseInt(id));
+				final String nl = System.lineSeparator();
+				final TicketService service = new TicketService();
+				final Ticket ticket = service.get(Integer.parseInt(id));
 
-			switch (args[0].toLowerCase()) {
-				case "view":
-					String message = "**[Tickets] #" + ticket.getId() + "** ";
-					message += ticket.isOpen() ? "(Open)" : "(Closed)";
-					message += nl + "**Owner:** " + ticket.getOwnerName();
-					message += nl + "**When:** " + ticket.getTimespan() + " ago";
-					message += nl + "**Description:** " + ticket.getDescription();
-					event.reply(message);
-					break;
-				case "close": {
-					if (!ticket.isOpen())
-						throw new InvalidInputException("Ticket already closed");
+				switch (args[0].toLowerCase()) {
+					case "view":
+						String message = PREFIX + "**#" + ticket.getId() + "** ";
+						message += ticket.isOpen() ? "(Open)" : "(Closed)";
+						message += nl + "**Owner:** " + ticket.getOwnerName();
+						message += nl + "**When:** " + ticket.getTimespan() + " ago";
+						message += nl + "**Description:** " + ticket.getDescription();
+						event.reply(message);
+						break;
+					case "close": {
+						if (!ticket.isOpen())
+							throw new InvalidInputException("Ticket already closed");
 
-					ticket.setOpen(false);
-					service.save(ticket);
+						ticket.setOpen(false);
+						service.save(ticket);
 
-					Tickets.broadcast(ticket,null, player.getName() + " closed ticket #" + ticket.getId());
-					break;
+						Tickets.broadcast(ticket,null, player.getName() + " closed ticket #" + ticket.getId());
+						break;
+					}
+					case "reopen": {
+						if (ticket.isOpen())
+							throw new InvalidInputException("Ticket already open");
+
+						ticket.setOpen(true);
+						service.save(ticket);
+
+						Tickets.broadcast(ticket,null, player.getName() + " reopened ticket #" + ticket.getId());
+						break;
+					}
 				}
-				case "reopen": {
-					if (ticket.isOpen())
-						throw new InvalidInputException("Ticket already open");
-
-					ticket.setOpen(true);
-					service.save(ticket);
-
-					Tickets.broadcast(ticket,null, player.getName() + " reopened ticket #" + ticket.getId());
-					break;
-				}
+			} catch (InvalidInputException ex) {
+				event.reply(PREFIX + ex.getMessage());
+			} catch (Exception ex) {
+				event.reply(PREFIX + "An internal error occurred while attempting to execute this command");
+				ex.printStackTrace();
 			}
 		});
 	}
