13937373e8624d770ecb0cd8492959a8889e783a
Pugabyte <pugabyte@projecteden.gg>
2020-10-10 18:57:45 -0400

    Add Emote toggle buttons, closes Pugabyte/BearNation#384

--- src/main/java/me/pugabyte/bncore/features/chat/commands/EmotesCommand.java
+++ src/main/java/me/pugabyte/bncore/features/chat/commands/EmotesCommand.java
@@ -1,5 +1,7 @@
 package me.pugabyte.bncore.features.chat.commands;
 
+import java.util.Arrays;
+import java.util.function.Function;
 import lombok.NonNull;
 import me.pugabyte.bncore.features.chat.Emotes;
 import me.pugabyte.bncore.framework.commands.models.CustomCommand;
@@ -12,9 +14,6 @@ import me.pugabyte.bncore.models.emote.EmoteUser;
 import me.pugabyte.bncore.utils.JsonBuilder;
 import org.bukkit.ChatColor;
 
-import java.util.Arrays;
-import java.util.function.Function;
-
 public class EmotesCommand extends CustomCommand {
 	private final EmoteService service = new EmoteService();
 	private EmoteUser user;
@@ -25,34 +24,46 @@ public class EmotesCommand extends CustomCommand {
 	}
 
 	@Path("[page]")
-	void run(@Arg("1") int page) {
+	void page(@Arg("1") int page) {
 		Function<Emotes, JsonBuilder> formatter = emote -> {
 			JsonBuilder json = json();
-			if (emote.getColors().isEmpty())
-//				json
-//						.next("&c ✖ ")
-//						.hover("&aClick to enable emote")
-//						.command("/emotes enable " + emote.name().toLowerCase())
-//						.next(" &8| &3" + emote.getKey() + " &7-  " + emote.getEmote());
-				json.next(" &3" + emote.getKey() + " &7-  " + emote.getEmote());
-			else
+			if (emote.getColors().isEmpty()) {
+				if (user.isEnabled(emote))
+					json
+							.next("&a ✔ ")
+							.hover("&cClick to disable emote")
+							.command("/emotes disable " + emote.name().toLowerCase());
+				else
+					json
+							.next("&c ✖ ")
+							.hover("&aClick to enable emote")
+							.command("/emotes enable " + emote.name().toLowerCase());
+				json.next(" &8| &3" + emote.getKey() + " &7-  " + emote.getEmote());
+			} else {
 				for (ChatColor color : emote.getColors()) {
 					if (!json.isInitialized())
 						json.initialize();
 					else
 						json.newline();
 
-//					json
-//							.next("&c ✖ ")
-//							.hover("&aClick to enable emote")
-//							.command("/emotes enable " + emote.name().toLowerCase() + " " + color.name().toLowerCase())
-//							.next(" &8| &3" + emote.getKey() + " &7-  " + color + emote.getEmote());
-					json.next(" &3" + emote.getKey() + " &7-  " + color + emote.getEmote());
+					if (user.isEnabled(emote, color))
+						json
+								.next("&a ✔ ")
+								.hover("&cClick to disable emote")
+								.command("/emotes disable " + emote.name().toLowerCase() + " " + color.name().toLowerCase());
+					else
+						json
+								.next("&c ✖ ")
+								.hover("&aClick to enable emote")
+								.command("/emotes enable " + emote.name().toLowerCase() + " " + color.name().toLowerCase());
+					json.next(" &8| &3" + emote.getKey() + " &7-  " + color + emote.getEmote());
 				}
+			}
 
 			return json;
 		};
 
+		line(3);
 		paginate(Arrays.asList(Emotes.values()), formatter, "/emotes", page);
 	}
 
@@ -66,13 +77,23 @@ public class EmotesCommand extends CustomCommand {
 	@TabCompleteIgnore
 	@Path("enable <emote> [color]")
 	void enable(Emotes emote, ChatColor color) {
-
+		int page = (emote.ordinal() / 10) + 1;
+		if (user.enable(emote, color)) {
+			service.save(user);
+			page(page);
+		} else
+			error(camelCase(emote) + " is already enabled");
 	}
 
 	@TabCompleteIgnore
 	@Path("disable <emote> [color]")
 	void disable(Emotes emote, ChatColor color) {
-
+		int page = (emote.ordinal() / 10) + 1;
+		if (user.disable(emote, color)) {
+			service.save(user);
+			page(page);
+		} else
+			error(camelCase(emote) + " is already disabled");
 	}
 
 }
