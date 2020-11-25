5a89e09dccfbf564500f0945489ce218a518391f
Pugabyte <pugabyte@projecteden.gg>
2020-11-24 21:06:50 -0500

    Add /pugmas district, light the tree kit

--- src/main/java/me/pugabyte/nexus/features/events/y2020/pugmas20/models/AdventChest.java
+++ src/main/java/me/pugabyte/nexus/features/events/y2020/pugmas20/models/AdventChest.java
@@ -1,10 +1,13 @@
 package me.pugabyte.nexus.features.events.y2020.pugmas20.models;
 
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NonNull;
-import me.pugabyte.nexus.features.events.y2020.pugmas20.AdventChests;
+import me.pugabyte.nexus.features.events.y2020.pugmas20.Pugmas20;
 import me.pugabyte.nexus.utils.StringUtils;
+import me.pugabyte.nexus.utils.WorldGuardUtils;
 import org.bukkit.Location;
 
 @Data
@@ -16,7 +19,7 @@ public class AdventChest {
 	public AdventChest(int day, Location location) {
 		this.day = day;
 		this.location = location;
-		this.district = AdventChests.getDistrict(location);
+		this.district = District.of(location);
 	}
 
 	public AdventChest(int day, Location location, District district) {
@@ -27,14 +30,30 @@ public class AdventChest {
 
 	@AllArgsConstructor
 	public enum District {
-		HARBOR,
-		PLAZA,
 		GARDENS,
 		FROZEN,
+		HARBOR,
+		PLAZA,
 		UNKNOWN;
+
+		@Getter
+		private static final String region = Pugmas20.getRegion() + "_district_";
 
 		public String getName() {
 			return StringUtils.camelCase(name());
+		}
+
+		public static District of(Location location) {
+			WorldGuardUtils WGUtils = new WorldGuardUtils(location);
+			District district = null;
+			for (ProtectedRegion region : WGUtils.getRegionsAt(location))
+				if (region.getId().contains(District.region))
+					district = District.valueOf(region.getId().replace(District.region, "").toUpperCase());
+
+			if (district == null)
+				district = District.UNKNOWN;
+
+			return district;
 		}
 	}
 }
