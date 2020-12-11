2d1e38888a2f897b0e6f73a4ad7f73c18b0dbc7c
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2020-12-11 13:23:50 -0600

    Remove restrictions with permission "use.fire"

--- src/main/java/me/pugabyte/nexus/features/listeners/Restrictions.java
+++ src/main/java/me/pugabyte/nexus/features/listeners/Restrictions.java
@@ -6,22 +6,15 @@ import me.pugabyte.nexus.models.nerd.Rank;
 import me.pugabyte.nexus.utils.ItemUtils;
 import me.pugabyte.nexus.utils.MaterialTag;
 import me.pugabyte.nexus.utils.PlayerUtils;
-import me.pugabyte.nexus.utils.Tasks;
 import me.pugabyte.nexus.utils.WorldGroup;
 import org.bukkit.Material;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.entity.EntityPotionEffectEvent.Cause;
-import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
@@ -42,104 +35,9 @@ public class Restrictions implements Listener {
 	}
 
 	@EventHandler
-	public void onInteractWithFire(PlayerInteractEvent event) {
-		if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK))
-			return;
-
-		if (ItemUtils.isNullOrAir(event.getItem()))
-			return;
-
-		Material itemType = event.getItem().getType();
-		if (!(itemType.equals(Material.FLINT_AND_STEEL) || itemType.equals(Material.FIRE_CHARGE)))
-			return;
-
-		Player player = event.getPlayer();
-		Material clickedMaterial = event.getClickedBlock().getType();
-		if (WorldGroup.get(player).equals(WorldGroup.CREATIVE))
-			if (clickedMaterial.equals(Material.TNT))
-				event.setCancelled(true);
-
-		if (!(clickedMaterial.equals(Material.OBSIDIAN) || clickedMaterial.equals(Material.NETHERRACK)))
-			if (!player.hasPermission("use.fire")) {
-				event.setCancelled(true);
-				PlayerUtils.send(player, PREFIX + "Sorry, but you are not a high enough rank to light fire! Please create a &c/ticket &fto ask a staff member to light fire for you");
-			}
-	}
-
-	@EventHandler
-	public void onPlaceLava(PlayerBucketEmptyEvent event) {
-		Material material = event.getBucket();
-		if (!material.equals(Material.LAVA_BUCKET))
-			return;
-
-		Player player = event.getPlayer();
-		if (!player.hasPermission("use.fire")) {
-			event.setCancelled(true);
-			PlayerUtils.send(player, PREFIX + "Hey " + player.getName() + "! I noticed that you are trying to place lava. Unfortunately lava placing is disabled for Member and below due to grief and safety issues");
-			Tasks.wait(20, () -> PlayerUtils.send(player, PREFIX + "However, a staff member will be happy to place it for you. Please create a &c/ticket &fand a staff member will be with your shortly! :)"));
-		}
-	}
-
-	@EventHandler
-	public void onPlaceTNT(BlockPlaceEvent event) {
-		Material material = event.getBlock().getType();
-		if (!material.equals(Material.TNT))
-			return;
-
-		Player player = event.getPlayer();
-		if (!player.hasPermission("use.fire")) {
-			event.setCancelled(true);
-			PlayerUtils.send(player, PREFIX + "Sorry, but you can't use TNT! You must be Member or above");
-		}
-	}
-
-	@EventHandler
-	public void onCraftTNT(CraftItemEvent event) {
-		if (!event.getRecipe().getResult().getType().equals(Material.TNT))
-			return;
-
-		Player player = (Player) event.getWhoClicked();
-		if (!player.hasPermission("use.fire")) {
-			event.setCancelled(true);
-			PlayerUtils.send(player, PREFIX + "Sorry, but you can't use TNT! You must be Member or above");
-		}
-	}
-
-	@EventHandler
 	public void onWitherRoseEffect(EntityPotionEffectEvent event) {
 		if (event.getCause() == Cause.WITHER_ROSE)
 			event.setCancelled(true);
-	}
-
-	@EventHandler
-	public void onInteractHoldingEnderCrystal(PlayerInteractEvent event) {
-		Player player = event.getPlayer();
-
-		if (player.hasPermission("use.fire"))
-			return;
-
-		if (!ItemUtils.isNullOrAir(event.getItem()) && event.getItem().getType().equals(Material.END_CRYSTAL)) {
-			event.setCancelled(true);
-			PlayerUtils.send(player, PREFIX + "Sorry, but you can't use Ender Crystals! You must be Member or above");
-		}
-	}
-
-	@EventHandler
-	public void onDamageEnderCrystal(EntityDamageByEntityEvent event) {
-		Entity damager = event.getDamager();
-		Player player = null;
-		if (damager instanceof Arrow)
-			if (((Arrow) damager).getShooter() instanceof Player)
-				player = (Player) ((Arrow) damager).getShooter();
-
-		if (!event.getEntity().getType().equals(EntityType.ENDER_CRYSTAL) || player == null)
-			return;
-
-		if (!player.hasPermission("use.fire")) {
-			event.setCancelled(true);
-			PlayerUtils.send(player, PREFIX + "Sorry, but you can't use Ender Crystals! You must be Member or above");
-		}
-
 	}
 
 	@EventHandler
