f182ed046ed569f46f6a3853ccaabf601f6480db
Pugabyte <pugabyte@projecteden.gg>
2021-01-18 12:59:10 -0600

    Only handle trash sign on right click

--- src/main/java/me/pugabyte/nexus/features/listeners/CommandSigns.java
+++ src/main/java/me/pugabyte/nexus/features/listeners/CommandSigns.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
@@ -75,8 +76,10 @@ public class CommandSigns implements Listener {
 				disposal(player, sign);
 				break;
 			case "[Trash]":
-				event.setCancelled(true);
-				trash(player);
+				if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+					event.setCancelled(true);
+					trash(player);
+				}
 				break;
 		}
 	}
