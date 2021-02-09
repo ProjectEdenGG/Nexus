5b84804658ff3ad21c2de55d6ce31c9429a660e4
Blast-MC <cjblanton2@gmail.com>
2021-02-09 00:35:30 -0500

    add chat feedback to crate give command

--- src/main/java/me/pugabyte/nexus/features/crates/CrateCommand.java
+++ src/main/java/me/pugabyte/nexus/features/crates/CrateCommand.java
@@ -7,6 +7,7 @@ import me.pugabyte.nexus.framework.commands.models.annotations.Arg;
 import me.pugabyte.nexus.framework.commands.models.annotations.Path;
 import me.pugabyte.nexus.framework.commands.models.annotations.Permission;
 import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
+import me.pugabyte.nexus.utils.StringUtils;
 import org.bukkit.OfflinePlayer;
 
 @Permission("group.admin")
@@ -19,6 +20,11 @@ public class CrateCommand extends CustomCommand {
 	@Path("give <type> [player] [amount]")
 	void key(CrateType type, @Arg("self") OfflinePlayer player, @Arg("1") Integer amount) {
 		type.give(player, amount);
+		if (player.isOnline())
+			send(player.getPlayer(), Crates.PREFIX + "You have been given &e" + amount + " " + StringUtils.camelCase(type.name()) +
+					" Crate Keys");
+		if (!isSelf(player))
+			send(Crates.PREFIX + "You gave &e" + amount + " " + StringUtils.camelCase(type.name()) + " Crate Keys &3to &e" + player.getName());
 	}
 
 	@Path("edit")
