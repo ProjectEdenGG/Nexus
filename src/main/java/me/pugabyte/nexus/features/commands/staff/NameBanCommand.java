056c82988f5b58934d6cca339903c7d8c62cfb95
Pugabyte <pugabyte@projecteden.gg>
2021-02-08 19:49:30 -0600

    Start nameban command

--- src/main/java/me/pugabyte/nexus/features/commands/staff/NameBanCommand.java
+++ src/main/java/me/pugabyte/nexus/features/commands/staff/NameBanCommand.java
@@ -2,14 +2,19 @@ package me.pugabyte.nexus.features.commands.staff;
 
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import me.pugabyte.nexus.Nexus;
 import me.pugabyte.nexus.framework.commands.models.CustomCommand;
 import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
+import me.pugabyte.nexus.models.nameban.NameBanConfig;
+import me.pugabyte.nexus.models.nameban.NameBanConfigService;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 @NoArgsConstructor
 public class NameBanCommand extends CustomCommand implements Listener {
+	final NameBanConfigService service = new NameBanConfigService();
+	final NameBanConfig config = service.get(Nexus.getUUID0());
 
 	public NameBanCommand(@NonNull CommandEvent event) {
 		super(event);
