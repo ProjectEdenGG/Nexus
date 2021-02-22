5ac7f118a3a45e74c934b8d14ad8887f6fda0001
Blast-MC <cjblanton2@gmail.com>
2021-02-22 02:03:05 -0500

    improve custom recipe menu

--- src/main/java/me/pugabyte/nexus/features/recipes/menu/CustomRecipesMenu.java
+++ src/main/java/me/pugabyte/nexus/features/recipes/menu/CustomRecipesMenu.java
@@ -4,8 +4,9 @@ import fr.minuskube.inv.ClickableItem;
 import fr.minuskube.inv.SmartInventory;
 import fr.minuskube.inv.content.InventoryContents;
 import fr.minuskube.inv.content.InventoryProvider;
+import fr.minuskube.inv.content.Pagination;
+import fr.minuskube.inv.content.SlotIterator;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import me.pugabyte.nexus.features.menus.MenuUtils;
 import me.pugabyte.nexus.features.recipes.models.NexusRecipe;
 import me.pugabyte.nexus.features.recipes.models.RecipeType;
@@ -31,12 +32,38 @@ public class CustomRecipesMenu {
 				.open(player);
 	}
 
-	@RequiredArgsConstructor
+	public static SmartInventory getMenu(RecipeType type) {
+		return SmartInventory.builder()
+				.title("Custom Recipes")
+				.size(3, 9)
+				.provider(new CustomRecipesProvider(type))
+				.build();
+	}
+
+	public static void open(NexusRecipe recipe, Player player) {
+		SmartInventory.builder()
+				.title("Custom Recipes")
+				.size(3, 9)
+				.provider(new CustomRecipesProvider(recipe))
+				.build()
+				.open(player);
+	}
+
 	public static class CustomRecipesProvider extends MenuUtils implements InventoryProvider {
 
 		@NonNull
 		public RecipeType type;
+		public NexusRecipe recipe;
 		int[] inputSlots = {2, 3, 4, 11, 12, 13, 20, 21, 22};
+
+		public CustomRecipesProvider(@NonNull RecipeType type) {
+			this.type = type;
+		}
+
+		public CustomRecipesProvider(NexusRecipe recipe) {
+			this.type = RecipeType.FUNCTIONAL;
+			this.recipe = recipe;
+		}
 
 		@Override
 		public void init(Player player, InventoryContents contents) {
@@ -55,10 +82,41 @@ public class CustomRecipesMenu {
 					}
 					break;
 				default:
+					if (type == RecipeType.FUNCTIONAL && recipe == null) break;
 					contents.fill(ClickableItem.empty(new ItemBuilder(Material.WHITE_STAINED_GLASS_PANE).name(" ").build()));
 					for (int i : inputSlots)
 						contents.set(i, ClickableItem.NONE);
 					addBackItem(contents, e -> CustomRecipesMenu.open(RecipeType.MAIN, player));
+			}
+			if (type == RecipeType.FUNCTIONAL) {
+				if (recipe == null) {
+					Pagination page = contents.pagination();
+					addBackItem(contents, e -> CustomRecipesMenu.open(RecipeType.MAIN, player));
+					List<NexusRecipe> recipes = type.getRecipes().stream().filter(nexusRecipe -> {
+						if (nexusRecipe.getPermission() != null)
+							return player.hasPermission(nexusRecipe.getPermission());
+						return true;
+					}).collect(Collectors.toList());
+					ClickableItem[] clickableItems = new ClickableItem[recipes.size()];
+					for (int i = 0; i < clickableItems.length; i++) {
+						int j = i;
+						clickableItems[i] = ClickableItem.from(recipes.get(j).getResult(), e -> CustomRecipesMenu.open(recipes.get(j), player));
+					}
+					page.setItems(clickableItems);
+					page.setItemsPerPage(18);
+					page.addToIterator(contents.newIterator(SlotIterator.Type.HORIZONTAL, 1, 0));
+
+					// Arrows
+					if (!page.isFirst())
+						contents.set(0, 2, ClickableItem.from(new ItemBuilder(Material.ARROW).name("<-- Back").build(), e ->
+								CustomRecipesMenu.getMenu(type).open(player, page.previous().getPage())));
+					if (!page.isLast())
+						contents.set(0, 6, ClickableItem.from(new ItemBuilder(Material.ARROW).name("Next -->").build(), e ->
+								CustomRecipesMenu.getMenu(type).open(player, page.next().getPage())));
+				} else {
+					addBackItem(contents, e -> CustomRecipesMenu.open(RecipeType.FUNCTIONAL, player));
+					addRecipeToMenu(contents, recipe);
+				}
 			}
 		}
 
@@ -67,7 +125,8 @@ public class CustomRecipesMenu {
 
 		@Override
 		public void update(Player player, InventoryContents contents) {
-			if (type == RecipeType.MAIN) return;
+			if (type == RecipeType.MAIN || type == RecipeType.FUNCTIONAL) return;
+
 			ticks++;
 			if (ticks == 20) ticks = 0;
 			if (ticks != 1) return;
@@ -81,6 +140,10 @@ public class CustomRecipesMenu {
 			NexusRecipe recipe = recipes.get(index);
 			for (int i : inputSlots)
 				contents.set(i, ClickableItem.NONE);
+			addRecipeToMenu(contents, recipe);
+		}
+
+		public void addRecipeToMenu(InventoryContents contents, NexusRecipe recipe) {
 			contents.set(1, 6, ClickableItem.empty(recipe.getResult()));
 			if (recipe.getPattern() != null) {
 				Map<Character, ItemStack> characterItemStackMap = new HashMap<>();
@@ -109,6 +172,7 @@ public class CustomRecipesMenu {
 					contents.set(inputSlots[slot], ClickableItem.empty(new ItemStack(RandomUtils.randomElement(recipe.getMaterialChoice().getChoices()))));
 			}
 		}
+
 	}
 
 
