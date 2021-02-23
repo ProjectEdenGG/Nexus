eef54da256f9f3745eb0dbc33f5971826954a55a
Pugabyte <pugabyte@projecteden.gg>
2021-02-23 15:51:55 -0600

    Fix killall with large radius

--- src/main/java/me/pugabyte/nexus/features/commands/staff/admin/KillEntityCommand.java
+++ src/main/java/me/pugabyte/nexus/features/commands/staff/admin/KillEntityCommand.java
@@ -13,6 +13,7 @@ import me.pugabyte.nexus.framework.commands.models.annotations.TabCompleterFor;
 import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
 import me.pugabyte.nexus.framework.exceptions.postconfigured.InvalidInputException;
 import me.pugabyte.nexus.utils.EntityUtils;
+import org.bukkit.Chunk;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -20,9 +21,9 @@ import org.bukkit.entity.Monster;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Permission("group.seniorstaff")
 @Aliases({"killall", "mobkill", "butcher", "killentities"})
@@ -92,9 +93,13 @@ public class KillEntityCommand extends CustomCommand {
 		toKill.remove(EntityType.PLAYER);
 
 		Runnable kill = () -> {
-			Set<Entity> entities = EntityUtils.getNearbyEntities(player().getLocation(), radius).keySet()
-					.stream().filter(entity -> toKill.contains(entity.getType()))
-					.collect(Collectors.toSet());
+			Set<Entity> entities = new HashSet<>();
+			for (final Chunk chunk : player().getWorld().getLoadedChunks())
+				for (final Entity entity : chunk.getEntities())
+					if (location().distanceSquared(entity.getLocation()) <= radius)
+						if (toKill.contains(entity.getType()))
+							entities.add(entity);
+
 			entities.forEach(Entity::remove);
 			send(PREFIX + "Killed " + entities.size() + " entities");
 		};
