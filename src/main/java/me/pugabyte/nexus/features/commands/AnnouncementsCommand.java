9e7005d0983f6ed10c5922ad377c8905beaf044f
Pugabyte <pugabyte@projecteden.gg>
2021-03-02 13:17:54 -0600

    Add hover/command/suggest to announcements, bugfixes

--- src/main/java/me/pugabyte/nexus/features/commands/AnnouncementsCommand.java
+++ src/main/java/me/pugabyte/nexus/features/commands/AnnouncementsCommand.java
@@ -1,5 +1,6 @@
 package me.pugabyte.nexus.features.commands;
 
+import joptsimple.internal.Strings;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -41,6 +42,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.time.LocalDateTime;
@@ -131,6 +133,10 @@ public class AnnouncementsCommand extends CustomCommand implements Listener {
 		line();
 		send(json(" &3Text: &7" + announcement.getText()).hover("&3Click to edit").suggest("/announcements edit text " + announcement.getId() + " " + announcement.getText()));
 		line();
+		send(json(" &3Hover: &7" + announcement.getHover()).hover("&3Click to edit").suggest("/announcements edit hover " + announcement.getId() + " " + (announcement.getHover() == null ? "" : announcement.getHover())));
+		send(json(" &3Command: &7" + announcement.getCommand()).hover("&3Click to edit").suggest("/announcements edit command " + announcement.getId() + " " + (announcement.getCommand() == null ? "" : announcement.getCommand())));
+		send(json(" &3Suggest: &7" + announcement.getSuggest()).hover("&3Click to edit").suggest("/announcements edit suggest " + announcement.getId() + " " + (announcement.getSuggest() == null ? "" : announcement.getSuggest())));
+		line();
 
 		if (announcement.isEnabled())
 			send(json(" " + StringUtils.CHECK + " Enabled").hover("&3Click to &cdisable").command("/announcements disable " + announcement.getId()));
@@ -149,9 +155,9 @@ public class AnnouncementsCommand extends CustomCommand implements Listener {
 		showPermissionsEdit(announcement, announcement.getHidePermissions(), "hidePermissions");
 
 		send(json(" &3Start time: &e" + (announcement.getStartTime() == null ? "None" : shortDateTimeFormat(announcement.getStartTime())))
-				.hover("&3Click to edit").suggest("/announcements edit startTime " + announcement.getId() + " YYYY-MM-DDTHH:MM:SS.ZZZ"));
+				.hover("&3Click to edit").suggest("/announcements edit startTime " + announcement.getId() + " YYYY-MM-DDTHH:MM:SS"));
 		send(json(" &3End time: &e" + (announcement.getEndTime() == null ? "None" : shortDateTimeFormat(announcement.getEndTime())))
-				.hover("&3Click to edit").suggest("/announcements edit endTime " + announcement.getId() + " YYYY-MM-DDTHH:MM:SS.ZZZ"));
+				.hover("&3Click to edit").suggest("/announcements edit endTime " + announcement.getId() + " YYYY-MM-DDTHH:MM:SS"));
 
 		send(json(" &3Condition: &e" + (announcement.getCondition() == null ? "None" : camelCase(announcement.getCondition())))
 				.hover("&3Click to edit").suggest("/announcements edit condition " + announcement.getId() + " "));
@@ -187,6 +193,24 @@ public class AnnouncementsCommand extends CustomCommand implements Listener {
 	@Path("edit text <id> <text...>")
 	void editText(Announcement announcement, String text) {
 		announcement.setText(text);
+		saveAndEdit(announcement);
+	}
+
+	@Path("edit hover <id> <text...>")
+	void editHover(Announcement announcement, String hover) {
+		announcement.setHover(hover);
+		saveAndEdit(announcement);
+	}
+
+	@Path("edit command <id> <text...>")
+	void editCommand(Announcement announcement, String command) {
+		announcement.setCommand(command);
+		saveAndEdit(announcement);
+	}
+
+	@Path("edit suggest <id> <text...>")
+	void editSuggest(Announcement announcement, String suggest) {
+		announcement.setSuggest(suggest);
 		saveAndEdit(announcement);
 	}
 
@@ -369,19 +393,31 @@ public class AnnouncementsCommand extends CustomCommand implements Listener {
 		public static class Announcement implements ConfigurationSerializable {
 			private String id;
 			private String text;
+			private String hover;
+			private String command;
+			private String suggest;
+			@Builder.Default
 			private boolean enabled = true;
 			private boolean motd;
+			@Builder.Default
 			private Set<String> showPermissions = new HashSet<>();
+			@Builder.Default
 			private Set<String> hidePermissions = new HashSet<>();
 			private LocalDateTime startTime;
 			private LocalDateTime endTime;
 			private Announcement.AnnouncementCondition condition;
 
+			@Getter
+			private static final String PREFIX = "&8&l[&b⚡&8&l] &7";
+
 			public Announcement(Map<String, Object> map) {
 				this.id = (String) map.getOrDefault("id", id);
 				this.text = (String) map.getOrDefault("text", text);
-				this.enabled = (boolean) map.getOrDefault("enabled", enabled);
-				this.motd = (boolean) map.getOrDefault("motd", motd);
+				this.hover = (String) map.getOrDefault("hover", hover);
+				this.command = (String) map.getOrDefault("command", command);
+				this.suggest = (String) map.getOrDefault("suggest", suggest);
+				this.enabled = map.get("enabled") != null ? (boolean) map.get("enabled") : enabled;
+				this.motd = map.get("motd") != null ? (boolean) map.get("motd") : motd;
 				this.showPermissions = map.get("showPermissions") != null ? new HashSet<>((List<String>) map.get("showPermissions")) : new HashSet<>();
 				this.hidePermissions = map.get("hidePermissions") != null ? new HashSet<>((List<String>) map.get("hidePermissions")) : new HashSet<>();
 				this.startTime = map.get("startTime") != null ? new LocalDateTimeConverter().decode(map.getOrDefault("startTime", startTime)) : null;
@@ -398,6 +434,9 @@ public class AnnouncementsCommand extends CustomCommand implements Listener {
 				return new LinkedHashMap<String, Object>() {{
 					put("id", id);
 					put("text", text);
+					put("hover", hover);
+					put("command", command);
+					put("suggest", suggest);
 					put("enabled", enabled);
 					put("motd", motd);
 					put("showPermissions", new ArrayList<>(showPermissions));
@@ -413,9 +452,18 @@ public class AnnouncementsCommand extends CustomCommand implements Listener {
 					PlayerUtils.send(player, text);
 				} else {
 					PlayerUtils.send(player, "");
-					PlayerUtils.send(player, "&8&l[&b⚡&8&l] &7" + text);
+					PlayerUtils.send(player, getJson());
 					PlayerUtils.send(player, "");
 				}
+			}
+
+			@NotNull
+			private JsonBuilder getJson() {
+				JsonBuilder json = new JsonBuilder(PREFIX + text);
+				if (!Strings.isNullOrEmpty(hover)) json.hover(hover);
+				if (!Strings.isNullOrEmpty(command)) json.command(command);
+				if (!Strings.isNullOrEmpty(suggest)) json.suggest(suggest);
+				return json;
 			}
 
 			public boolean test(Player player) {
