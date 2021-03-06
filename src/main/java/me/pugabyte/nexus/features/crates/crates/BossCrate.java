ff6039d9ebc6fafbbf07dc4496a5edf9725b4239
Blast-MC <cjblanton2@gmail.com>
2021-03-06 00:47:20 -0500

    fix boss crate hologram

--- src/main/java/me/pugabyte/nexus/features/crates/crates/BossCrate.java
+++ src/main/java/me/pugabyte/nexus/features/crates/crates/BossCrate.java
@@ -2,6 +2,7 @@ package me.pugabyte.nexus.features.crates.crates;
 
 import me.pugabyte.nexus.features.crates.models.Crate;
 import me.pugabyte.nexus.features.crates.models.CrateType;
+import me.pugabyte.nexus.models.nerd.Rank;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +17,9 @@ public class BossCrate extends Crate {
 	@Override
 	public List<String> getCrateHologramLines() {
 		return new ArrayList<String>() {{
-			add("&8&l--&k=&f&l[+]=&k-&f&l-");
-			add("&f&k[+] &8&lBoss Crate &f&k[+]");
-			add("&f&l-&k-=&f&l[+&k]&f&l=--");
+			add("&f&l--=[+]=--");
+			add("&f&k[+] " + Rank.OWNER.getColor() + "&lBoss Crate &f&k[+]");
+			add("&f&l--=[+]=--");
 			add("&cComing Soon...");
 		}};
 	}
