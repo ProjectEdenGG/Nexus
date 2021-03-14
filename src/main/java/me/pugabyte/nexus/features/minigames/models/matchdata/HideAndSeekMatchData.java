922a79a501aa7cd8bdb957d761c035b57be8411e
lexikiq <noellekiq@gmail.com>
2021-03-14 03:34:49 -0400

    add HnS block blacklist

--- src/main/java/me/pugabyte/nexus/features/minigames/models/matchdata/HideAndSeekMatchData.java
+++ src/main/java/me/pugabyte/nexus/features/minigames/models/matchdata/HideAndSeekMatchData.java
@@ -7,27 +7,47 @@ import me.pugabyte.nexus.features.minigames.models.Match;
 import me.pugabyte.nexus.features.minigames.models.MatchData;
 import me.pugabyte.nexus.features.minigames.models.Minigamer;
 import me.pugabyte.nexus.features.minigames.models.annotations.MatchDataFor;
+import me.pugabyte.nexus.utils.MaterialTag;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Data
 @MatchDataFor(HideAndSeek.class)
 public class HideAndSeekMatchData extends MatchData {
+	private static final Set<Material> BANNED_MATERIALS = new HashSet<>();
 	private final Map<UUID, Disguise> disguises = new HashMap<>();
 	private final Map<UUID, Material> blockChoices = new HashMap<>();
-	private final List<Material> mapMaterials = new ArrayList<>(arena.getBlockList());
+	private final List<Material> mapMaterials = arena.getBlockList().stream().filter(material -> !BANNED_MATERIALS.contains(material) && material.isBlock()).collect(Collectors.toList());
 	private final Map<UUID, FallingBlock> solidBlocks = new HashMap<>();
 	private final Map<Minigamer, Location> solidPlayers = new HashMap<>();
 	private final Random random = new Random();
+
+	static {
+		// drunk wakka told me to use MaterialTag.INVENTORY_BLOCKS but that includes shit like barrels and furnaces and anvils and a whole lot of perfectly fine & functional blocks
+		// so i'm just manually doing these instead
+		BANNED_MATERIALS.addAll(Arrays.asList(
+				Material.CHEST,
+				Material.TRAPPED_CHEST,
+				Material.ENDER_CHEST,
+				Material.BARRIER,
+				Material.AIR
+		));
+
+		// i've found some broken walls that don't have hitboxes before from conversion errors, so just to be safe...
+		BANNED_MATERIALS.addAll(MaterialTag.WALLS.getValues());
+	}
 
 	public HideAndSeekMatchData(Match match) {
 		super(match);
