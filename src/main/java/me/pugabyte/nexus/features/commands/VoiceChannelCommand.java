9743b62ea1453908e84e925a89954a8431e4bec0
lexikiq <noellekiq@gmail.com>
2021-03-15 23:45:14 -0400

    reuse minigame voice channels set

--- src/main/java/me/pugabyte/nexus/features/commands/VoiceChannelCommand.java
+++ src/main/java/me/pugabyte/nexus/features/commands/VoiceChannelCommand.java
@@ -1,10 +1,7 @@
 package me.pugabyte.nexus.features.commands;
 
-import com.google.common.collect.ImmutableSet;
 import lombok.NonNull;
 import me.pugabyte.nexus.features.discord.Discord;
-import me.pugabyte.nexus.features.discord.DiscordId;
-import me.pugabyte.nexus.features.minigames.models.mechanics.multiplayer.teams.TeamMechanic;
 import me.pugabyte.nexus.framework.commands.models.CustomCommand;
 import me.pugabyte.nexus.framework.commands.models.annotations.Path;
 import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
@@ -12,13 +9,10 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
+import static me.pugabyte.nexus.features.minigames.models.mechanics.multiplayer.teams.TeamMechanic.MINIGAME_VOICE_CHANNELS;
+import static me.pugabyte.nexus.features.minigames.models.mechanics.multiplayer.teams.TeamMechanic.getVoiceChannelMember;
 
 public class VoiceChannelCommand extends CustomCommand {
-	private static final Set<String> VALID_VOICE_CHANNELS = ImmutableSet.copyOf(Arrays.stream(DiscordId.VoiceChannel.values()).map(DiscordId.VoiceChannel::getId).collect(Collectors.toList()));
-
 	public VoiceChannelCommand(@NonNull CommandEvent event) {
 		super(event);
 	}
@@ -33,10 +27,10 @@ public class VoiceChannelCommand extends CustomCommand {
 		if (vc == null)
 			error("Could not find that voice channel");
 
-		if (!VALID_VOICE_CHANNELS.contains(channel))
+		if (!MINIGAME_VOICE_CHANNELS.contains(channel))
 			error("You can only move to Minigame voice channels");
 
-		Member member = TeamMechanic.getVoiceChannelMember(player());
+		Member member = getVoiceChannelMember(player());
 		if (member == null)
 			error("Could not find you in a voice channel");
 
