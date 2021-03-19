68c9bb13e4f792d2fd1e314d870489d8eddea8a6
Pugabyte <pugabyte@projecteden.gg>
2021-03-19 12:57:35 -0500

    Better way of preventing egg teleport in spawn

--- src/main/java/me/pugabyte/nexus/features/events/y2021/easter21/Easter21Command.java
+++ src/main/java/me/pugabyte/nexus/features/events/y2021/easter21/Easter21Command.java
@@ -5,13 +5,12 @@ import me.pugabyte.nexus.features.warps.commands._WarpCommand;
 import me.pugabyte.nexus.framework.commands.models.annotations.Permission;
 import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
 import me.pugabyte.nexus.models.warps.WarpType;
-import me.pugabyte.nexus.utils.Utils.ActionGroup;
 import me.pugabyte.nexus.utils.WorldGuardUtils;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.BlockFromToEvent;
 
 @NoArgsConstructor
 @Permission("group.admin")
@@ -27,15 +26,9 @@ public class Easter21Command extends _WarpCommand implements Listener {
 	}
 
 	@EventHandler
-	public void onEggInteract(PlayerInteractEvent event) {
-		if (!ActionGroup.CLICK_BLOCK.applies(event))
-			return;
-
-		Block block = event.getClickedBlock();
-		if (block == null)
-			return;
-
-		if (block.getType() != Material.DRAGON_EGG)
+	public void onEggTeleport(BlockFromToEvent event) {
+		Block block = event.getBlock();
+		if (event.getBlock().getType() != Material.DRAGON_EGG)
 			return;
 
 		WorldGuardUtils worldGuardUtils = new WorldGuardUtils(block.getWorld());
