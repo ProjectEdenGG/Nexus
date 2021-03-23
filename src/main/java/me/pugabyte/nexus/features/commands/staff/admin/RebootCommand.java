e38374ea83d056f0f006b7a17976c44eba2c0233
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-03-23 16:03:14 -0500

    add titles & kick to RebootCommand

--- src/main/java/me/pugabyte/nexus/features/commands/staff/admin/RebootCommand.java
+++ src/main/java/me/pugabyte/nexus/features/commands/staff/admin/RebootCommand.java
@@ -8,6 +8,9 @@ import me.pugabyte.nexus.framework.commands.models.annotations.Confirm;
 import me.pugabyte.nexus.framework.commands.models.annotations.Path;
 import me.pugabyte.nexus.framework.commands.models.annotations.Permission;
 import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
+import me.pugabyte.nexus.utils.Tasks;
+import me.pugabyte.nexus.utils.Time;
+import me.pugabyte.nexus.utils.TitleUtils;
 
 @Permission("group.admin")
 public class RebootCommand extends CustomCommand {
@@ -20,7 +23,7 @@ public class RebootCommand extends CustomCommand {
 
 	@Path
 	@Confirm
-	void run() {
+	void reboot() {
 		if (queued) {
 			queued = false;
 			send(PREFIX + "Cancelled");
@@ -32,6 +35,19 @@ public class RebootCommand extends CustomCommand {
 		ReloadCondition.MINIGAMES.run();
 		ReloadCondition.CRATES.run();
 		ReloadCondition.WITHER.run();
+
+		// For testing purposes
+		TitleUtils.sendTitle(player(), "&cRebooting server,", "&cbrb ~60 seconds");
+		Tasks.wait(Time.SECOND.x(10), () -> player().kickPlayer("Rebooting server! Join back in about 60 seconds"));
+
+		// Actual code
+//		TitleUtils.sendTitleToAllPlayers("&cRebooting server,", "&cbrb ~60 seconds");
+//		Tasks.wait(Time.SECOND.x(10), () -> {
+//			for (Player player : Bukkit.getOnlinePlayers())
+//				player.kickPlayer("Rebooting server! Join back in about 60 seconds");
+//		});
+
+
 	}
 
 	static {
