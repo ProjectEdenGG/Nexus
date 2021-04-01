94eee557274f65c4064505a9c9b2895842a1282f
Pugabyte <pugabyte@projecteden.gg>
2021-04-01 12:45:49 -0500

    Add /easter topLocations

--- src/main/java/me/pugabyte/nexus/features/events/y2021/easter21/EasterCommand.java
+++ src/main/java/me/pugabyte/nexus/features/events/y2021/easter21/EasterCommand.java
@@ -12,6 +12,8 @@ import me.pugabyte.nexus.models.easter21.Easter21UserService;
 import me.pugabyte.nexus.models.warps.Warp;
 import me.pugabyte.nexus.models.warps.WarpService;
 import me.pugabyte.nexus.models.warps.WarpType;
+import me.pugabyte.nexus.utils.JsonBuilder;
+import me.pugabyte.nexus.utils.Utils;
 import me.pugabyte.nexus.utils.Utils.ActionGroup;
 import me.pugabyte.nexus.utils.WorldGuardUtils;
 import org.bukkit.Location;
@@ -24,10 +26,16 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 
 import static java.util.stream.Collectors.toList;
+import static me.pugabyte.nexus.utils.StringUtils.getShorterLocationString;
+import static me.pugabyte.nexus.utils.StringUtils.getTeleportCommand;
 
 @NoArgsConstructor
 public class EasterCommand extends CustomCommand implements Listener {
@@ -51,6 +59,23 @@ public class EasterCommand extends CustomCommand implements Listener {
 
 		send(PREFIX + "Top egg hunters  &3|  Total: &e" + sum);
 		paginate(all, (user, index) -> json("&3" + index + " &e" + user.getNickname() + " &7- " + user.getFound().size()), "/easter top", page);
+	}
+
+	@Path("topLocations [page]")
+	@Permission("group.admin")
+	void topLocations(@Arg("1") int page) {
+		Map<Location, Integer> counts = new HashMap<Location, Integer>() {{
+			for (Easter21User user : new Easter21UserService().<Easter21User>getAll())
+				for (Location location : user.getFound())
+					put(location, getOrDefault(location, 0) + 1);
+		}};
+
+		send(PREFIX + "Most found eggs");
+		BiFunction<Location, String, JsonBuilder> formatter = (location, index) ->
+				json("&3" + index + " &e" + getShorterLocationString(location) + " &7- " + counts.get(location))
+						.command(getTeleportCommand(location))
+						.hover("&eClick to teleport");
+		paginate(new ArrayList<>(Utils.sortByValueReverse(counts).keySet()), formatter, "/easter topLocations", page);
 	}
 
 	@Path("start")
--- src/main/java/me/pugabyte/nexus/utils/StringUtils.java
+++ src/main/java/me/pugabyte/nexus/utils/StringUtils.java
@@ -683,15 +683,16 @@ public class StringUtils {
 		return (int) loc.getX() + " " + (int) loc.getY() + " " +  (int) loc.getZ() + " " + loc.getWorld().getName();
 	}
 
-	public static void sendJsonLocation(String message, Location location, Player player) {
-		int x = (int) location.getX();
-		int y = (int) location.getY();
-		int z = (int) location.getZ();
-		double yaw = location.getYaw();
-		double pitch = location.getPitch();
-		String world = location.getWorld().getName();
+	public static String getShorterLocationString(Location loc) {
+		return (int) loc.getX() + " " + (int) loc.getY() + " " +  (int) loc.getZ();
+	}
 
-		new JsonBuilder().next(message).command("/tppos " + x + " " + y + " " + z + " " + yaw + " " + pitch + " " + world).send(player);
+	public static void sendJsonLocation(String message, Location location, Player player) {
+		new JsonBuilder().next(message).command(getTeleportCommand(location)).send(player);
+	}
+
+	public static String getTeleportCommand(Location location) {
+		return "/tppos " + (int) location.getX() + " " + (int) location.getY() + " " + (int) location.getZ() + " " + location.getYaw() + " " + location.getPitch() + " " + location.getWorld().getName();
 	}
 
 	private static final String HASTEBIN = "https://paste.bnn.gg/";
