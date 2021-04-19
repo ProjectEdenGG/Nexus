983d96c7b8d172dc0379fa18243999bd5b92c596
lexikiq <noellekiq@gmail.com>
2021-04-18 21:37:25 -0400

    fix minigame scoreboards

--- src/main/java/me/pugabyte/nexus/framework/interfaces/Colored.java
+++ src/main/java/me/pugabyte/nexus/framework/interfaces/Colored.java
@@ -3,6 +3,7 @@ package me.pugabyte.nexus.framework.interfaces;
 import net.kyori.adventure.text.format.TextColor;
 import net.md_5.bungee.api.ChatColor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
 
@@ -17,6 +18,7 @@ public interface Colored {
 
 	/**
 	 * Returns the chat color corresponding to this object.
+	 * <br>This method does not use official color codes and thus will not work with Scoreboards or other displays that only use strings.
 	 */
 	default @NotNull ChatColor getChatColor() {
 		return ChatColor.of(getColor());
@@ -34,6 +36,13 @@ public interface Colored {
 	 */
 	default @NotNull org.bukkit.Color getBukkitColor() {
 		return org.bukkit.Color.fromRGB(getColor().getRGB());
+	}
+
+	/**
+	 * Returns the Bukkit ChatColor corresponding to this object, which is one of the vanilla chat colors.
+	 */
+	default @Nullable org.bukkit.ChatColor getBukkitChatColor() {
+		return null;
 	}
 
 	/**
