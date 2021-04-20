c50d03d7828c765028a020e549d8affc3631b972
lexikiq <noellekiq@gmail.com>
2021-04-20 14:31:36 -0400

    Fix ParticleProjectile

--- src/main/java/me/pugabyte/nexus/features/minigames/models/perks/ParticleProjectile.java
+++ src/main/java/me/pugabyte/nexus/features/minigames/models/perks/ParticleProjectile.java
@@ -5,23 +5,26 @@ import me.pugabyte.nexus.features.minigames.models.Match;
 import me.pugabyte.nexus.features.minigames.models.perks.common.ParticleProjectilePerk;
 import me.pugabyte.nexus.utils.Tasks;
 import org.bukkit.entity.Projectile;
+import org.bukkit.scheduler.BukkitRunnable;
 
 @Data
 public class ParticleProjectile {
     private final ParticleProjectilePerk perk;
     private final Projectile projectile;
-    private final int taskId;
     private final Match match;
     public ParticleProjectile(ParticleProjectilePerk perk, Projectile projectile, Match match) {
         this.perk = perk;
         this.projectile = projectile;
         this.match = match;
-        this.taskId = Tasks.repeat(1, 1, () -> {
-            if (!projectile.isValid() || projectile.isDead() || projectile.isOnGround()) {
-                Tasks.cancel(getTaskId());
-                return;
+        Tasks.repeat(1, 1, new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (!projectile.isValid() || projectile.isDead() || projectile.isOnGround()) {
+                    cancel();
+                    return;
+                }
+                perk.tick(projectile, match);
             }
-            perk.tick(projectile, match);
         });
     }
 }
