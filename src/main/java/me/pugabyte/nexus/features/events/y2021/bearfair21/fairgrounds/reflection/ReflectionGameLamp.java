b2b2b1556f9fed3a240a3847a7d51e9c2cd88b7f
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-04-22 15:07:46 -0500

    bf21 work: better colors

--- src/main/java/me/pugabyte/nexus/features/events/y2021/bearfair21/fairgrounds/reflection/ReflectionGameLamp.java
+++ src/main/java/me/pugabyte/nexus/features/events/y2021/bearfair21/fairgrounds/reflection/ReflectionGameLamp.java
@@ -9,19 +9,19 @@ import org.bukkit.Material;
 
 @AllArgsConstructor
 public enum ReflectionGameLamp {
-	SOUL_SOIL(Material.SOUL_SOIL, loc(57, -99), 3, 9, ChatColor.of("#452d15")),
+	SOUL_SOIL(Material.SOUL_SOIL, loc(57, -99), 3, 9, ChatColor.of("#593a1b")),
 	PURPUR(Material.PURPUR_BLOCK, loc(57, -96), 5, 9, ChatColor.of("#9c529c")),
-	LODESTONE(Material.LODESTONE, loc(57, -93), 3, 9, ChatColor.of("#7a7a7a")),
-	MAGMA(Material.MAGMA_BLOCK, loc(57, -90), 5, 9, ChatColor.of("#7a2f04")),
+	LODESTONE(Material.LODESTONE, loc(57, -93), 3, 9, ChatColor.of("#aaaaaa")),
+	MAGMA(Material.MAGMA_BLOCK, loc(57, -90), 5, 9, ChatColor.of("#b04609")),
 	KELP(Material.DRIED_KELP_BLOCK, loc(57, -87), 1, 9, ChatColor.of("#314a18")),
-	BEDROCK(Material.BEDROCK, loc(55, -85), 4, 10, ChatColor.of("#454545")),
+	BEDROCK(Material.BEDROCK, loc(55, -85), 4, 10, ChatColor.of("#828282")),
 	PUMPKIN(Material.PUMPKIN, loc(52, -85), 2, 8, ChatColor.of("#b36d17")),
 	MELON(Material.MELON, loc(46, -85), 2, 8, ChatColor.of("#84bd21")),
-	NETHERITE(Material.NETHERITE_BLOCK, loc(43, -85), 4, 10, ChatColor.of("#393739")),
+	NETHERITE(Material.NETHERITE_BLOCK, loc(43, -85), 4, 10, ChatColor.of("#696969")),
 	EMERALD(Material.EMERALD_ORE, loc(41, -87), 1, 9, ChatColor.of("#09af47")),
 	PRISMARINE(Material.DARK_PRISMARINE, loc(41, -90), 5, 9, ChatColor.of("#257a5c")),
-	HONEYCOMB(Material.HONEYCOMB_BLOCK, loc(41, -93), 3, 9, ChatColor.of("#e2b53b")),
-	ICE(Material.BLUE_ICE, loc(41, -96), 5, 9, ChatColor.of("#7eafcb")),
+	HONEYCOMB(Material.HONEYCOMB_BLOCK, loc(41, -93), 3, 9, ChatColor.of("#e29f3b")),
+	ICE(Material.BLUE_ICE, loc(41, -96), 5, 9, ChatColor.of("#68bae8")),
 	SPONGE(Material.SPONGE, loc(41, -99), 3, 9, ChatColor.of("#c3c63d")),
 	;
 
