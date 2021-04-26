e9dedb7d92249972bc8fff658fb764243ffa6f77
lexikiq <noellekiq@gmail.com>
2021-04-26 16:52:26 -0400

    Fix ColoredAndNicknamed#getVanillaColoredName

--- src/main/java/me/pugabyte/nexus/framework/interfaces/ColoredAndNicknamed.java
+++ src/main/java/me/pugabyte/nexus/framework/interfaces/ColoredAndNicknamed.java
@@ -14,6 +14,13 @@ public interface ColoredAndNicknamed extends ColoredAndNamed, Nicknamed {
 	}
 
 	/**
+	 * Returns this object's nickname with a vanilla chat color per {@link #getVanillaChatColor()} prefixed.
+	 */
+	default @NotNull String getVanillaColoredName() {
+		return getVanillaChatColor() + getNickname();
+	}
+
+	/**
 	 * Returns a component corresponding with this object. Uses the object's nickname by default. May include colors.
 	 * @return an adventure text component
 	 */
