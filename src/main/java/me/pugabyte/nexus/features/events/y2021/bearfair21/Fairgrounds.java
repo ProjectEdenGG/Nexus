261a4a47f688164247e5ae325a88275223d20ba2
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-04-27 00:05:20 -0500

    bf21 work

--- src/main/java/me/pugabyte/nexus/features/events/y2021/bearfair21/Fairgrounds.java
+++ src/main/java/me/pugabyte/nexus/features/events/y2021/bearfair21/Fairgrounds.java
@@ -1,6 +1,7 @@
 package me.pugabyte.nexus.features.events.y2021.bearfair21;
 
 import eden.utils.TimeUtils.Time;
+import me.pugabyte.nexus.Nexus;
 import me.pugabyte.nexus.features.events.y2021.bearfair21.fairgrounds.Archery;
 import me.pugabyte.nexus.features.events.y2021.bearfair21.fairgrounds.Frogger;
 import me.pugabyte.nexus.features.events.y2021.bearfair21.fairgrounds.Interactables;
@@ -28,9 +29,11 @@ public class Fairgrounds {
 	}
 
 	private void ridesTask() {
+		// Workaround for https://github.com/TheClowner/ccRides-Support/issues/31
 		AtomicBoolean resetRides = new AtomicBoolean(false);
 		List<String> rides = Arrays.asList("carousel", "chairswing", "droptower", "enterprise", "ferriswheel", "jets", "pendulum", "swingship", "swingtower", "teacups");
 
+		Nexus.log("Reloading all rides in bearfair...");
 		Tasks.repeat(Time.SECOND.x(5), Time.SECOND.x(15), () -> {
 			int count = BearFair21.getWGUtils().getPlayersInRegion(BearFair21.getRegion()).size();
 			if (count == 0)
