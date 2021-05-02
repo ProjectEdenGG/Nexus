c6a23e5f5fec044001425aff5ae03ca9c7f0295f
lexikiq <noellekiq@gmail.com>
2021-05-02 00:08:17 -0400

    Fix cooldown regex

--- src/main/java/me/pugabyte/nexus/models/cooldown/Cooldown.java
+++ src/main/java/me/pugabyte/nexus/models/cooldown/Cooldown.java
@@ -35,7 +35,7 @@ public class Cooldown implements PlayerOwnedObject {
 	private Map<String, LocalDateTime> cooldowns = new HashMap<>();
 
 	// I tried to find the actual valid characters but I don't understand BSON's spec so I gave up -lexi
-	private static final Pattern VALID_TYPE = Pattern.compile("^[a-z_-]+$", Pattern.CASE_INSENSITIVE);
+	private static final Pattern VALID_TYPE = Pattern.compile("^[a-z_:#-]+$", Pattern.CASE_INSENSITIVE);
 
 	public Cooldown(@NotNull UUID uuid) {
 		this.uuid = uuid;
@@ -43,7 +43,7 @@ public class Cooldown implements PlayerOwnedObject {
 
 	/**
 	 * Ensures the type does not use invalid characters
-	 * @param type an arbitrary string corresponding to the type of cooldown hopefully matching the regex ^[A-Za-z_-]+$
+	 * @param type an arbitrary string corresponding to the type of cooldown hopefully matching the regex ^[A-Za-z_:#-]+$
 	 * @throws IllegalArgumentException type uses invalid characters
 	 * @return the input with spaces replaced with underscores
 	 */
@@ -57,7 +57,7 @@ public class Cooldown implements PlayerOwnedObject {
 	 * Checks if a provided type has a saved cooldown time.
 	 * <p>
 	 * This method does not check if the saved time has expired or not.
-	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_-]+$
+	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_:#-]+$
 	 * @see #check(String) check(type)
 	 * @return true if a cooldown time is present
 	 */
@@ -68,7 +68,7 @@ public class Cooldown implements PlayerOwnedObject {
 
 	/**
 	 * Gets the expiry time for a provided cooldown.
-	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_-]+$
+	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_:#-]+$
 	 * @see #check(String) check(type)
 	 * @return expiration time of a cooldown or null if none is set
 	 */
@@ -83,7 +83,7 @@ public class Cooldown implements PlayerOwnedObject {
 	 * Returns true if the player is not on cooldown.
 	 * </p>
 	 * Unlike {@link CooldownService}, this does not create a new cooldown.
-	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_-]+$
+	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_:#-]+$
 	 * @return true if player is not on cooldown
 	 */
 	public boolean check(String type) {
@@ -95,7 +95,7 @@ public class Cooldown implements PlayerOwnedObject {
 	 * Creates a cooldown.
 	 * <p>
 	 * This method will override existing cooldowns.
-	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_-]+$
+	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_:#-]+$
 	 * @param ticks how long the cooldown should last in ticks
 	 * @return this object
 	 */
@@ -108,7 +108,7 @@ public class Cooldown implements PlayerOwnedObject {
 
 	/**
 	 * Clears a cooldown
-	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_-]+$
+	 * @param type an arbitrary string corresponding to the type of cooldown matching the regex ^[A-Za-z_:#-]+$
 	 */
 	public void clear(String type) {
 		type = checkType(type);
