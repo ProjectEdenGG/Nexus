830adcce555d4f686dffe72769487b0a41fe051b
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-05-04 15:02:37 -0500

    re-enable events

--- src/main/java/me/pugabyte/nexus/features/events/Events.java
+++ src/main/java/me/pugabyte/nexus/features/events/Events.java
@@ -1,19 +1,22 @@
 package me.pugabyte.nexus.features.events;
 
+import eden.annotations.Environments;
+import eden.utils.Env;
+import me.pugabyte.nexus.features.events.aeveonproject.AeveonProject;
 import me.pugabyte.nexus.features.events.y2021.bearfair21.BearFair21;
 import me.pugabyte.nexus.features.events.y2021.bearfair21.fairgrounds.minigolf.MiniGolf;
 import me.pugabyte.nexus.framework.features.Feature;
 import me.pugabyte.nexus.utils.StringUtils;
 
-//@Environments(Env.PROD)
+@Environments(Env.PROD)
 public class Events extends Feature {
 	public static String PREFIX = StringUtils.getPrefix("Events");
 
 	@Override
 	public void onStart() {
-//		new ArmorStandStalker();
-//		new ScavHuntLegacy();
-//		new AeveonProject();
+		new ArmorStandStalker();
+		new ScavHuntLegacy();
+		new AeveonProject();
 		new BearFair21();
 	}
 
