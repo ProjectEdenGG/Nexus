9895c8d71567a735080ec5fe32b431609d3d625a
lexikiq <noellekiq@gmail.com>
2021-05-09 21:09:44 -0400

    WorldGuardFlagUtils updates
    - Adds javadocs
    - Allows varargs
    - Fixes #queryValue(Location, Flag): now assumes ownership of regions like the other test/query methods
    - Fixes #query(Player, Flag): now properly considers if the player is an owner or member of any regions
    - Adds #canPlace(Player) and #canBreak(Player) to test if a player can place/break at their current location
    - Adds #hasBypass(Player) to test if a player has World Guard bypassing permissions in their current world

--- src/main/java/me/pugabyte/nexus/utils/WorldGuardFlagUtils.java
+++ src/main/java/me/pugabyte/nexus/utils/WorldGuardFlagUtils.java
@@ -6,7 +6,6 @@ import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.domains.Association;
-import com.sk89q.worldguard.protection.DelayedRegionOverlapAssociation;
 import com.sk89q.worldguard.protection.association.Associables;
 import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.flags.IntegerFlag;
@@ -26,28 +25,89 @@ import org.apache.commons.lang.Validate;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+/**
+ * Utilities for getting the state
+ */
 public class WorldGuardFlagUtils {
 
+	/**
+	 * Nexus's custom World Guard flags
+	 */
 	@AllArgsConstructor
 	public enum Flags {
+		/**
+		 * Enables showing the Pugmas snow effect to players in the region
+		 */
 		SNOW_EFFECT(registerFlag(new StateFlag("snow-effect", false))),
+		/**
+		 * Whether or not to allow item frames to break via means besides being destroyed by a player
+		 */
 		HANGING_BREAK(registerFlag(new StateFlag("hanging-break", false))),
+		/**
+		 * Toggles grass blocks becoming dirt
+		 */
 		GRASS_DECAY(registerFlag(new StateFlag("grass-decay", false))),
+		/**
+		 * Toggles hostile mob spawning
+		 */
 		HOSTILE_SPAWN(registerFlag(new StateFlag("hostile-spawn", false))),
+		/**
+		 * Allows a set of mobs to spawn
+		 */
 		ALLOW_SPAWN(registerFlag(new SetFlag("allow-spawn", new RegistryFlag(null, com.sk89q.worldedit.world.entity.EntityType.REGISTRY)))),
+		/**
+		 * Toggles mobs getting angry at entities
+		 */
 		MOB_AGGRESSION(registerFlag(new StateFlag("mob-aggression", false))),
+		/**
+		 * Toggles if players can tame animals
+		 */
 		TAMING(registerFlag(new StateFlag("taming", false))),
+		/**
+		 * Toggles if players can use trapdoors
+		 */
 		USE_TRAP_DOORS(registerFlag(new StateFlag("use-trap-doors", false))),
+		/**
+		 * Toggles if players get damaged while inside water
+		 */
 		MINIGAMES_WATER_DAMAGE(registerFlag(new StateFlag("minigames-water-damage", false))),
+		/**
+		 * Displays a message on a player's action bar upon entry
+		 */
 		GREETING_ACTIONBAR(registerFlag(new StringFlag("nexus-greeting-actionbar"))),
+		/**
+		 * Displays a message on a player's action bar upon exit
+		 */
 		FAREWELL_ACTIONBAR(registerFlag(new StringFlag("nexus-farewell-actionbar"))),
+		/**
+		 * Sets how long action bar messages should last in ticks
+		 */
 		ACTIONBAR_TICKS(registerFlag(new IntegerFlag("nexus-actionbar-ticks"))),
+		/**
+		 * Displays a title to a player upon entry
+		 */
 		GREETING_TITLE(registerFlag(new StringFlag("nexus-greeting-title"))),
+		/**
+		 * Displays a title to a player upon exit
+		 */
 		FAREWELL_TITLE(registerFlag(new StringFlag("nexus-farewell-title"))),
+		/**
+		 * Displays a subtitle to a player upon entry
+		 */
 		GREETING_SUBTITLE(registerFlag(new StringFlag("nexus-greeting-subtitle"))),
+		/**
+		 * Displays a subtitle to a player upon exit
+		 */
 		FAREWELL_SUBTITLE(registerFlag(new StringFlag("nexus-farewell-subtitle"))),
+		/**
+		 * Sets how long (sub)title messages should last in ticks
+		 */
 		TITLE_TICKS(registerFlag(new IntegerFlag("nexus-title-ticks"))),
+		/**
+		 * Sets how long (sub)title messages should fade in and out in ticks
+		 */
 		TITLE_FADE(registerFlag(new IntegerFlag("nexus-title-fade"))),
 		;
 
@@ -112,82 +172,236 @@ public class WorldGuardFlagUtils {
 //		}
 //	}
 
+	/**
+	 * Test if the effective value of a flag is {@code ALLOW}.
+	 *
+	 * <p>This method does not check the region bypass permission. That must be done by the calling code.</p>
+	 *
+	 * @param player player to check region membership and location
+	 * @param flag flag to check
+	 * @return true if the result was {@code ALLOW}
+	 * @see #hasBypass(HasPlayer)
+	 */
 	public static boolean test(HasPlayer player, Flags flag) {
 		return test(player, (StateFlag) flag.get());
 	}
 
-	public static boolean test(@NotNull HasPlayer player, @NotNull StateFlag flag) {
-		Validate.notNull(flag, "Flag cannot be null");
+	/**
+	 * Test whether the (effective) value for a list of state flags equals {@code ALLOW}.
+	 *
+	 * <p>This method does not check the region bypass permission. That must be done by the calling code.</p>
+	 *
+	 * @param player player to check region membership and location
+	 * @param flags flags to check
+	 * @return true if the result was {@code ALLOW}
+	 * @see #hasBypass(HasPlayer)
+	 */
+	public static boolean test(@NotNull HasPlayer player, @NotNull StateFlag... flags) {
+		Validate.notNull(flags, "Flag cannot be null");
 
 		Player _player = player.getPlayer();
 		LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(_player);
 		Location loc = BukkitAdapter.adapt(_player.getLocation());
 		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
-		return container.createQuery().testState(loc, localPlayer, flag);
+		return container.createQuery().testState(loc, localPlayer, flags);
 	}
 
+	/**
+	 * Test if the effective value of a flag is {@code ALLOW}.
+	 *
+	 * <p>This method assumes it is being run by the owner of a region.</p>
+	 *
+	 * @param location location to check from
+	 * @param flag flag to check
+	 * @return true if the result was {@code ALLOW}
+	 */
 	public static boolean test(org.bukkit.Location location, Flags flag) {
 		return test(location, (StateFlag) flag.get());
 	}
 
-	public static boolean test(org.bukkit.Location location, StateFlag flag) {
-		Validate.notNull(flag, "Flag cannot be null");
+	/**
+	 * Test whether the (effective) value for a list of state flags equals {@code ALLOW}.
+	 *
+	 * <p>This method assumes it is being run by the owner of a region.</p>
+	 *
+	 * @param location location to check from
+	 * @param flags flags to check
+	 * @return true if the result was {@code ALLOW}
+	 */
+	public static boolean test(org.bukkit.Location location, StateFlag... flags) {
+		Validate.notNull(flags, "Flag cannot be null");
 
 		Location loc = BukkitAdapter.adapt(location);
 		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
 		RegionQuery query = container.createQuery();
-		return query.testState(loc, new DelayedRegionOverlapAssociation(query, loc), flag);
+		return query.testState(loc, Associables.constant(Association.OWNER), flags);
 	}
 
+	/**
+	 * Gets the effective state of a flag. This follows state rules, where {@code DENY} overrides
+	 * {@code ALLOW} overrides {@code NONE}.
+	 *
+	 * <p>This method does not check the region bypass permission. That must be done by the calling code.</p>
+	 *
+	 * @param player player to check region membership and location
+	 * @param flag flag to check
+	 * @return effective state
+	 * @see #hasBypass(HasPlayer)
+	 */
+	@Nullable
 	public static State query(HasPlayer player, Flags flag) {
 		return query(player, (StateFlag) flag.get());
 	}
 
-	public static State query(HasPlayer player, StateFlag flag) {
-		Validate.notNull(flag, "Flag cannot be null");
+	/**
+	 * Gets the effective state for a list of flags. This follows state rules, where {@code DENY} overrides
+	 * {@code ALLOW} overrides {@code NONE}.
+	 *
+	 * <p>This method does not check the region bypass permission. That must be done by the calling code.</p>
+	 *
+	 * @param player player to check region membership and location
+	 * @param flags flags to check
+	 * @return effective state
+	 * @see #hasBypass(HasPlayer)
+	 */
+	@Nullable
+	public static State query(HasPlayer player, StateFlag... flags) {
+		Validate.notNull(flags, "Flag cannot be null");
 
+		LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(player.getPlayer());
 		Location loc = BukkitAdapter.adapt(player.getPlayer().getLocation());
 		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
 		RegionQuery query = container.createQuery();
-		return query.queryState(loc, new DelayedRegionOverlapAssociation(query, loc), flag);
+		return query.queryState(loc, localPlayer, flags);
 	}
 
+	/**
+	 * Gets the effective state of a flag. This follows state rules, where {@code DENY} overrides
+	 * {@code ALLOW} overrides {@code NONE}.
+	 *
+	 * <p>This method assumes it is being run by the owner of a region.</p>
+	 *
+	 * @param location location to check from
+	 * @param flag flag to check
+	 * @return effective state
+	 */
+	@Nullable
 	public static State query(org.bukkit.Location location, Flags flag) {
 		return query(location, (StateFlag) flag.get());
 	}
 
+	/**
+	 * Gets the effective state of a flag. This follows state rules, where {@code DENY} overrides
+	 * {@code ALLOW} overrides {@code NONE}.
+	 *
+	 * <p>This method assumes it is being run by the owner of a region.</p>
+	 *
+	 * @param block location to check from
+	 * @param flag flag to check
+	 * @return effective state
+	 */
+	@Nullable
 	public static State query(Block block, Flags flag) {
 		return query(block.getLocation(), (StateFlag) flag.get());
 	}
 
-	public static State query(org.bukkit.Location location, StateFlag flag) {
-		Validate.notNull(flag, "Flag cannot be null");
+	/**
+	 * Gets the effective state for a list of flags. This follows state rules, where {@code DENY} overrides
+	 * {@code ALLOW} overrides {@code NONE}.
+	 *
+	 * <p>This method assumes it is being run by the owner of a region.</p>
+	 *
+	 * @param location location to check from
+	 * @param flags flags to check
+	 * @return effective state
+	 */
+	@Nullable
+	public static State query(org.bukkit.Location location, StateFlag... flags) {
+		Validate.notNull(flags, "Flag cannot be null");
 
 		Location loc = BukkitAdapter.adapt(location);
 		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
 		RegionQuery query = container.createQuery();
-		return query.queryState(loc, new DelayedRegionOverlapAssociation(query, loc), flag);
+		return query.queryState(loc, Associables.constant(Association.OWNER), flags);
 	}
 
+	/**
+	 * Gets the effective value of a flag.
+	 *
+	 * <p>This method assumes it is being run by the owner of a region.</p>
+	 *
+	 * @param location location to check from
+	 * @param flag flag to check
+	 * @return effective value
+	 */
+	@Nullable
 	public static <T> T queryValue(org.bukkit.Location location, Flags flag) {
 		return queryValue(location, (Flag<T>) flag.get());
 	}
 
+	/**
+	 * Gets the effective value of a flag.
+	 *
+	 * <p>This method assumes it is being run by the owner of a region.</p>
+	 *
+	 * @param location location to check from
+	 * @param flag flag to check
+	 * @return effective value
+	 */
+	@Nullable
 	public static <T> T queryValue(org.bukkit.Location location, Flag<T> flag) {
 		Validate.notNull(flag, "Flag cannot be null");
 
 		Location loc = BukkitAdapter.adapt(location);
 		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
 		RegionQuery query = container.createQuery();
-		return query.queryValue(loc, Associables.constant(Association.NON_MEMBER), flag);
+		return query.queryValue(loc, Associables.constant(Association.OWNER), flag);
 	}
 
+	/**
+	 * Gets the effective value of a flag.
+	 *
+	 * <p>This method does not check the region bypass permission. That must be done by the calling code.</p>
+	 *
+	 * @param player player to check region membership and location
+	 * @param flag flag to check
+	 * @return effective value
+	 * @see #hasBypass(HasPlayer)
+	 */
+	@Nullable
 	public static <T> T getValueFor(HasPlayer player, Flag<T> flag) {
 		Player _player = player.getPlayer();
 		LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(_player);
 		com.sk89q.worldedit.util.Location loc = BukkitAdapter.adapt(_player.getLocation());
 		RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
 		return container.createQuery().queryValue(loc, localPlayer, flag);
+	}
+
+	/**
+	 * Tests if a player bypasses world guard flags
+	 * @param player player
+	 * @return if the player bypasses world guard flags
+	 */
+	public static boolean hasBypass(HasPlayer player) {
+		Player _player = player.getPlayer();
+		return WorldGuard.getInstance().getPlatform().getSessionManager().hasBypass(WorldGuardPlugin.inst().wrapPlayer(_player), BukkitAdapter.adapt(_player.getWorld()));
+	}
+
+	/**
+	 * Tests if a player can place blocks in their current region(s)
+	 * @return true if the player can place blocks
+	 */
+	public static boolean canPlace(HasPlayer player) {
+		// TODO: test block location too (requires some rewriting)
+		return hasBypass(player) || test(player, com.sk89q.worldguard.protection.flags.Flags.BUILD, com.sk89q.worldguard.protection.flags.Flags.BLOCK_PLACE);
+	}
+
+	/**
+	 * Tests if a player can break blocks in their current region(s)
+	 * @return true if the player can break blocks
+	 */
+	public static boolean canBreak(HasPlayer player) {
+		return hasBypass(player) || test(player, com.sk89q.worldguard.protection.flags.Flags.BUILD, com.sk89q.worldguard.protection.flags.Flags.BLOCK_BREAK);
 	}
 
 }
