bd8932a9b78b135380f090e4fa3679e9a0fe05df
Pugabyte <pugabyte@projecteden.gg>
2021-05-18 00:53:44 -0500

    Always use coordinates if /tppos, cleanup

--- src/main/java/me/pugabyte/nexus/features/commands/poof/TeleportCommand.java
+++ src/main/java/me/pugabyte/nexus/features/commands/poof/TeleportCommand.java
@@ -41,6 +41,7 @@ import static me.pugabyte.nexus.utils.StringUtils.getTeleportCommand;
 @Aliases({"tp", "tppos"})
 @Redirect(from = "/tpo", to = "/tp override")
 public class TeleportCommand extends CustomCommand implements Listener {
+	private static final String mapLinkRegex = "(http(s)?:\\/\\/)?(blue|staff)?map.projecteden.gg/#[a-zA-Z0-9_]+(:-?[0-9]+(\\.[0-9]+)?){8}.*";
 
 	public TeleportCommand(@NonNull CommandEvent event) {
 		super(event);
@@ -66,70 +67,80 @@ public class TeleportCommand extends CustomCommand implements Listener {
 			return;
 		}
 
-		if (arg1.matches("(http(s)?:\\/\\/)?(blue|staff)?map.projecteden.gg/#[a-zA-Z0-9_]+(:-?[0-9]+(\\.[0-9]+)?){8}.*")) {
-			String[] split = arg1.split("#");
-			String[] coords = split[1].split(":");
-			AtomicReference<String> worldName = new AtomicReference<>(coords[0]);
-			BlueMapAPI.getInstance().flatMap(api -> api.getMap(worldName.get())).ifPresent(map -> worldName.set(map.getWorld().getSaveFolder().toFile().getName()));
-
-			World world = Bukkit.getWorld(worldName.get());
-			if (world == null)
-				error("World &e" + worldName + " &cnot found");
-
-			double x = Double.parseDouble(coords[1]);
-			double z = Double.parseDouble(coords[3]);
-			double terrainHeight = Double.parseDouble(coords[2]);
-			double aboveGround = Double.parseDouble(coords[4]);
-			double y = terrainHeight + aboveGround;
-			if (y > 275) y = 275;
-
-			Location location = new Location(world, x, y, z, 0, 90);
-			player().teleportAsync(location, TeleportCause.COMMAND);
-			return;
-		}
-
-		if (isCoord(arg(1)) && isCoord(arg(2)) && isCoord(arg(3))) {
-			Location location = location();
-			Modify modifier = RelativeLocation.modify(location).x(arg(1)).y(arg(2)).z(arg(3));
-			if (arg(4) != null && arg(5) != null && isCoord(arg(4)) && isCoord(arg(5))) {
-				modifier.yaw(arg(4)).pitch(arg(5));
-				if (arg(6) != null) {
-					if (Bukkit.getWorld(arg(6)) == null)
-						error("World &e" + arg(6) + " &cnot found");
-					location.setWorld(Bukkit.getWorld(arg(6)));
-				}
-			} else if (!isNullOrEmpty(arg(4))) {
-				if (Bukkit.getWorld(arg(4)) == null)
-					error("World &e" + arg(4) + " &cnot found");
-				else
-					location.setWorld(Bukkit.getWorld(arg(4)));
-			}
-
-			player().teleportAsync(modifier.update(), TeleportCause.COMMAND);
-		} else if (isOfflinePlayerArg(1)) {
-			OfflinePlayer player1 = offlinePlayerArg(1);
-			Location location1 = Nerd.of(player1).getLocation();
-			if (isOfflinePlayerArg(2)) {
-				OfflinePlayer player2 = offlinePlayerArg(2);
-				if (player1.isOnline() && player1.getPlayer() != null) {
-					if (checkTeleportDisabled(player1.getPlayer(), player2))
-						return;
-
-					player1.getPlayer().teleportAsync(Nerd.of(player2).getLocation(), TeleportCause.COMMAND);
-					send(PREFIX + "Poofing to &e" + Nickname.of(player2) + (player2.isOnline() ? "" : " &3(Offline)"));
-				} else
-					throw new PlayerNotOnlineException(player1);
-			} else {
-				if (checkTeleportDisabled(player(), player1))
-					return;
-
-				player().teleportAsync(location1, TeleportCause.COMMAND);
-				send(PREFIX + "Poofing to &e" + Nickname.of(player1) + (player1.isOnline() ? "" : " &3(Offline)"));
-			}
-		} else {
+		if (arg1.matches(mapLinkRegex))
+			map();
+		else if (getAliasUsed().equalsIgnoreCase("tppos") || (isCoord(arg(1)) && isCoord(arg(2)) && isCoord(arg(3))))
+			coordinates();
+		else if (isOfflinePlayerArg(1))
+			players();
+		else {
 			send("&c/" + getAliasUsed() + " <player> [player]");
 			send("&c/" + getAliasUsed() + " <x> <y> <z> [yaw] [pitch] [world]");
 		}
+	}
+
+	private void map() {
+		String[] split = arg(1).split("#");
+		String[] coords = split[1].split(":");
+		AtomicReference<String> worldName = new AtomicReference<>(coords[0]);
+		BlueMapAPI.getInstance().flatMap(api -> api.getMap(worldName.get())).ifPresent(map -> worldName.set(map.getWorld().getSaveFolder().toFile().getName()));
+
+		World world = Bukkit.getWorld(worldName.get());
+		if (world == null)
+			error("World &e" + worldName + " &cnot found");
+
+		double x = Double.parseDouble(coords[1]);
+		double z = Double.parseDouble(coords[3]);
+		double terrainHeight = Double.parseDouble(coords[2]);
+		double aboveGround = Double.parseDouble(coords[4]);
+		double y = terrainHeight + aboveGround;
+		if (y > 275) y = 275;
+
+		Location location = new Location(world, x, y, z, 0, 90);
+		player().teleportAsync(location, TeleportCause.COMMAND);
+		return;
+	}
+
+	private void players() {
+		OfflinePlayer player1 = offlinePlayerArg(1);
+		Location location1 = Nerd.of(player1).getLocation();
+		if (isOfflinePlayerArg(2)) {
+			OfflinePlayer player2 = offlinePlayerArg(2);
+			if (player1.isOnline() && player1.getPlayer() != null) {
+				if (checkTeleportDisabled(player1.getPlayer(), player2))
+					return;
+
+				player1.getPlayer().teleportAsync(Nerd.of(player2).getLocation(), TeleportCause.COMMAND);
+				send(PREFIX + "Poofing to &e" + Nickname.of(player2) + (player2.isOnline() ? "" : " &3(Offline)"));
+			} else
+				throw new PlayerNotOnlineException(player1);
+		} else {
+			if (checkTeleportDisabled(player(), player1))
+				return;
+
+			player().teleportAsync(location1, TeleportCause.COMMAND);
+			send(PREFIX + "Poofing to &e" + Nickname.of(player1) + (player1.isOnline() ? "" : " &3(Offline)"));
+		}
+	}
+
+	private void coordinates() {
+		Location location = location();
+		Modify modifier = RelativeLocation.modify(location).x(arg(1)).y(arg(2)).z(arg(3));
+		if (arg(4) != null && arg(5) != null && isCoord(arg(4)) && isCoord(arg(5))) {
+			modifier.yaw(arg(4)).pitch(arg(5));
+			if (arg(6) != null) {
+				if (Bukkit.getWorld(arg(6)) == null)
+					error("World &e" + arg(6) + " &cnot found");
+				location.setWorld(Bukkit.getWorld(arg(6)));
+			}
+		} else if (!isNullOrEmpty(arg(4))) {
+			if (Bukkit.getWorld(arg(4)) == null)
+				error("World &e" + arg(4) + " &cnot found");
+			else
+				location.setWorld(Bukkit.getWorld(arg(4)));
+		}
+
+		player().teleportAsync(modifier.update(), TeleportCause.COMMAND);
 	}
 
 	private boolean checkTeleportDisabled(Player from, OfflinePlayer to) {
