5cc8960f307ceed4324a4df4e40ddbdef241e96c
Pugabyte <pugabyte@projecteden.gg>
2021-06-26 21:05:13 -0500

    Fix ProjectEdenGG/Issues#696

--- src/main/java/me/pugabyte/nexus/framework/commands/models/CustomCommand.java
+++ src/main/java/me/pugabyte/nexus/framework/commands/models/CustomCommand.java
@@ -708,8 +708,11 @@ public abstract class CustomCommand extends ICustomCommand {
 	@SneakyThrows
 	protected PlayerOwnedObject convertToPlayerOwnedObject(String value, Class<? extends PlayerOwnedObject> type) {
 		Class<? extends MongoService> service = (Class<? extends MongoService>) MongoService.ofObject(type);
-		if (service != null)
-			return service.newInstance().get(convertToOfflinePlayer(value));
+		if (service != null) {
+			final OfflinePlayer player = convertToOfflinePlayer(value);
+			if (player != null)
+				return service.newInstance().get(player);
+		}
 		return null;
 	}
 
