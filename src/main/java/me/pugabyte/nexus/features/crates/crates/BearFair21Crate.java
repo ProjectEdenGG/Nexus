d89acdbc46870972834bf3f8d0efeec80e998f43
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-06-27 21:14:50 -0500

    fix crate item announce

--- src/main/java/me/pugabyte/nexus/features/crates/crates/BearFair21Crate.java
+++ src/main/java/me/pugabyte/nexus/features/crates/crates/BearFair21Crate.java
@@ -1,9 +1,15 @@
 package me.pugabyte.nexus.features.crates.crates;
 
+import me.pugabyte.nexus.features.chat.Chat.Broadcast;
+import me.pugabyte.nexus.features.commands.MuteMenuCommand.MuteMenuProvider.MuteMenuItem;
 import me.pugabyte.nexus.features.crates.models.Crate;
+import me.pugabyte.nexus.features.crates.models.CrateLoot;
 import me.pugabyte.nexus.features.crates.models.CrateType;
+import me.pugabyte.nexus.features.crates.models.events.CrateSpawnItemEvent;
+import me.pugabyte.nexus.models.nickname.Nickname;
 import me.pugabyte.nexus.utils.ColorType;
 import org.bukkit.Color;
+import org.bukkit.event.EventHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,5 +32,14 @@ public class BearFair21Crate extends Crate {
 	@Override
 	public Color[] getBandColors() {
 		return new Color[]{ColorType.YELLOW.getBukkitColor(), ColorType.CYAN.getBukkitColor()};
+	}
+
+	@EventHandler
+	public void onItemSpawn(CrateSpawnItemEvent event) {
+		CrateLoot loot = event.getCrateLoot();
+		if (getCrateType().equals(event.getCrateType()) && loot.getItems().contains(CrateType.MYSTERY.getKey())) {
+			String message = "&e" + Nickname.of(event.getPlayer()) + " &3has received a &eMystery Crate Key &3from the &eBear Fair 21 Crate";
+			Broadcast.all().prefix("Crates").message(message).muteMenuItem(MuteMenuItem.CRATES).send();
+		}
 	}
 }
--- src/main/java/me/pugabyte/nexus/features/crates/crates/VoteCrate.java
+++ src/main/java/me/pugabyte/nexus/features/crates/crates/VoteCrate.java
@@ -39,7 +39,7 @@ public class VoteCrate extends Crate {
 	@EventHandler
 	public void onItemSpawn(CrateSpawnItemEvent event) {
 		CrateLoot loot = event.getCrateLoot();
-		if (loot.getItems().contains(CrateType.MYSTERY.getKey())) {
+		if (getCrateType().equals(event.getCrateType()) && loot.getItems().contains(CrateType.MYSTERY.getKey())) {
 			String message = "&e" + Nickname.of(event.getPlayer()) + " &3has received a &eMystery Crate Key &3from the &eVote Crate";
 			Broadcast.all().prefix("Crates").message(message).muteMenuItem(MuteMenuItem.CRATES).send();
 		}
