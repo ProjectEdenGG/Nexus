7b0efbb38e47424ce2482b160f568ede0fa37c04
Pugabyte <pugabyte@projecteden.gg>
2021-07-04 12:41:11 -0500

    Reorder slab logic

--- src/main/java/me/pugabyte/nexus/features/listeners/SlabBreak.java
+++ src/main/java/me/pugabyte/nexus/features/listeners/SlabBreak.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.type.Slab;
 import org.bukkit.block.data.type.Slab.Type;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -17,43 +18,50 @@ public class SlabBreak implements Listener {
 
 	@EventHandler
 	public void onSlabBreak(BlockBreakEvent event) {
-		if (event.isCancelled()) return;
-		if (!Rank.of(event.getPlayer()).isStaff()) return;
+		if (event.isCancelled())
+			return;
 
-		Material type = event.getBlock().getType();
-		Material handType = event.getPlayer().getInventory().getItemInMainHand().getType();
-		if (MaterialTag.SLABS.isTagged(type) && MaterialTag.SLABS.isTagged(handType)) {
-			Slab data = (Slab) event.getBlock().getBlockData();
-			Block target = event.getPlayer().getTargetBlockExact(10);
-			if (data.getType() != Type.DOUBLE || target == null)
-				return;
+		final Player player = event.getPlayer();
+		final Material type = event.getBlock().getType();
+		final Material handType = player.getInventory().getItemInMainHand().getType();
 
-			event.setCancelled(true);
+		if (!MaterialTag.SLABS.isTagged(type))
+			return;
+		if (!MaterialTag.SLABS.isTagged(handType))
+			return;
+		if (!Rank.of(player).isStaff())
+			return;
 
-			Vector direction = event.getPlayer().getLocation().getDirection();
-			Vector blockVector = null;
-			for (double d = 1; d < 16; d += .06) {
-				Vector multiplied = direction.clone().multiply(d).add(new Vector(0, event.getPlayer().getEyeHeight(), 0)).add(event.getPlayer().getLocation().toVector());
-				Block foundBlock = multiplied.toLocation(event.getPlayer().getWorld()).getBlock();
-				if (foundBlock.getType() == type && foundBlock.getLocation().equals(target.getLocation())) {
-					blockVector = multiplied;
-					break;
-				}
+		Slab data = (Slab) event.getBlock().getBlockData();
+		Block target = player.getTargetBlockExact(10);
+		if (data.getType() != Type.DOUBLE || target == null)
+			return;
+
+		event.setCancelled(true);
+
+		Vector direction = player.getLocation().getDirection();
+		Vector blockVector = null;
+		for (double d = 1; d < 16; d += .06) {
+			Vector multiplied = direction.clone().multiply(d).add(new Vector(0, player.getEyeHeight(), 0)).add(player.getLocation().toVector());
+			Block foundBlock = multiplied.toLocation(player.getWorld()).getBlock();
+			if (foundBlock.getType() == type && foundBlock.getLocation().equals(target.getLocation())) {
+				blockVector = multiplied;
+				break;
 			}
-
-			if (blockVector != null) {
-				double blockY = blockVector.getY() - ((int) blockVector.getY());
-				if (blockY > .5)
-					data.setType(Type.BOTTOM);
-				else
-					data.setType(Type.TOP);
-			} else
-				data.setType(Type.BOTTOM);
-
-			event.getBlock().setBlockData(data);
-			if (event.getPlayer().getGameMode() == GameMode.SURVIVAL)
-				event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType()));
 		}
+
+		if (blockVector != null) {
+			double blockY = blockVector.getY() - ((int) blockVector.getY());
+			if (blockY > .5)
+				data.setType(Type.BOTTOM);
+			else
+				data.setType(Type.TOP);
+		} else
+			data.setType(Type.BOTTOM);
+
+		event.getBlock().setBlockData(data);
+		if (player.getGameMode() == GameMode.SURVIVAL)
+			event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType()));
 	}
 
 }
