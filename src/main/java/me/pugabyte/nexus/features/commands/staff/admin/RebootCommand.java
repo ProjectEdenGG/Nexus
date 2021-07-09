a9eac8367a9713c6c6a6976284f07518e4b36caf
Pugabyte <pugabyte@projecteden.gg>
2021-07-09 15:12:11 -0500

    Add timestamp to reboot kick, show title on join

--- src/main/java/me/pugabyte/nexus/features/commands/staff/admin/RebootCommand.java
+++ src/main/java/me/pugabyte/nexus/features/commands/staff/admin/RebootCommand.java
@@ -3,6 +3,7 @@ package me.pugabyte.nexus.features.commands.staff.admin;
 import eden.utils.Env;
 import eden.utils.TimeUtils.Time;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import me.pugabyte.nexus.Nexus;
 import me.pugabyte.nexus.features.NexusCommand.ReloadCondition;
@@ -14,16 +15,22 @@ import me.pugabyte.nexus.framework.commands.models.annotations.Permission;
 import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
 import me.pugabyte.nexus.utils.PlayerUtils;
 import me.pugabyte.nexus.utils.Tasks;
+import me.pugabyte.nexus.utils.TimeUtils;
 import me.pugabyte.nexus.utils.TitleBuilder;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 import static me.pugabyte.nexus.utils.StringUtils.colorize;
 
+@NoArgsConstructor
 @Permission("group.seniorstaff")
-public class RebootCommand extends CustomCommand {
+public class RebootCommand extends CustomCommand implements Listener {
 	@Getter
 	private static boolean queued;
 	private static boolean rebooting;
@@ -59,7 +66,7 @@ public class RebootCommand extends CustomCommand {
 			rebooting = false;
 			conditions.forEach(ReloadCondition::run);
 			for (Player player : PlayerUtils.getOnlinePlayers())
-				player.kickPlayer(colorize("&6&lRebooting server!\n&eCome back in about 60 seconds"));
+				player.kickPlayer(colorize("&6&lRebooting server!\n&eCome back in about 60 seconds\n&7" + TimeUtils.shortDateTimeFormat(LocalDateTime.now()) + " EST"));
 			BashCommand.execute("mark2 send -n " + (Nexus.getEnv() == Env.PROD ? "smp" : "test") + " ~restart");
 		});
 	}
@@ -72,6 +79,15 @@ public class RebootCommand extends CustomCommand {
 				Nexus.log("Reboot failed: " + ex.getMessage());
 			}
 		});
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		if (!rebooting)
+			return;
+
+		new TitleBuilder().players(event.getPlayer()).title("&cRebooting server").subtitle("&cCome back in ~60 seconds").send();
+		Koda.dm(event.getPlayer(), "Rebooting server, come back in 60 seconds");
 	}
 
 }
