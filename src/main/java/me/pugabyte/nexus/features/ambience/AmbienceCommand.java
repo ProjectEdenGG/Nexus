ec383bf5945b985d0019617b8d77c205a5b1e556
Pugabyte <pugabyte@projecteden.gg>
2021-07-11 23:45:08 -0500

    Add Ambience

---
+++ src/main/java/me/pugabyte/nexus/features/ambience/AmbienceCommand.java
@@ -0,0 +1,142 @@
+package me.pugabyte.nexus.features.ambience;
+
+import eden.utils.TimeUtils.Time;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import me.pugabyte.nexus.Nexus;
+import me.pugabyte.nexus.framework.commands.models.CustomCommand;
+import me.pugabyte.nexus.framework.commands.models.annotations.Arg;
+import me.pugabyte.nexus.framework.commands.models.annotations.Path;
+import me.pugabyte.nexus.framework.commands.models.events.CommandEvent;
+import me.pugabyte.nexus.models.ambience.AmbienceConfig;
+import me.pugabyte.nexus.models.ambience.AmbienceConfig.Ambience;
+import me.pugabyte.nexus.models.ambience.AmbienceConfig.Ambience.AmbienceType;
+import me.pugabyte.nexus.models.ambience.AmbienceConfigService;
+import me.pugabyte.nexus.utils.JsonBuilder;
+import me.pugabyte.nexus.utils.PlayerUtils;
+import me.pugabyte.nexus.utils.StringUtils;
+import me.pugabyte.nexus.utils.Tasks;
+import org.bukkit.Location;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import static me.pugabyte.nexus.utils.ItemUtils.isNullOrAir;
+
+@NoArgsConstructor
+public class AmbienceCommand extends CustomCommand implements Listener {
+	final AmbienceConfigService service = new AmbienceConfigService();
+	final AmbienceConfig config = service.get0();
+
+	public AmbienceCommand(@NonNull CommandEvent event) {
+		super(event);
+	}
+
+	@Path("types [page]")
+	void types(@Arg("1") int page) {
+		final List<AmbienceType> types = List.of(AmbienceType.values());
+		if (types.isEmpty())
+			error("No ambience types defined");
+
+		send(PREFIX + "Ambience types:");
+
+		BiFunction<AmbienceType, String, JsonBuilder> formatter = (type, index) ->
+			json(" &3" + index + " &e" + camelCase(type) + " &7- " + config.get(type).size())
+				.command("/ambience list " + type.name().toLowerCase())
+				.hover("&eClick to view");
+
+		paginate(types, formatter, "/ambience types", page);
+	}
+
+	@Path("list <type> [page]")
+	void list(AmbienceType type, @Arg("1") int page) {
+		final List<Ambience> ambiences = config.get(type);
+		if (ambiences.isEmpty())
+			error("No " + camelCase(type) + " ambience found");
+
+		send(PREFIX + camelCase(type) + " ambience:");
+		final BiFunction<Ambience, String, JsonBuilder> formatter = (ambience, index) ->
+			json(" &3" + index + " &e" + StringUtils.getLocationString(ambience.getLocation()))
+				.command(StringUtils.getTeleportCommand(ambience.getLocation()))
+				.hover("&eClick to teleport");
+
+		paginate(ambiences, formatter, "/ambience list " + type.name().toLowerCase(), page);
+	}
+
+	static {
+		final AmbienceConfigService service = new AmbienceConfigService();
+		final AmbienceConfig config = service.get0();
+
+		Tasks.repeat(Time.MINUTE, Time.MINUTE, () -> {
+			for (AmbienceConfig.Ambience ambience : new ArrayList<>(config.getAmbiences())) {
+				if (!ambience.getLocation().isChunkLoaded())
+					continue;
+
+				if (!ambience.validate())
+					continue;
+
+				ambience.play();
+			}
+		});
+	}
+
+	@EventHandler
+	public void onRemoveAmbience(EntityDamageByEntityEvent event) {
+		if (!(event.getDamager() instanceof Player player))
+			return;
+
+		if (!(event.getEntity() instanceof ItemFrame itemFrame))
+			return;
+
+		final AmbienceConfigService service = new AmbienceConfigService();
+		final AmbienceConfig config = service.get0();
+
+		final AmbienceConfig.Ambience ambience = config.get(itemFrame.getLocation());
+		if (ambience == null)
+			return;
+
+		config.delete(ambience);
+		service.save(config);
+		if (Nexus.isDebug())
+			PlayerUtils.send(player, PREFIX + "Removed " + camelCase(ambience.getType()));
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onAddAmbience(PlayerInteractEntityEvent event) {
+		final Player player = event.getPlayer();
+
+		if (!(event.getRightClicked() instanceof ItemFrame itemFrame))
+			return;
+
+		final Location location = itemFrame.getLocation();
+		final ItemStack item = player.getInventory().getItem(event.getHand());
+
+		if (isNullOrAir(item))
+			return;
+
+		final AmbienceConfigService service = new AmbienceConfigService();
+		final AmbienceConfig config = service.get0();
+
+		for (AmbienceType ambienceType : AmbienceType.values()) {
+			if (!ambienceType.equals(item))
+				continue;
+
+			config.add(new AmbienceConfig.Ambience(location.toBlockLocation(), ambienceType));
+			service.save(config);
+			if (Nexus.isDebug())
+				PlayerUtils.send(player, PREFIX + "Added " + camelCase(ambienceType));
+			break;
+		}
+	}
+
+
+}
---
+++ src/main/java/me/pugabyte/nexus/models/ambience/AmbienceConfig.java
@@ -0,0 +1,169 @@
+package me.pugabyte.nexus.models.ambience;
+
+import dev.morphia.annotations.Converters;
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import dev.morphia.annotations.PostLoad;
+import eden.mongodb.serializers.UUIDConverter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import me.pugabyte.nexus.Nexus;
+import me.pugabyte.nexus.framework.persistence.serializer.mongodb.LocationConverter;
+import me.pugabyte.nexus.models.PlayerOwnedObject;
+import me.pugabyte.nexus.models.ambience.AmbienceConfig.Ambience.AmbienceType;
+import me.pugabyte.nexus.utils.ItemBuilder;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static eden.utils.StringUtils.camelCase;
+import static me.pugabyte.nexus.utils.ItemUtils.isNullOrAir;
+import static me.pugabyte.nexus.utils.StringUtils.getShortLocationString;
+
+@Data
+@Builder
+@Entity("ambience_config")
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Converters({UUIDConverter.class, LocationConverter.class})
+public class AmbienceConfig implements PlayerOwnedObject {
+	@Id
+	@NonNull
+	private UUID uuid;
+
+	private List<Ambience> ambiences = new ArrayList<>();
+	private transient Map<Location, Ambience> ambienceMap = new HashMap<>();
+
+	@PostLoad
+	void postLoad() {
+		for (Ambience ambience : ambiences)
+			ambienceMap.put(ambience.getLocation(), ambience);
+	}
+
+	public List<Ambience> get(AmbienceType type) {
+		return ambiences.stream().filter(ambience -> ambience.getType() == type).toList();
+	}
+
+	public Ambience get(Location location) {
+		return ambienceMap.get(location.toBlockLocation());
+	}
+
+	public void add(Ambience ambience) {
+		this.ambiences.add(ambience);
+		ambienceMap.put(ambience.getLocation(), ambience);
+	}
+
+	public boolean delete(Location location) {
+		Ambience ambience = get(location);
+		if (ambience == null)
+			return false;
+
+		return delete(ambience);
+	}
+
+	public boolean delete(Ambience ambience) {
+		if (!this.ambiences.contains(ambience))
+			return false;
+
+		this.ambiences.remove(ambience);
+		ambienceMap.remove(ambience.getLocation());
+		return true;
+	}
+
+	@Data
+	@AllArgsConstructor
+	public static class Ambience {
+		private Location location;
+		private AmbienceType type;
+
+		public boolean validate() {
+			if (!type.getType().validate(this)) {
+				Nexus.warn("[Ambience] " + camelCase(type) + " at " + getShortLocationString(location) + " invalid, removing");
+				config().delete(this);
+				return false;
+			}
+
+			return true;
+		}
+
+		static AmbienceConfig config() {
+			return new AmbienceConfigService().get0();
+		}
+
+		public void play() {
+			type.play(location);
+		}
+
+		@Getter
+		@AllArgsConstructor
+		public enum AmbienceType {
+			METAL_WINDCHIMES(AmbienceLocationType.ITEM_FRAME, Material.AMETHYST_SHARD, 1) {
+				@Override
+				void play(Location location) {
+					// TODO
+				}
+			},
+			;
+
+			private final AmbienceLocationType type;
+			private final Material material;
+			private final int customModelData;
+
+			abstract void play(Location location);
+
+			public boolean equals(ItemStack itemStack) {
+				if (itemStack.getType() != material)
+					return false;
+				if (new ItemBuilder(itemStack).customModelData() != customModelData)
+					return false;
+
+				return true;
+			}
+
+			private enum AmbienceLocationType {
+				ITEM_FRAME {
+					public boolean validate(Ambience ambience) {
+						final Location location = ambience.getLocation();
+						for (ItemFrame itemFrame : location.getNearbyEntitiesByType(ItemFrame.class, 1, 1, 1)) {
+							if (!itemFrame.getLocation().toBlockLocation().equals(location.toBlockLocation()))
+								continue;
+
+							if (isNullOrAir(itemFrame.getItem()))
+								continue;
+
+							if (!ambience.getType().equals(itemFrame.getItem()))
+								continue;
+
+							return true;
+						}
+
+						return false;
+					}
+				},
+				BLOCK {
+					public boolean validate(Ambience ambience) {
+						return ambience.getLocation().getBlock().getType() == ambience.getType().getMaterial();
+					}
+				},
+				;
+
+				abstract public boolean validate(Ambience ambience);
+			}
+		}
+
+	}
+
+}
