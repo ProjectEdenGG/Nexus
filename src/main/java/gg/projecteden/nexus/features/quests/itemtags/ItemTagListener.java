69a58c2b926a1b4c0c712075e519f90cb7bdebc2
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-07-24 19:24:15 -0500

    make ItemTags survival only, and release it

--- src/main/java/gg/projecteden/nexus/features/quests/itemtags/ItemTagListener.java
+++ src/main/java/gg/projecteden/nexus/features/quests/itemtags/ItemTagListener.java
@@ -3,17 +3,25 @@ package gg.projecteden.nexus.features.quests.itemtags;
 import com.destroystokyo.paper.event.inventory.PrepareResultEvent;
 import com.gmail.nossr50.events.skills.repair.McMMOPlayerRepairCheckEvent;
 import gg.projecteden.nexus.Nexus;
-import gg.projecteden.nexus.utils.PlayerUtils.Dev;
+import gg.projecteden.nexus.utils.WorldGroup;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Hanging;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.hanging.HangingBreakEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
 
 import static gg.projecteden.nexus.features.quests.itemtags.ItemTagsUtils.updateItem;
 import static gg.projecteden.nexus.utils.ItemUtils.isNullOrAir;
@@ -24,14 +32,13 @@ public class ItemTagListener implements Listener {
 		Nexus.registerListener(this);
 	}
 
-	public static boolean isNotTesting(Player player) {
-		return !Dev.WAKKA.is(player);
-	}
-
 	@EventHandler
 	public void onEnchantItem(EnchantItemEvent event) {
-		if (!(event.getView().getPlayer() instanceof Player player)) return;
-		if (isNotTesting(player)) return;
+		if (!(event.getView().getPlayer() instanceof Player player))
+			return;
+
+		if (WorldGroup.of(player) != WorldGroup.SURVIVAL)
+			return;
 
 		ItemStack result = event.getItem();
 		if (isNullOrAir(result)) return;
@@ -43,8 +50,11 @@ public class ItemTagListener implements Listener {
 
 	@EventHandler
 	public void onCraftItem(PrepareItemCraftEvent event) {
-		if (!(event.getView().getPlayer() instanceof Player player)) return;
-		if (isNotTesting(player)) return;
+		if (!(event.getView().getPlayer() instanceof Player player))
+			return;
+
+		if (WorldGroup.of(player) != WorldGroup.SURVIVAL)
+			return;
 
 		ItemStack result = event.getInventory().getResult();
 		if (isNullOrAir(result)) return;
@@ -57,9 +67,12 @@ public class ItemTagListener implements Listener {
 
 	@EventHandler
 	public void onItemDamage(PlayerItemDamageEvent event) {
-		if (isNotTesting(event.getPlayer())) return;
 		ItemStack result = event.getItem();
-		if (isNullOrAir(result)) return;
+		if (isNullOrAir(result))
+			return;
+
+		if (WorldGroup.of(event.getPlayer()) != WorldGroup.SURVIVAL)
+			return;
 
 		ItemStack updated = updateItem(result);
 
@@ -69,8 +82,11 @@ public class ItemTagListener implements Listener {
 	// Includes Anvil, Grindstone, and Smithing Table
 	@EventHandler
 	public void onPrepareItem(PrepareResultEvent event) {
-		if (!(event.getView().getPlayer() instanceof Player player)) return;
-		if (isNotTesting(player)) return;
+		if (!(event.getView().getPlayer() instanceof Player player))
+			return;
+
+		if (WorldGroup.of(player) != WorldGroup.SURVIVAL)
+			return;
 
 		ItemStack result = event.getResult();
 		if (isNullOrAir(result)) return;
@@ -80,68 +96,64 @@ public class ItemTagListener implements Listener {
 		event.setResult(updated);
 	}
 
-	// TODO ItemTags: Uncomment
-//	@EventHandler
-//	public void onBreakItemFrame(EntityDamageByEntityEvent event){
-//
-//		if(!event.getEntityType().equals(EntityType.ITEM_FRAME))
-//			return;
-//
-//		ItemFrame itemFrame = (ItemFrame) event.getEntity();
-//		ItemStack item = itemFrame.getItem();
-//		if(isNullOrAir(item)) return;
-//
-//		ItemStack updated = updateItem(item);
-//
-//		itemFrame.setItem(updated);
-//	}
+	@EventHandler
+	public void onBreakItemFrame(EntityDamageByEntityEvent event) {
+		if (!event.getEntityType().equals(EntityType.ITEM_FRAME))
+			return;
 
-	// TODO ItemTags: Uncomment
-//	@EventHandler
-//	public void onItemFrameBreak(HangingBreakEvent event){
-//		if (!(event.getEntity().getType().equals(EntityType.ITEM_FRAME)))
-//			return;
-//
-//		if (event.isCancelled())
-//			return;
-//
-//		ItemFrame itemFrame = (ItemFrame) event.getEntity();
-//		ItemStack item = itemFrame.getItem();
-//		if (isNullOrAir(item)) return;
-//
-//		ItemStack updated = updateItem(item);
-//
-//		itemFrame.setItem(updated);
-//	}
+		if (WorldGroup.of(event.getEntity()) != WorldGroup.SURVIVAL)
+			return;
 
-	// TODO ItemTags: Uncomment
-//	@EventHandler
-//	public void onEntityDeath(EntityDeathEvent event) {
-//		int ndx = 0;
-//		for (ItemStack drop : new ArrayList<>(event.getDrops())) {
-//			ItemStack updated = updateItem(drop);
-//			event.getDrops().set(ndx, updated);
-//			++ndx;
-//		}
-//	}
+		ItemFrame itemFrame = (ItemFrame) event.getEntity();
+		ItemStack item = itemFrame.getItem();
+		if (isNullOrAir(item)) return;
 
-	// TODO: uncomment if mythic mobs is added
-//	@EventHandler
-//	public void onMythicMMobEntityDeath(MythicMobDeathEvent event) {
-//		int ndx = 0;
-//		for (ItemStack drop : new ArrayList<>(event.getDrops())) {
-//			ItemStack updated = updateItem(drop);
-//			event.getDrops().set(ndx, updated);
-//			++ndx;
-//		}
-//	}
+		ItemStack updated = updateItem(item);
+
+		itemFrame.setItem(updated);
+	}
+
+	@EventHandler
+	public void onItemFrameBreak(HangingBreakEvent event) {
+		Hanging entity = event.getEntity();
+		if (!(entity.getType().equals(EntityType.ITEM_FRAME)))
+			return;
+
+		if (WorldGroup.of(entity) != WorldGroup.SURVIVAL)
+			return;
+
+		if (event.isCancelled())
+			return;
+
+		ItemFrame itemFrame = (ItemFrame) entity;
+		ItemStack item = itemFrame.getItem();
+		if (isNullOrAir(item)) return;
+
+		ItemStack updated = updateItem(item);
+
+		itemFrame.setItem(updated);
+	}
+
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent event) {
+		if (WorldGroup.of(event.getEntity()) != WorldGroup.SURVIVAL)
+			return;
+
+		int ndx = 0;
+		for (ItemStack drop : new ArrayList<>(event.getDrops())) {
+			ItemStack updated = updateItem(drop);
+			event.getDrops().set(ndx, updated);
+			++ndx;
+		}
+	}
 
 	@EventHandler
 	public void onFishingLoot(PlayerFishEvent event) {
 		if (!event.getState().equals(PlayerFishEvent.State.CAUGHT_FISH))
 			return;
 
-		if (isNotTesting(event.getPlayer())) return;
+		if (WorldGroup.of(event.getPlayer()) != WorldGroup.SURVIVAL)
+			return;
 
 		Entity caught = event.getCaught();
 		if (!(caught instanceof Item item)) return;
@@ -152,11 +164,24 @@ public class ItemTagListener implements Listener {
 
 	@EventHandler
 	public void onMcMMORepair(McMMOPlayerRepairCheckEvent event) {
-		if (isNotTesting(event.getPlayer())) return;
+		if (WorldGroup.of(event.getPlayer()) != WorldGroup.SURVIVAL)
+			return;
 
 		ItemStack repaired = event.getRepairedObject().clone();
 		updateItem(repaired);
 		event.getRepairedObject().setItemMeta(repaired.getItemMeta());
 	}
+
+	// TODO: uncomment if mythic mobs is added
+//	@EventHandler
+//	public void onMythicMMobEntityDeath(MythicMobDeathEvent event) {
+//		if(WorldGroup.of(event.getEntity()) != WorldGroup.SURVIVAL) return;
+//		int ndx = 0;
+//		for (ItemStack drop : new ArrayList<>(event.getDrops())) {
+//			ItemStack updated = updateItem(drop);
+//			event.getDrops().set(ndx, updated);
+//			++ndx;
+//		}
+//	}
 
 }
