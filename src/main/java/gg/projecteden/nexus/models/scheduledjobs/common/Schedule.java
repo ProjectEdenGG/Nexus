9e107b800615219d3c93d5c0e254a4796cac1bcb
Pugabyte <pugabyte@projecteden.gg>
2021-07-27 22:16:57 -0500

    Add crontab guru link

--- src/main/java/gg/projecteden/nexus/models/scheduledjobs/common/Schedule.java
+++ src/main/java/gg/projecteden/nexus/models/scheduledjobs/common/Schedule.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * https://crontab.guru/
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Schedule {
