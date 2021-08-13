7ada6e8e0f89dfb7880e645dcf5ad0cd0cfa6ad3
lexikiq <noellekiq@gmail.com>
2021-08-12 20:19:20 -0400

    Fix item description
    removes hardcoded text

--- src/main/java/gg/projecteden/nexus/features/mcmmo/menus/McMMOResetProvider.java
+++ src/main/java/gg/projecteden/nexus/features/mcmmo/menus/McMMOResetProvider.java
@@ -211,7 +211,7 @@ public class McMMOResetProvider extends MenuUtils implements InventoryProvider {
 					.name("&e" + StringUtils.camelCase(skill.name()))
 					.lore("&3Level: &e" + mcmmoPlayer.getSkillLevel(PrimarySkillType.valueOf(skill.name())) +
 							"|| ||&3&lReward:" +
-							"||&f" + DEPOSIT_PRETTY + " (x2.5 for level 200)" +
+							"||&f" + DEPOSIT_PRETTY + " (x" + MAX_DEPOSIT_MULTIPLIER + " for level " + TIER_TWO + ")" +
 							"||&f" + skill.getRewardDescription() +
 							"|| ||&3Number of Prestieges: &e" + mcMMOPrestige.getPrestige(skill.name()))
 					.build();
