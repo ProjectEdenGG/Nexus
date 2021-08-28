14e0dbad65acc4cf565952fcb99eb020b21d2556
Blast-Mc <cjblanton2@gmail.com>
2021-08-28 17:42:01 -0400

    add message for rare items in boss crate, remove coming soon line

--- src/main/java/gg/projecteden/nexus/features/crates/crates/BossCrate.java
+++ src/main/java/gg/projecteden/nexus/features/crates/crates/BossCrate.java
@@ -1,8 +1,14 @@
 package gg.projecteden.nexus.features.crates.crates;
 
+import gg.projecteden.nexus.features.chat.Chat;
+import gg.projecteden.nexus.features.commands.MuteMenuCommand;
 import gg.projecteden.nexus.features.crates.models.Crate;
+import gg.projecteden.nexus.features.crates.models.CrateLoot;
 import gg.projecteden.nexus.features.crates.models.CrateType;
+import gg.projecteden.nexus.features.crates.models.events.CrateSpawnItemEvent;
 import gg.projecteden.nexus.models.nerd.Rank;
+import gg.projecteden.nexus.models.nickname.Nickname;
+import org.bukkit.event.EventHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +26,15 @@ public class BossCrate extends Crate {
 			add("&f&l--=[+]=--");
 			add("&f&k[+] " + Rank.OWNER.getChatColor() + "&lBoss Crate &f&k[+]");
 			add("&f&l--=[+]=--");
-			add("&cComing Soon...");
 		}};
+	}
+
+	@EventHandler
+	public void onItemSpawn(CrateSpawnItemEvent event) {
+		CrateLoot loot = event.getCrateLoot();
+		if (getCrateType().equals(event.getCrateType()) && loot.getWeight() <= 2) {
+			String message = String.format("&e%s &3has received a %s in the Boss Crate", Nickname.of(event.getPlayer()), loot.getTitle());
+			Chat.Broadcast.all().prefix("Crates").message(message).muteMenuItem(MuteMenuCommand.MuteMenuProvider.MuteMenuItem.CRATES).send();
+		}
 	}
 }
