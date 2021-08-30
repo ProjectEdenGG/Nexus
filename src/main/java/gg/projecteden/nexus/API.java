28c79fd8f602181c373b90b0cc6c03c368fab46a
GriffinCodes <pugabyte@projecteden.gg>
2021-08-30 15:12:54 -0500

    Automatically load all type converters

--- src/main/java/gg/projecteden/nexus/API.java
+++ src/main/java/gg/projecteden/nexus/API.java
@@ -4,27 +4,17 @@ import com.google.gson.GsonBuilder;
 import dev.morphia.converters.TypeConverter;
 import gg.projecteden.EdenAPI;
 import gg.projecteden.mongodb.DatabaseConfig;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.ChannelConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.ChatColorConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.ColorConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.CostumeConverter;
 import gg.projecteden.nexus.framework.persistence.serializer.mongodb.ItemStackConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.JobConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.LocationConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.MobHeadConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.PrivateChannelConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.PublicChannelConverter;
-import gg.projecteden.nexus.framework.persistence.serializer.mongodb.QuestConverter;
 import gg.projecteden.nexus.utils.SerializationUtils.JSON.LocalDateGsonSerializer;
 import gg.projecteden.nexus.utils.SerializationUtils.JSON.LocalDateTimeGsonSerializer;
 import gg.projecteden.nexus.utils.SerializationUtils.JSON.LocationGsonSerializer;
 import gg.projecteden.utils.Env;
 import org.bukkit.Location;
+import org.reflections.Reflections;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.List;
 
 public class API extends EdenAPI {
 
@@ -53,19 +43,7 @@ public class API extends EdenAPI {
 
 	@Override
 	public Collection<? extends Class<? extends TypeConverter>> getMongoConverters() {
-		return List.of(
-				ChannelConverter.class,
-				ChatColorConverter.class,
-				ColorConverter.class,
-				CostumeConverter.class,
-				ItemStackConverter.class,
-				JobConverter.class,
-				LocationConverter.class,
-				MobHeadConverter.class,
-				QuestConverter.class,
-				PrivateChannelConverter.class,
-				PublicChannelConverter.class
-		);
+		return new Reflections(ItemStackConverter.class.getPackage().getName()).getSubTypesOf(TypeConverter.class);
 	}
 
 	@Override
