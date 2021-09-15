b7e8963617497d13d9125383f9f52e9196b5f4ef
GriffinCodes <pugabyte@projecteden.gg>
2021-09-15 13:29:09 -0500

    Refactor train

--- src/main/java/gg/projecteden/nexus/features/events/y2021/pugmas21/Pugmas21Command.java
+++ src/main/java/gg/projecteden/nexus/features/events/y2021/pugmas21/Pugmas21Command.java
@@ -6,22 +6,7 @@ import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.annotations.Switch;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
-import gg.projecteden.nexus.utils.ItemBuilder;
-import gg.projecteden.nexus.utils.Tasks;
-import gg.projecteden.utils.TimeUtils.TickTime;
 import lombok.NonNull;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Particle;
-import org.bukkit.World;
-import org.bukkit.block.BlockFace;
-import org.bukkit.craftbukkit.v1_17_R1.entity.CraftArmorStand;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.util.EulerAngle;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Pugmas21Command extends CustomCommand {
 
@@ -29,86 +14,29 @@ public class Pugmas21Command extends CustomCommand {
 		super(event);
 	}
 
-	private static final int MODELS_COMPLETED = 7;
-
-	private ArmorStand trainArmorStand(int model, Location location, boolean correctModel) {
-		final ItemBuilder item = new ItemBuilder(Material.MINECART).customModelData(!correctModel && model > MODELS_COMPLETED ? 2 : model);
-		final ArmorStand armorStand = location().getWorld().spawn(location, ArmorStand.class);
-		armorStand.setRightArmPose(EulerAngle.ZERO);
-		armorStand.setLeftArmPose(EulerAngle.ZERO);
-		armorStand.setHeadPose(EulerAngle.ZERO);
-		armorStand.setInvulnerable(true);
-		armorStand.setGravity(false);
-		armorStand.setBasePlate(false);
-		armorStand.setArms(true);
-		armorStand.setDisabledSlots(EquipmentSlot.values());
-		armorStand.setItem(EquipmentSlot.HEAD, item.build());
-		return armorStand;
-	}
-
-	@Path("armorstand <model>")
+	@Path("train armorStand <model>")
 	@Permission("group.admin")
 	void train(int model) {
-		trainArmorStand(model, location(), true);
+		Train.armorStand(model, location());
 	}
 
-	private static final double SEPARATOR = 7.5;
-
-	@Path("train [--speed] [--seconds] [--respawn] [--ticks] [--hasImpulse] [--keep]")
+	@Path("train start")
 	void train(
-		@Arg(".25") @Switch double speed,
+		@Arg(".3") @Switch double speed,
 		@Arg("60") @Switch int seconds,
-		@Arg("true") @Switch boolean respawn,
-		@Arg("true") @Switch boolean hasImpulse,
 		@Arg("false") @Switch boolean keep,
-		@Arg("4") @Switch double smoke_back,
-		@Arg("5.3") @Switch double smoke_up
+		@Arg("4") @Switch double smokeBack,
+		@Arg("5.3") @Switch double smokeUp
 	) {
-		player().teleport(location().toCenterLocation());
-		final Location location = location();
-		final BlockFace forwards = player().getFacing();
-		final BlockFace backwards = forwards.getOppositeFace();
-
-		final List<ArmorStand> armorStands = new ArrayList<>();
-
-		for (int i = 1; i <= 18; i++) {
-			armorStands.add(trainArmorStand(i, location, keep));
-			location.add(backwards.getDirection().multiply(SEPARATOR));
-		}
-
-		if (keep)
-			return;
-
-		final int repeat = Tasks.repeat(1, 1, () -> {
-			smoke(armorStands.iterator().next().getLocation()
-				.add(backwards.getDirection().multiply(smoke_back))
-				.add(BlockFace.UP.getDirection().multiply(smoke_up)));
-
-			for (ArmorStand armorStand : armorStands) {
-				((CraftArmorStand) armorStand).getHandle().af = hasImpulse; // force packet
-				armorStand.teleport(armorStand.getLocation().add(forwards.getDirection().multiply(speed)));
-			}
-		});
-
-		Tasks.wait(TickTime.SECOND.x(seconds), () -> {
-			Tasks.cancel(repeat);
-			for (ArmorStand armorStand : armorStands)
-				armorStand.remove();
-		});
-	}
-
-	private void smoke(Location smoke) {
-		final World world = smoke.getWorld();
-		Particle particle = Particle.CAMPFIRE_COSY_SMOKE;
-
-		double x = 0;
-		double y = 3;
-		double z = 0;
-		double speed = 0.01;
-		int count = 0;
-
-		world.spawnParticle(particle, smoke, count, x, y, z, speed);
-		world.spawnParticle(particle, smoke, count, x, y, z, speed);
+		Train.builder()
+			.location(location())
+			.direction(player().getFacing())
+			.speed(speed)
+			.seconds(seconds)
+			.smokeBack(smokeBack)
+			.smokeUp(smokeUp)
+			.build()
+			.start();
 	}
 
 	@Path("npcs interact <npc>")
---
+++ src/main/java/gg/projecteden/nexus/features/events/y2021/pugmas21/Train.java
@@ -0,0 +1,119 @@
+package gg.projecteden.nexus.features.events.y2021.pugmas21;
+
+import gg.projecteden.nexus.utils.ItemBuilder;
+import gg.projecteden.nexus.utils.LocationUtils;
+import gg.projecteden.nexus.utils.Tasks;
+import gg.projecteden.utils.TimeUtils.TickTime;
+import lombok.Builder;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftArmorStand;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.util.EulerAngle;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Train {
+	private final Location location;
+	private final BlockFace forwards;
+	private final BlockFace backwards;
+	private final double speed;
+	private final int seconds;
+	private final Vector smokeBack;
+	private final Vector smokeUp;
+
+	private final List<ArmorStand> armorStands = new ArrayList<>();
+	private int taskId;
+
+	private static final int TOTAL_MODELS = 18;
+	private static final double SEPARATOR = 7.5;
+
+	@Builder
+	public Train(Location location, BlockFace direction, double speed, int seconds, double smokeBack, double smokeUp) {
+		this.location = LocationUtils.getCenteredLocation(location);
+		this.forwards = direction;
+		this.backwards = direction.getOppositeFace();
+		this.speed = speed;
+		this.seconds = seconds;
+		this.smokeBack = backwards.getDirection().multiply(smokeBack);
+		this.smokeUp = BlockFace.UP.getDirection().multiply(smokeUp);
+	}
+
+	public void start() {
+		spawnArmorStands();
+
+		taskId = Tasks.repeat(1, 1, this::move);
+
+		Tasks.wait(TickTime.SECOND.x(seconds), this::stop);
+	}
+
+	public void stop() {
+		Tasks.cancel(taskId);
+		armorStands.forEach(Entity::remove);
+	}
+
+	private void move() {
+		for (ArmorStand armorStand : armorStands)
+			move(armorStand);
+
+		new Smoke(getSmokeLocation());
+	}
+
+	private void move(ArmorStand armorStand) {
+		forcePacket(armorStand);
+		armorStand.teleport(armorStand.getLocation().add(forwards.getDirection().multiply(speed)));
+	}
+
+	private void forcePacket(ArmorStand armorStand) {
+		((CraftArmorStand) armorStand).getHandle().af = true; // hasImpulse
+	}
+
+	private Location getSmokeLocation() {
+		return armorStands.iterator().next().getLocation().add(smokeBack).add(smokeUp);
+	}
+
+	private void spawnArmorStands() {
+		for (int i = 1; i <= TOTAL_MODELS; i++) {
+			armorStands.add(armorStand(i, location));
+			location.add(backwards.getDirection().multiply(SEPARATOR));
+		}
+	}
+
+	public static ArmorStand armorStand(int model, Location location) {
+		final ItemBuilder item = new ItemBuilder(Material.MINECART).customModelData(model);
+		final ArmorStand armorStand = location.getWorld().spawn(location, ArmorStand.class);
+		armorStand.setRightArmPose(EulerAngle.ZERO);
+		armorStand.setLeftArmPose(EulerAngle.ZERO);
+		armorStand.setHeadPose(EulerAngle.ZERO);
+		armorStand.setInvulnerable(true);
+		armorStand.setGravity(false);
+		armorStand.setBasePlate(false);
+		armorStand.setArms(true);
+		armorStand.setDisabledSlots(EquipmentSlot.values());
+		armorStand.setItem(EquipmentSlot.HEAD, item.build());
+		return armorStand;
+	}
+
+	public static class Smoke {
+		private static final Particle particle = Particle.CAMPFIRE_COSY_SMOKE;
+		private static final double x = 0;
+		private static final double y = 3;
+		private static final double z = 0;
+		private static final double speed = 0.01;
+		private static final int count = 0;
+
+		public Smoke(Location location) {
+			final World world = location.getWorld();
+			world.spawnParticle(particle, location, count, x, y, z, speed);
+			world.spawnParticle(particle, location, count, x, y, z, speed);
+		}
+	}
+
+}
