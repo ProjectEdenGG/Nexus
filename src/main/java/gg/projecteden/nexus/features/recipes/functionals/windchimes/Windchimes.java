5f7a5d030457032e69756344ce209d5241a8b7ff
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-09-19 02:25:24 -0500

    add todo for playing sounds when interacting with windchimes

--- src/main/java/gg/projecteden/nexus/features/recipes/functionals/windchimes/Windchimes.java
+++ src/main/java/gg/projecteden/nexus/features/recipes/functionals/windchimes/Windchimes.java
@@ -2,12 +2,24 @@ package gg.projecteden.nexus.features.recipes.functionals.windchimes;
 
 import gg.projecteden.nexus.Nexus;
 import gg.projecteden.nexus.features.recipes.models.FunctionalRecipe;
+import gg.projecteden.nexus.models.nerd.Nerd;
 import gg.projecteden.nexus.utils.ItemBuilder;
+import gg.projecteden.nexus.utils.ItemUtils;
 import gg.projecteden.nexus.utils.MaterialTag;
+import gg.projecteden.nexus.utils.PlayerUtils;
+import gg.projecteden.nexus.utils.Utils.ActionGroup;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.RecipeChoice;
@@ -16,6 +28,7 @@ import org.bukkit.inventory.ShapedRecipe;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static gg.projecteden.utils.StringUtils.camelCase;
 
@@ -81,6 +94,52 @@ public abstract class Windchimes extends FunctionalRecipe {
 	@Override
 	public MaterialChoice getMaterialChoice() {
 		return new RecipeChoice.MaterialChoice(MaterialTag.WOOD_BUTTONS);
+	}
+
+	public static boolean isWindchime(ItemStack item) {
+		if (!item.getType().equals(Material.AMETHYST_SHARD))
+			return false;
+
+		if (new ItemBuilder(item).customModelData() > WindchimeType.values().length)
+			return false;
+
+		return true;
+
+	}
+
+	@EventHandler
+	public void onClickWindchime(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+
+		if (player.isSneaking()) return;
+		if (!EquipmentSlot.HAND.equals(event.getHand())) return;
+		if (!ActionGroup.RIGHT_CLICK.applies(event)) return;
+
+		ItemFrame itemFrame = PlayerUtils.getTargetItemFrame(player, 4, Map.of(BlockFace.UP, 1));
+		if (itemFrame == null || ItemUtils.isNullOrAir(itemFrame.getItem())) return;
+		if (!isWindchime(itemFrame.getItem())) return;
+
+		if (!Nerd.of(player).getRank().isAdmin()) return;
+
+		event.setCancelled(true);
+		player.sendMessage("\nTODO: make windchime sound");
+	}
+
+	@EventHandler
+	public void onInteractItemFrame(PlayerInteractEntityEvent event) {
+		Player player = event.getPlayer();
+
+		if (player.isSneaking()) return;
+		if (!EquipmentSlot.HAND.equals(event.getHand())) return;
+
+		Entity clicked = event.getRightClicked();
+		if (!(clicked instanceof ItemFrame itemFrame)) return;
+		if (!isWindchime(itemFrame.getItem())) return;
+
+		if (!Nerd.of(player).getRank().isAdmin()) return;
+
+		event.setCancelled(true);
+		player.sendMessage("\nTODO: make windchime sound");
 	}
 
 }
