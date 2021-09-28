51230ee8ca9d4a9f9e70c90265d12a048f3f4438
GriffinCodes <pugabyte@projecteden.gg>
2021-09-27 19:16:54 -0500

    Add permission to /alert <player>

--- src/main/java/gg/projecteden/nexus/features/chat/alerts/AlertCommand.java
+++ src/main/java/gg/projecteden/nexus/features/chat/alerts/AlertCommand.java
@@ -2,10 +2,12 @@ package gg.projecteden.nexus.features.chat.alerts;
 
 import gg.projecteden.nexus.framework.commands.models.CustomCommand;
 import gg.projecteden.nexus.framework.commands.models.annotations.Path;
+import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
 import gg.projecteden.nexus.utils.SoundUtils.Jingle;
 import org.bukkit.entity.Player;
 
+@Permission("group.staff")
 public class AlertCommand extends CustomCommand {
 
 	public AlertCommand(CommandEvent event) {
