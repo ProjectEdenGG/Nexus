cd658fe172787af24e5005e4a768e692d672d881
Caleb <luicelcs@gmail.com>
2021-09-30 21:14:00 -0500

    Refactor DNDCommand.java, add onJoin DND message

--- src/main/java/gg/projecteden/nexus/features/commands/DNDCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/DNDCommand.java
@@ -5,6 +5,7 @@ import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
 import gg.projecteden.nexus.models.dnd.DNDUser;
 import gg.projecteden.nexus.models.dnd.DNDUserService;
+import gg.projecteden.nexus.utils.PlayerUtils;
 import lombok.NoArgsConstructor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,28 +14,30 @@ import org.bukkit.event.player.PlayerJoinEvent;
 @NoArgsConstructor
 public class DNDCommand extends CustomCommand implements Listener {
 	private static final DNDUserService service = new DNDUserService();
+	private DNDUser user;
 
 	public DNDCommand(CommandEvent event) {
 		super(event);
+		if (isPlayerCommandEvent())
+			user = service.get(player());
 	}
 
 	@Path("[state]")
 	public void toggle(Boolean state) {
-		DNDUser user = service.get(event.getPlayer());
-
 		if (state == null)
 			state = !user.isDnd();
 		user.setDnd(state);
 		service.save(user);
 
 		if (state)
-			send(PREFIX + "You have &aenabled Do Not Disturb mode! Make sure you manage what's muted in &e/mutemenu&3.");
+			send(PREFIX + "You have &aenabled &3Do Not Disturb mode! Make sure you manage what's muted in &e/mutemenu&3.");
 		else
-			send(PREFIX + "You have &cdisabled Do Not Disturb mode!");
+			send(PREFIX + "You have &cdisabled &3Do Not Disturb mode!");
 	}
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
-		// TODO
+		if (user.isDnd())
+			PlayerUtils.send(user.getPlayer(), PREFIX + "You currently have Do Not Disturb mode &aenabled&3.");
 	}
 }
