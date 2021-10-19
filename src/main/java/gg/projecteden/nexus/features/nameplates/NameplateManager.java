8226e90dee6b5c1b06c869cd0527fb02171bc04a
GriffinCodes <pugabyte@projecteden.gg>
2021-10-18 22:08:45 -0500

    Ignore error sending to offline players

--- src/main/java/gg/projecteden/nexus/features/nameplates/NameplateManager.java
+++ src/main/java/gg/projecteden/nexus/features/nameplates/NameplateManager.java
@@ -231,7 +231,8 @@ public class NameplateManager {
 
 			Nameplates.debug("    Sending " + packet.getType().getPacketClass().getSimpleName() + " packet to " + player.getName());
 			this.protocolManager.sendServerPacket(player, packet);
-		} catch (InvocationTargetException | IllegalArgumentException ex) {
+		} catch (IllegalArgumentException ignore) {
+		} catch (InvocationTargetException ex) {
 			Nexus.warn("      Unable to send " + packet.getType().getPacketClass().getSimpleName() + " packet to " + player.getName());
 			ex.printStackTrace();
 		}
