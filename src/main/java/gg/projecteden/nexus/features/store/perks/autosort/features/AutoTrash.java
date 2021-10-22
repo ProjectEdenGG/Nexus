9d5d22dd106f29688ee92cce08872145ce766563
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-10-22 08:59:06 -0500

    fix AutoTrash counting itemTags as unique lore

--- src/main/java/gg/projecteden/nexus/features/store/perks/autosort/features/AutoTrash.java
+++ src/main/java/gg/projecteden/nexus/features/store/perks/autosort/features/AutoTrash.java
@@ -1,5 +1,6 @@
 package gg.projecteden.nexus.features.store.perks.autosort.features;
 
+import gg.projecteden.nexus.features.itemtags.ItemTagsUtils;
 import gg.projecteden.nexus.features.resourcepack.models.CustomModel;
 import gg.projecteden.nexus.features.store.perks.autosort.AutoSortFeature;
 import gg.projecteden.nexus.models.autosort.AutoSortUser;
@@ -7,6 +8,7 @@ import gg.projecteden.nexus.models.autosort.AutoSortUser.AutoTrashBehavior;
 import gg.projecteden.nexus.models.dumpster.Dumpster;
 import gg.projecteden.nexus.models.dumpster.DumpsterService;
 import gg.projecteden.nexus.utils.WorldGroup;
+import gg.projecteden.utils.Utils;
 import lombok.NoArgsConstructor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,6 +18,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
+import java.util.List;
 
 @NoArgsConstructor
 public class AutoTrash implements Listener {
@@ -34,7 +37,14 @@ public class AutoTrash implements Listener {
 
 		ItemStack item = event.getItem().getItemStack();
 		ItemMeta meta = item.getItemMeta();
-		if (meta.hasDisplayName() || meta.hasLore() || meta.hasEnchants() || CustomModel.exists(item))
+		List<String> lore = item.clone().getLore();
+		if (meta.hasLore() && !Utils.isNullOrEmpty(lore)) {
+			ItemTagsUtils.clearTags(lore);
+			if (lore.size() > 0)
+				return;
+		}
+
+		if (meta.hasDisplayName() || meta.hasEnchants() || CustomModel.exists(item))
 			return;
 
 		if (!user.getAutoTrashInclude().contains(item.getType()))
