908bac13156a100b4f80ae16a998769fdd31f07f
lexikiq <noellekiq@gmail.com>
2021-12-11 11:34:02 -0500

    Spare Marshy

--- src/main/java/gg/projecteden/nexus/features/commands/staff/SlapCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/staff/SlapCommand.java
@@ -6,13 +6,7 @@ import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
 import gg.projecteden.nexus.models.nerd.Nerd;
 import gg.projecteden.nexus.utils.PlayerUtils;
-import gg.projecteden.nexus.utils.Tasks;
-import gg.projecteden.utils.TimeUtils.TickTime;
 import org.bukkit.entity.Player;
-
-import java.util.concurrent.atomic.AtomicReference;
-
-import static gg.projecteden.nexus.utils.RandomUtils.randomInt;
 
 @Permission("group.staff")
 public class SlapCommand extends CustomCommand {
@@ -30,17 +24,6 @@ public class SlapCommand extends CustomCommand {
 		final Player player = nerd.getOnlinePlayer();
 		player.setVelocity(player.getLocation().getDirection().multiply(-2).setY(player.getEyeLocation().getPitch() > 0 ? 1.5 : -1.5));
 		PlayerUtils.send(nerd, "&6You have been slapped!");
-	}
-
-	static {
-		new AtomicReference<Runnable>() {{
-			set(() -> Tasks.wait(TickTime.MINUTE.x(randomInt(5, 30)), () -> {
-				final Nerd marshy = Nerd.of("Marshy");
-				if (marshy.isOnline())
-					slap(marshy);
-				get().run();
-			}));
-		}}.get().run();
 	}
 
 }
