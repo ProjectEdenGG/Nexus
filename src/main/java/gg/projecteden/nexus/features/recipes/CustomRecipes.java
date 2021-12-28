b7da9e1be735dc1b5cc1f3c414faf306e0bb8fef
GriffinCodes <pugabyte@projecteden.gg>
2021-12-27 22:05:41 -0600

    Add stair->planks recipe

--- src/main/java/gg/projecteden/nexus/features/recipes/CustomRecipes.java
+++ src/main/java/gg/projecteden/nexus/features/recipes/CustomRecipes.java
@@ -273,17 +273,17 @@ public class CustomRecipes extends Feature implements Listener {
 		shapeless().add(choiceOf(MaterialTag.WOOL)).toMake(Material.STRING, 4).build().type(RecipeType.MISC).register();
 		shapeless().add(Material.PRISMARINE).toMake(Material.PRISMARINE_SHARD, 4).build().type(RecipeType.MISC).register();
 		shapeless().add(Material.PRISMARINE_BRICKS).toMake(Material.PRISMARINE_SHARD, 9).build().type(RecipeType.MISC).register();
-
 		shapeless().add(Material.MOSS_CARPET, 3).toMake(Material.MOSS_BLOCK, 2).build().type(RecipeType.MISC).register();
-		for (ColorType color : ColorType.getDyes())
-			shapeless().add(color.getCarpet(), 3).toMake(color.getWool(), 2).build().type(RecipeType.MISC).register();
 
-		for (ColorType color : ColorType.getDyes())
+		for (ColorType color : ColorType.getDyes()) {
+			shapeless().add(color.getCarpet(), 3).toMake(color.getWool(), 2).build().type(RecipeType.MISC).register();
 			shapeless().add(color.getConcrete(), 2).toMake(color.getConcretePowder(), 2).extra("powderize").build().type(RecipeType.MISC).register();
+		}
 
 		for (WoodType wood : WoodType.values()) {
 			shapeless().add(wood.getStrippedLog(), 2).toMake(wood.getLog(), 2).build().type(RecipeType.MISC).register();
 			shapeless().add(wood.getStrippedWood(), 2).toMake(wood.getWood(), 2).build().type(RecipeType.MISC).register();
+			shapeless().add(wood.getStair()).toMake(wood.getPlanks(), 6).build().type(RecipeType.MISC).register();
 		}
 
 		dyeStation();
