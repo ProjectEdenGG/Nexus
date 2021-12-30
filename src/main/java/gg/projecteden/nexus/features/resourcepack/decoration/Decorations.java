bb842db5dc59d142bfc23f1868c261a5299cc524
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2021-12-29 23:41:31 -0600

    fix temp items

--- src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/Decorations.java
+++ src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/Decorations.java
@@ -4,8 +4,6 @@ import gg.projecteden.nexus.features.resourcepack.decoration.common.Decoration;
 import gg.projecteden.nexus.features.resourcepack.decoration.types.Bench;
 import gg.projecteden.nexus.features.resourcepack.decoration.types.BlockDecor;
 import gg.projecteden.nexus.features.resourcepack.decoration.types.Chair;
-import gg.projecteden.nexus.features.resourcepack.decoration.types.LargeFireplace;
-import gg.projecteden.nexus.features.resourcepack.decoration.types.MobPlushie;
 import gg.projecteden.nexus.utils.ItemBuilder.CustomModelData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,9 +12,9 @@ import org.bukkit.inventory.ItemStack;
 
 @AllArgsConstructor
 public enum Decorations {
-	MOB_PLUSHIE_ENDERDRAGON(new MobPlushie("Ender Dragon MobPlushie", -1, 5.0)),
-	FIREPLACE_LARGE_DARK(new LargeFireplace("Dark Fireplace", -1)),
-	DYE_STATION(new BlockDecor("Dye Station", 1, Material.CRAFTING_TABLE)),
+//	MOB_PLUSHIE_ENDERDRAGON(new MobPlushie("Ender Dragon MobPlushie", -1, 5.0)),
+//	FIREPLACE_LARGE_DARK(new LargeFireplace("Dark Fireplace", -1)),
+DYE_STATION(new BlockDecor("Dye Station", 1, Material.CRAFTING_TABLE)),
 	CHAIR_WOODEN_BASIC(new Chair("Wooden Chair", 400)),
 	CHAIR_WOODEN_CUSHION(new Chair("Cushioned Wooden Chair", 401)),
 	BENCH_WOODEN(new Bench("Wooden Bench", 450));
