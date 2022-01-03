e6080af3970f41e3734a5e6ee6fe05278507de11
GriffinCodes <pugabyte@projecteden.gg>
2022-01-02 22:47:53 -0600

    Objectives

---
+++ src/main/java/gg/projecteden/nexus/models/objective/ObjectiveUserService.java
@@ -0,0 +1,19 @@
+package gg.projecteden.nexus.models.objective;
+
+
+import gg.projecteden.mongodb.annotations.ObjectClass;
+import gg.projecteden.nexus.framework.persistence.mongodb.player.MongoPlayerService;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ObjectClass(ObjectiveUser.class)
+public class ObjectiveUserService extends MongoPlayerService<ObjectiveUser> {
+	private final static Map<UUID, ObjectiveUser> cache = new ConcurrentHashMap<>();
+
+	public Map<UUID, ObjectiveUser> getCache() {
+		return cache;
+	}
+
+}
