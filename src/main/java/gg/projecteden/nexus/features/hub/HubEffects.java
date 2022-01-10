577d2a4b953baa998b488ec3104c979c2d3cebbc
GriffinCodes <pugabyte@projecteden.gg>
2022-01-09 22:21:42 -0600

    Cleanup hub effects

--- src/main/java/gg/projecteden/nexus/features/hub/HubEffects.java
+++ src/main/java/gg/projecteden/nexus/features/hub/HubEffects.java
@@ -5,33 +5,37 @@ import gg.projecteden.nexus.features.effects.Effects;
 import gg.projecteden.nexus.utils.RandomUtils;
 import gg.projecteden.nexus.utils.Tasks;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class HubEffects extends Effects {
 
 	@Override
 	public void particles() {
-		ParticleBuilder creative_splash = new ParticleBuilder(Particle.WATER_SPLASH)
-			.location(loc(81.5, 130.3, 60.5))
-			.offset(0.15, 0, 0.15);
+		List<ParticleBuilder> particles = List.of(
 
-		ParticleBuilder creative_drip = new ParticleBuilder(Particle.DRIPPING_DRIPSTONE_WATER)
-			.location(loc(100.5, 137.9, 39.5))
-			.offset(0.15, 0, 0.15);
+			new ParticleBuilder(Particle.DRIPPING_DRIPSTONE_WATER)
+				.location(loc(100.5, 137.9, 39.5))
+				.offset(0.15, 0, 0.15),
 
-		List<ParticleBuilder> particles = new ArrayList<>(Arrays.asList(creative_drip, creative_splash));
+			new ParticleBuilder(Particle.WATER_SPLASH)
+				.location(loc(81.5, 130.3, 60.5))
+				.offset(0.15, 0, 0.15)
+
+		);
 
 		Tasks.repeat(0, 2, () -> {
 			for (ParticleBuilder particleBuilder : particles) {
-				if (RandomUtils.chanceOf(50)) {
-					if (hasNearbyPlayers(25, particleBuilder.location()))
+				final Location location = particleBuilder.location();
+				if (location == null || !location.isChunkLoaded())
+					continue;
+
+				if (RandomUtils.chanceOf(50))
+					if (hasNearbyPlayers(25, location))
 						particleBuilder.spawn();
-				}
 			}
 		});
 	}
