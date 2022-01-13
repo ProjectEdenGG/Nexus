b5fca060fff718d95347b51d6aa8161e9a11bb1c
GriffinCodes <pugabyte@projecteden.gg>
2022-01-12 18:31:05 -0600

    Add /ambience near

--- src/main/java/gg/projecteden/nexus/features/ambience/AmbienceCommand.java
+++ src/main/java/gg/projecteden/nexus/features/ambience/AmbienceCommand.java
@@ -7,6 +7,7 @@ import gg.projecteden.nexus.framework.commands.models.annotations.Arg;
 import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission.Group;
+import gg.projecteden.nexus.framework.commands.models.annotations.Switch;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
 import gg.projecteden.nexus.models.ambience.AmbienceConfig;
 import gg.projecteden.nexus.models.ambience.AmbienceConfig.Ambience;
@@ -17,7 +18,6 @@ import gg.projecteden.nexus.models.ambience.AmbienceUserService;
 import gg.projecteden.nexus.utils.JsonBuilder;
 import gg.projecteden.nexus.utils.PlayerUtils;
 import gg.projecteden.nexus.utils.RandomUtils;
-import gg.projecteden.nexus.utils.StringUtils;
 import gg.projecteden.nexus.utils.Tasks;
 import gg.projecteden.utils.TimeUtils.TickTime;
 import lombok.NoArgsConstructor;
@@ -37,6 +37,8 @@ import java.util.List;
 import java.util.function.BiFunction;
 
 import static gg.projecteden.nexus.utils.Nullables.isNullOrAir;
+import static gg.projecteden.nexus.utils.StringUtils.getLocationString;
+import static gg.projecteden.nexus.utils.StringUtils.getTeleportCommand;
 
 @NoArgsConstructor
 @Permission(Group.STAFF)
@@ -107,11 +109,36 @@ public class AmbienceCommand extends CustomCommand implements Listener {
 
 		send(PREFIX + camelCase(type) + " ambience:");
 		final BiFunction<Ambience, String, JsonBuilder> formatter = (ambience, index) ->
-			json(index + " &e" + StringUtils.getLocationString(ambience.getLocation()))
-				.command(StringUtils.getTeleportCommand(ambience.getLocation()))
+			json(index + " &e" + getLocationString(ambience.getLocation()))
+				.command(getTeleportCommand(ambience.getLocation()))
 				.hover("&eClick to teleport");
 
 		paginate(ambiences, formatter, "/ambience list " + type.name().toLowerCase(), page);
+	}
+
+	@Path("near [type] [page] [--radius]")
+	void near(AmbienceType type, @Arg("1") int page, @Switch @Arg("20") int radius) {
+		List<Ambience> ambiences;
+		if (type == null)
+			ambiences = config.getAmbiences();
+		else
+			ambiences = config.get(type);
+
+		ambiences = ambiences.stream()
+			.filter(ambience -> ambience.getLocation().getWorld().equals(world()))
+			.filter(ambience -> ambience.getLocation().distance(location()) <= radius)
+			.toList();
+
+		if (ambiences.isEmpty())
+			error("No " + (type == null ? "" : camelCase(type) + " ") + "ambience found within " + radius + " blocks");
+
+		send(PREFIX + "Nearby " + (type == null ? "" : camelCase(type) + " ") + "ambience:");
+		final BiFunction<Ambience, String, JsonBuilder> formatter = (ambience, index) ->
+			json(index + " &e" + getLocationString(ambience.getLocation()))
+				.command(getTeleportCommand(ambience.getLocation()))
+				.hover("&eClick to teleport");
+
+		paginate(ambiences, formatter, "/ambience near " + (type == null ? "" : type.name().toLowerCase()) + " --radius=" + radius, page);
 	}
 
 	@Path("play")
