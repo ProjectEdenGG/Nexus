074e09463a508c5fb93c2c6226c387466dffa93a
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2022-01-13 23:45:22 -0600

    fix colors

--- src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/DecorationType.java
+++ src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/DecorationType.java
@@ -30,7 +30,7 @@ public enum DecorationType {
 	CHAIR_WOODEN_BASIC(new Chair("Wooden Chair", 400, DyedPart.WHOLE)),
 	CHAIR_WOODEN_CUSHION(new Chair("Cushioned Wooden Chair", 401, DyedPart.CUSHION)),
 	// Stools
-	STOOL_WOODEN_BASIC(new Chair("Wooden Stool", 500, DyedPart.CUSHION)),
+	STOOL_WOODEN_BASIC(new Chair("Wooden Stool", 500, DyedPart.WHOLE)),
 	STOOL_WOODEN_CUSHION(new Chair("Cushioned Wooden Stool", 501, DyedPart.CUSHION)),
 	// Benches
 	BENCH_WOODEN(new Bench("Wooden Bench", 450, DyedPart.WHOLE)),
