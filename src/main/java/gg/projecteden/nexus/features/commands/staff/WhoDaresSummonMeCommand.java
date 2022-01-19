a2ef7085a77e988178c1f19e6600c43e8f4fb038
GriffinCodes <pugabyte@projecteden.gg>
2022-01-18 20:23:49 -0600

    Change WhoDaresSummonMe permission & add cooldown

--- src/main/java/gg/projecteden/nexus/features/commands/staff/WhoDaresSummonMeCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/staff/WhoDaresSummonMeCommand.java
@@ -1,8 +1,10 @@
 package gg.projecteden.nexus.features.commands.staff;
 
 import gg.projecteden.nexus.framework.commands.models.CustomCommand;
+import gg.projecteden.nexus.framework.commands.models.annotations.Cooldown;
 import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
+import gg.projecteden.nexus.framework.commands.models.annotations.Permission.Group;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
 import gg.projecteden.nexus.utils.Tasks;
 import gg.projecteden.utils.TimeUtils.TickTime;
@@ -12,7 +14,8 @@ import org.bukkit.entity.Bat;
 import java.util.ArrayList;
 import java.util.List;
 
-@Permission("pv.see")
+@Permission(Group.MODERATOR)
+@Cooldown(value = TickTime.MINUTE, x = 5)
 public class WhoDaresSummonMeCommand extends CustomCommand {
 
 	public WhoDaresSummonMeCommand(CommandEvent event) {
@@ -29,9 +32,8 @@ public class WhoDaresSummonMeCommand extends CustomCommand {
 		world.strikeLightningEffect(location().clone().subtract(5, 0, 0));
 		world.strikeLightningEffect(location().clone().subtract(0, 0, 5));
 		List<Bat> bats = new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++)
 			bats.add(world.spawn(location(), Bat.class));
-		}
 		Tasks.wait(TickTime.SECOND.x(5), () -> bats.forEach(Bat::remove));
 	}
 
