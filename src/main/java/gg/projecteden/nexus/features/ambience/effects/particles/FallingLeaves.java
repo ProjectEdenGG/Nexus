ed022c7b6035344c3101999211ddd626938c62b1
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2022-01-26 22:25:53 -0600

    improve falling leaves particle and colors

--- src/main/java/gg/projecteden/nexus/features/ambience/effects/particles/FallingLeaves.java
+++ src/main/java/gg/projecteden/nexus/features/ambience/effects/particles/FallingLeaves.java
@@ -6,18 +6,23 @@ import gg.projecteden.nexus.features.particles.effects.DotEffect;
 import gg.projecteden.nexus.models.ambience.AmbienceUser;
 import gg.projecteden.nexus.utils.RandomUtils;
 import gg.projecteden.utils.TimeUtils.TickTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 @NoArgsConstructor
 public class FallingLeaves extends ParticleEffect {
-	private Material material;
+	private LeavesParticle leavesParticle;
 	private double x;
 	private double y;
 	private double z;
@@ -25,9 +30,9 @@ public class FallingLeaves extends ParticleEffect {
 	public static final int LIFE = TickTime.SECOND.x(6);
 
 	public FallingLeaves(AmbienceUser user, Block block, double chance) {
-		super(user, ParticleEffectType.FALLING_LEAVES, Particle.BLOCK_CRACK, LIFE, chance);
+		super(user, ParticleEffectType.FALLING_LEAVES, Particle.FALLING_DUST, LIFE, chance);
 
-		this.material = block.getType();
+		this.leavesParticle = LeavesParticle.of(block.getType());
 		this.x = block.getX();
 		this.y = block.getY();
 		this.z = block.getZ();
@@ -43,10 +48,51 @@ public class FallingLeaves extends ParticleEffect {
 		double yRange = y - 0.05;
 		double zRange = z + RandomUtils.randomDouble(.2, .8);
 
-		player.spawnParticle(particle, xRange, yRange, zRange, 0, 0, 0, 0, 1, Bukkit.createBlockData(material));
+		Material particleMaterial = leavesParticle.getBiomeMaterial(player);
+
+		player.spawnParticle(particle, xRange, yRange, zRange, 0, 0, 0, 0, 1, Bukkit.createBlockData(particleMaterial));
 
 		if (user.isDebug())
 			DotEffect.debug(player, new Location(player.getWorld(), xRange, yRange, zRange), Color.RED);
+	}
+
+	@AllArgsConstructor
+	@RequiredArgsConstructor
+	private enum LeavesParticle {
+		OAK(Material.GREEN_CONCRETE, Material.GREEN_TERRACOTTA),
+		SPRUCE(Material.SPRUCE_LEAVES),
+		BIRCH(Material.GREEN_TERRACOTTA),
+		JUNGLE(Material.GREEN_CONCRETE, Material.GREEN_TERRACOTTA),
+		ACACIA(Material.GREEN_CONCRETE, Material.GREEN_TERRACOTTA),
+		DARK_OAK(Material.GREEN_CONCRETE, Material.GREEN_TERRACOTTA),
+		AZALEA(Material.GREEN_CONCRETE),
+		FLOWERING_AZALEA(Material.GREEN_CONCRETE),
+		;
+
+		@Getter
+		private final Material material;
+		@Getter
+		private Material swampMaterial = null;
+
+		public static @NonNull LeavesParticle of(Material material) {
+			String leavesStr = material.name().toUpperCase().replace("_LEAVES", "");
+
+			for (LeavesParticle leavesParticle : LeavesParticle.values()) {
+				if (leavesParticle.name().equalsIgnoreCase(leavesStr))
+					return leavesParticle;
+			}
+
+			return OAK;
+		}
+
+		public Material getBiomeMaterial(Player player) {
+			Biome biome = player.getLocation().getBlock().getBiome();
+			if (biome == Biome.SWAMP) {
+				return this.getSwampMaterial();
+			}
+
+			return this.material;
+		}
 	}
 
 }
