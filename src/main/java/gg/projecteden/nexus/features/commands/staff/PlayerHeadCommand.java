4f290ded16ad4602f26daaa7e9a0d161938191fa
GriffinCodes <pugabyte@projecteden.gg>
2022-01-30 12:04:03 -0600

    Fix playerhead cooldown & limit worlds

--- src/main/java/gg/projecteden/nexus/features/commands/staff/PlayerHeadCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/staff/PlayerHeadCommand.java
@@ -4,19 +4,23 @@ import gg.projecteden.nexus.Nexus;
 import gg.projecteden.nexus.framework.commands.models.CustomCommand;
 import gg.projecteden.nexus.framework.commands.models.annotations.Aliases;
 import gg.projecteden.nexus.framework.commands.models.annotations.Arg;
-import gg.projecteden.nexus.framework.commands.models.annotations.Cooldown;
 import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission.Group;
 import gg.projecteden.nexus.framework.commands.models.annotations.Redirects.Redirect;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
+import gg.projecteden.nexus.framework.exceptions.postconfigured.CommandCooldownException;
+import gg.projecteden.nexus.models.cooldown.CooldownService;
 import gg.projecteden.nexus.models.nerd.Nerd;
 import gg.projecteden.nexus.utils.ItemBuilder;
+import gg.projecteden.nexus.utils.WorldGroup;
 import gg.projecteden.utils.TimeUtils.TickTime;
 import lombok.NonNull;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
 
 import static gg.projecteden.nexus.features.commands.staff.PlayerHeadCommand.PERMISSION;
 import static gg.projecteden.nexus.utils.Nullables.isNullOrAir;
@@ -26,14 +30,28 @@ import static gg.projecteden.nexus.utils.Nullables.isNullOrAir;
 @Redirect(from = "/donorskull", to = "/playerhead")
 public class PlayerHeadCommand extends CustomCommand {
 	public static final String PERMISSION = "essentials.skull";
+	private static final List<WorldGroup> allowedWorldGroups = List.of(
+		WorldGroup.SURVIVAL,
+		WorldGroup.CREATIVE,
+		WorldGroup.SKYBLOCK,
+		WorldGroup.ONEBLOCK
+	);
 
 	public PlayerHeadCommand(@NonNull CommandEvent event) {
 		super(event);
 	}
 
 	@Path("[owner]")
-	@Cooldown(value = TickTime.DAY, bypass = Group.STAFF)
 	void run(@Arg(value = "self", permission = Group.STAFF) Nerd owner) {
+		if (!isStaff()) {
+			if (!allowedWorldGroups.contains(worldGroup()))
+				error("You cannot use this command in this world");
+
+			final String cooldownId = "playerhead-" + worldGroup().name();
+			if (!new CooldownService().check(player(), cooldownId, TickTime.DAY))
+				throw new CommandCooldownException(player(), cooldownId);
+		}
+
 		giveItem(new ItemBuilder(Material.PLAYER_HEAD).name("&f" + owner.getNickname() + "'s Head").skullOwner(owner).build());
 	}
 
