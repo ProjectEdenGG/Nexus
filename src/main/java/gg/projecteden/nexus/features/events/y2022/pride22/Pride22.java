ff873a88b0ebec8a0df202c717d7812496783c56
GriffinCodes <pugabyte@projecteden.gg>
2022-04-17 15:15:57 -0500

    Set up Pride22 and Halloween22

---
+++ src/main/java/gg/projecteden/nexus/features/events/y2022/pride22/Pride22.java
@@ -0,0 +1,32 @@
+package gg.projecteden.nexus.features.events.y2022.pride22;
+
+import gg.projecteden.nexus.features.events.EdenEvent;
+import gg.projecteden.nexus.features.events.y2022.pride22.quests.Pride22Entity;
+import gg.projecteden.nexus.features.events.y2022.pride22.quests.Pride22NPC;
+import gg.projecteden.nexus.features.events.y2022.pride22.quests.Pride22QuestItem;
+import gg.projecteden.nexus.features.events.y2022.pride22.quests.Pride22QuestReward;
+import gg.projecteden.nexus.features.events.y2022.pride22.quests.Pride22QuestTask;
+import gg.projecteden.nexus.features.quests.QuestConfig;
+import gg.projecteden.nexus.framework.annotations.Date;
+import gg.projecteden.nexus.framework.features.Features;
+import gg.projecteden.nexus.models.warps.WarpType;
+
+@QuestConfig(
+	tasks = Pride22QuestTask.class,
+	npcs = Pride22NPC.class,
+	entities = Pride22Entity.class,
+	items = Pride22QuestItem.class,
+	rewards = Pride22QuestReward.class,
+	start = @Date(m = 6, d = 1, y = 2022),
+	end = @Date(m = 6, d = 30, y = 2022),
+	world = "pride22",
+	region = "pride22",
+	warpType = WarpType.PRIDE22
+)
+public class Pride22 extends EdenEvent {
+
+	public static Pride22 get() {
+		return Features.get(Pride22.class);
+	}
+
+}
---
+++ src/main/java/gg/projecteden/nexus/features/events/y2022/pride22/quests/Pride22QuestTask.java
@@ -0,0 +1,19 @@
+package gg.projecteden.nexus.features.events.y2022.pride22.quests;
+
+import gg.projecteden.nexus.features.quests.tasks.common.IQuestTask;
+import gg.projecteden.nexus.features.quests.tasks.common.QuestTask.TaskBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Pride22QuestTask implements IQuestTask {
+	;
+
+	private final TaskBuilder<?, ?, ?> task;
+
+	@Override
+	public TaskBuilder<?, ?, ?> get() {
+		return task;
+	}
+}
