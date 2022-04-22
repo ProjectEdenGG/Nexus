236705bf841ecda91b6cf079f42f7b8bd25f30f0
Lexi <lexi@qixils.dev>
2022-04-21 20:59:47 -0400

    fix compile on new paper/parchment

--- src/main/java/gg/projecteden/nexus/features/customenchants/CustomEnchant.java
+++ src/main/java/gg/projecteden/nexus/features/customenchants/CustomEnchant.java
@@ -128,4 +128,9 @@ public abstract class CustomEnchant extends Enchantment {
 		return items;
 	}
 
+	@Override
+	public @NotNull String translationKey() {
+		// TODO
+		throw new UnsupportedOperationException();
+	}
 }
