557c9e443e68bbf700a8fb056caee9e2cacdbda5
GriffinCodes <pugabyte@projecteden.gg>
2022-04-25 18:29:43 -0500

    Add ability to force birthday announcement

--- src/main/java/gg/projecteden/nexus/models/scheduledjobs/jobs/BirthdaysAddRoleJob.java
+++ src/main/java/gg/projecteden/nexus/models/scheduledjobs/jobs/BirthdaysAddRoleJob.java
@@ -3,14 +3,12 @@ package gg.projecteden.nexus.models.scheduledjobs.jobs;
 import gg.projecteden.annotations.Async;
 import gg.projecteden.models.scheduledjobs.common.AbstractJob;
 import gg.projecteden.models.scheduledjobs.common.Schedule;
-import gg.projecteden.nexus.features.discord.Discord;
+import gg.projecteden.nexus.features.commands.BirthdaysCommand;
 import gg.projecteden.nexus.models.discord.DiscordUser;
 import gg.projecteden.nexus.models.discord.DiscordUserService;
 import gg.projecteden.nexus.models.geoip.GeoIP;
 import gg.projecteden.nexus.models.geoip.GeoIPService;
 import gg.projecteden.nexus.models.nerd.Nerd;
-import gg.projecteden.utils.DiscordId.Role;
-import gg.projecteden.utils.DiscordId.TextChannel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -58,13 +56,7 @@ public class BirthdaysAddRoleJob extends AbstractJob {
 			if (discordUser.getMember() == null)
 				continue;
 
-			final Role role = nerd.getRank().isStaff() ? Role.STAFF_BIRTHDAY : Role.BIRTHDAY;
-			discordUser.addRole(role);
-
-			Discord.koda("Happy Birthday " + discordUser.getMember().getAsMention() + "!", TextChannel.BIRTHDAYS).thenAccept(message ->
-				message.createThreadChannel("Wish " + nerd.getNickname() + " a happy birthday!").queue());
-
-			new BirthdaysRemoveRoleJob(nerd.getUuid()).schedule(LocalDateTime.now().plusDays(1));
+			BirthdaysCommand.announcement(discordUser);
 		}
 		return completed();
 	}
