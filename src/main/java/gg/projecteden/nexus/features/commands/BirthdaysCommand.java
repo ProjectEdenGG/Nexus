dc9ff3da531c0bc66464d969e1b64b19e7254dbe
GriffinCodes <pugabyte@projecteden.gg>
2022-04-25 19:45:15 -0500

    Add /birthday of/unset, cleanup

--- src/main/java/gg/projecteden/nexus/features/commands/BirthdaysCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/BirthdaysCommand.java
@@ -22,7 +22,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.function.BiFunction;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
@@ -30,19 +29,30 @@ import static java.time.temporal.ChronoUnit.DAYS;
 @Description("View player birthdays")
 public class BirthdaysCommand extends CustomCommand {
 	private final NerdService service = new NerdService();
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
 
 	public BirthdaysCommand(CommandEvent event) {
 		super(event);
 	}
 
+	@Path("of [player]")
+	void of(@Arg("self") Nerd nerd) {
+		if (nerd.getBirthday() == null)
+			error((isSelf(nerd) ? "You haven't" : nerd.getNickname() + " hasn't") + " set a birthday");
+
+		send(PREFIX + (isSelf(nerd) ? "Your" : nerd.getNickname() + "'s") + " birthday is &e" + formatter.format(nerd.getBirthday()));
+	}
+
 	@Path("[page]")
-	void birthday(@Arg("1") int page) {
+	void list(@Arg("1") int page) {
 		List<Nerd> nerds = service.getNerdsWithBirthdays();
 		final LocalDate now = LocalDate.now();
 
 		nerds.sort((nerd1, nerd2) -> (int) DAYS.between(now, getNextBirthday(nerd1)) - (int) DAYS.between(now, getNextBirthday(nerd2)));
 
-		final BiFunction<Nerd, String, JsonBuilder> formatter = (nerd, index) -> {
+		line();
+		send(PREFIX + "Upcoming birthdays:");
+		paginate(nerds, (nerd, index) -> {
 			final JsonBuilder json = json("&3" + index + " &e" + nerd.getColoredName() + " &7- ");
 
 			long until = DAYS.between(now, getNextBirthday(nerd));
@@ -52,25 +62,32 @@ public class BirthdaysCommand extends CustomCommand {
 			else
 				json.next("&7" + until + plural(" day", until));
 
-			return json.hover(nerd.getBirthday().format(DateTimeFormatter.ofPattern("MMMM d, yyyy")));
-		};
-
-		line();
-		send(PREFIX + "Upcoming birthdays:");
-		paginate(nerds, formatter, "/birthdays", page);
+			return json.hover(formatter.format(nerd.getBirthday()));
+		}, "/birthdays", page);
 	}
 
 	@Path("set <birthday> [player]")
 	void set(LocalDate birthday, @Arg(value = "self", permission = Group.SENIOR_STAFF) Nerd nerd) {
-		final String formatted = DateTimeFormatter.ofPattern("MMMM d, yyyy").format(birthday);
+		final String formatted = formatter.format(birthday);
 
 		if (!isStaff())
 			if (nerd.getBirthday() != null)
-				error("You have already set your birthday to &e" + birthday + ". If it is incorrect, please ask a staff member to change it.");
+				error("You have already set your birthday to &e" + formatted + ". If it is incorrect, please ask a staff member to change it.");
 
 		nerd.setBirthday(birthday);
 		service.save(nerd);
 		send(PREFIX + (isSelf(nerd) ? "Your" : Nickname.of(nerd) + "'s") + " birthday has been set to &e" + formatted);
+	}
+
+	@Path("unset [player]")
+	@Permission(Group.STAFF)
+	void unset(Nerd nerd) {
+		if (nerd.getBirthday() == null)
+			error(nerd.getNickname() + " does not have a birthday set");
+
+		nerd.setBirthday(null);
+		service.save(nerd);
+		send(PREFIX + "Unset " + nerd.getNickname() + "'s birthday");
 	}
 
 	@Permission(Group.ADMIN)
