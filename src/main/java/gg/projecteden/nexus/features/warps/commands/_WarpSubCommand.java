c48c0fb4fd03c434f13ea5fabd8d3dafe7886529
GriffinCodes <pugabyte@projecteden.gg>
2022-06-03 12:33:20 -0500

    Fix hub bugs

--- src/main/java/gg/projecteden/nexus/features/warps/commands/_WarpSubCommand.java
+++ src/main/java/gg/projecteden/nexus/features/warps/commands/_WarpSubCommand.java
@@ -14,49 +14,49 @@ public abstract class _WarpSubCommand extends _WarpCommand {
 		super(event);
 	}
 
-	@Path("warps (list|warps) [filter]")
+	@Path("(warp|warps) list [filter]")
 	@Description("List available warps")
 	public void list(@Arg(tabCompleter = Warp.class) String filter) {
 		super.list(filter);
 	}
 
-	@Path("warps (set|create) <name>")
+	@Path("(warp|warps) (set|create) <name>")
 	@Description("Create a new warp")
 	public void set(@Arg(tabCompleter = Warp.class) String name) {
 		super.set(name);
 	}
 
-	@Path("warps reset <name>")
+	@Path("(warp|warps) reset <name>")
 	@Description("Update a warp's location")
 	public void reset(@Arg(tabCompleter = Warp.class) String name) {
 		super.reset(name);
 	}
 
-	@Path("warps (rm|remove|delete|del) <name>")
+	@Path("(warp|warps) (rm|remove|delete|del) <name>")
 	@Description("Delete a warp")
 	public void delete(Warp warp) {
 		super.delete(warp);
 	}
 
-	@Path("warps (teleport|tp|warp) <name>")
+	@Path("(warp|warps) (teleport|tp|warp) <name>")
 	@Description("Teleport to a warp")
 	public void teleport(Warp warp) {
 		super.teleport(warp);
 	}
 
-	@Path("warps <name>")
+	@Path("(warp|warps) <name>")
 	@Description("Teleport to a warp")
 	public void tp(Warp warp) {
 		super.tp(warp);
 	}
 
-	@Path("warps tp nearest")
+	@Path("(warp|warps) tp nearest")
 	@Description("Teleport to the nearest warp")
 	public void teleportNearest() {
 		super.teleportNearest();
 	}
 
-	@Path("warps nearest")
+	@Path("(warp|warps) nearest")
 	@Description("View the nearest warp")
 	public void nearest() {
 		super.nearest();
