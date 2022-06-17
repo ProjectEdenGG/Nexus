5962fdeef04205f3b188162056cfdf82629f51f7
Lexi <lexi@qixils.dev>
2022-06-16 21:07:06 -0400

    fix: tracker is finally perfect

--- src/main/java/gg/projecteden/nexus/features/survival/KnackBeacon.java
+++ src/main/java/gg/projecteden/nexus/features/survival/KnackBeacon.java
@@ -4,14 +4,16 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockTypes;
 import gg.projecteden.nexus.framework.features.Feature;
+import gg.projecteden.nexus.utils.JsonBuilder;
 import gg.projecteden.nexus.utils.PlayerUtils.Dev;
 import gg.projecteden.nexus.utils.Tasks;
 import gg.projecteden.nexus.utils.WorldGuardUtils;
 import gg.projecteden.utils.TimeUtils.TickTime;
 import net.kyori.adventure.bossbar.BossBar.Listener;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Chest;
 import org.bukkit.boss.KeyedBossBar;
@@ -20,12 +22,10 @@ import org.bukkit.inventory.ItemStack;
 import java.util.List;
 
 public class KnackBeacon extends Feature implements Listener {
-	private static final int GOAL = 5904;
+	private static final double GOAL = 5904;
 	private final List<Location> chests = List.of(
 		new Location(Bukkit.getWorld("survival"), -2145.5, 50.5, 1797.5),
-		new Location(Bukkit.getWorld("survival"), -2145.5, 50.5, 1796.5),
-		new Location(Bukkit.getWorld("survival"), -2145.5, 51.5, 1797.5),
-		new Location(Bukkit.getWorld("survival"), -2145.5, 51.5, 1796.5)
+		new Location(Bukkit.getWorld("survival"), -2145.5, 51.5, 1797.5)
 	);
 	private KeyedBossBar bossBar;
 
@@ -37,7 +37,7 @@ public class KnackBeacon extends Feature implements Listener {
 			return;
 		}
 
-		Tasks.repeat(1, TickTime.SECOND.x(10), () -> {
+		Tasks.repeat(1, TickTime.SECOND.x(5), () -> {
 			int obtained = 0;
 			// count blocks in beacon
 			WorldGuardUtils wg = new WorldGuardUtils("survival");
@@ -56,15 +56,20 @@ public class KnackBeacon extends Feature implements Listener {
 				}
 				for (ItemStack item : chest.getInventory()) {
 					if (item == null) continue;
-					if (item.getType().equals(Material.NETHERITE_INGOT)) {
-						obtained += (item.getAmount() * 4);
-					} else if (item.getType().equals(Material.NETHERITE_SCRAP)) {
-						obtained += item.getAmount();
-					}
+					int multiplier = switch (item.getType()) {
+						case NETHERITE_SCRAP -> 1;
+						case NETHERITE_INGOT -> 4;
+						case NETHERITE_BLOCK -> 9*4;
+						default -> 0;
+					};
+					obtained += (item.getAmount() * multiplier);
 				}
 			}
 			// update boss bar
-			bossBar.setProgress((double) obtained / GOAL);
+			double progress = obtained / GOAL;
+			bossBar.setProgress(Math.min(1, progress));
+			Component title = new JsonBuilder("&cNetherite Beacon Progress &7(%.2f%%)".formatted(progress * 100)).build();
+			bossBar.setTitle(LegacyComponentSerializer.legacySection().serialize(title));
 		});
 	}
 }
