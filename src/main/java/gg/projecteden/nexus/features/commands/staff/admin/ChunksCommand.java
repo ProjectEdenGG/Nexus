5dd79a451c4d7b64b4832bec715f4205e977922d
GriffinCodes <griffin@projecteden.gg>
2022-06-26 08:13:24 -0500

    Add /chunks forceloaded list

--- src/main/java/gg/projecteden/nexus/features/commands/staff/admin/ChunksCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/staff/admin/ChunksCommand.java
@@ -4,15 +4,21 @@ import com.sk89q.worldedit.regions.Region;
 import gg.projecteden.api.common.utils.CompletableFutures;
 import gg.projecteden.nexus.framework.commands.models.CustomCommand;
 import gg.projecteden.nexus.framework.commands.models.annotations.Aliases;
+import gg.projecteden.nexus.framework.commands.models.annotations.Arg;
 import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission.Group;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
+import gg.projecteden.nexus.utils.JsonBuilder;
 import lombok.NonNull;
 import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 @Aliases("chunk")
@@ -21,6 +27,24 @@ public class ChunksCommand extends CustomCommand {
 
 	public ChunksCommand(@NonNull CommandEvent event) {
 		super(event);
+	}
+
+	@Path("forceLoaded list [world] [page]")
+	void forceLoaded_list(@Arg("current") World world, @Arg("1") int page) {
+		final var chunks = Arrays.stream(world.getLoadedChunks())
+			.filter(Chunk::isForceLoaded)
+			.toList();
+
+		if (chunks.isEmpty())
+			error("No force loaded chunks in world &e" + world.getName());
+
+		send(PREFIX + "Force loaded chunks in &e" + world.getName());
+
+		final BiFunction<@NotNull Chunk, String, JsonBuilder> formatter = (chunk, index) ->
+			json("&3" + index + " &e" + chunk.getX() + ", " + chunk.getZ())
+				.command("/tppos " + ((chunk.getX() << 4) + 8) + " 200 " + ((chunk.getZ() << 4) + 8) + " " + world.getName());
+
+		paginate(chunks, formatter, "/chunks forceLoaded list " + world.getName(), page);
 	}
 
 	@Path("forceLoaded get")
