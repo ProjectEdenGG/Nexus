3f863c3780e644e812290fe16c85c50b48d2b4cb
GriffinCodes <griffin@projecteden.gg>
2022-07-07 00:27:45 -0500

    Comment out spin attack

--- src/main/java/gg/projecteden/nexus/features/wither/fights/CorruptedFight.java
+++ src/main/java/gg/projecteden/nexus/features/wither/fights/CorruptedFight.java
@@ -1,28 +1,24 @@
 package gg.projecteden.nexus.features.wither.fights;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.PacketTypeEnum;
-import com.comphenix.protocol.events.ListeningWhitelist;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.events.PacketListener;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import gg.projecteden.api.common.utils.EnumUtils;
 import gg.projecteden.api.common.utils.TimeUtils.TickTime;
-import gg.projecteden.nexus.Nexus;
 import gg.projecteden.nexus.features.crates.models.CrateType;
 import gg.projecteden.nexus.features.wither.WitherChallenge;
 import gg.projecteden.nexus.features.wither.models.WitherFight;
-import gg.projecteden.nexus.utils.*;
+import gg.projecteden.nexus.utils.EntityUtils;
+import gg.projecteden.nexus.utils.PlayerUtils;
+import gg.projecteden.nexus.utils.PotionEffectBuilder;
+import gg.projecteden.nexus.utils.RandomUtils;
+import gg.projecteden.nexus.utils.Tasks;
+import gg.projecteden.nexus.utils.WorldEditUtils;
+import gg.projecteden.nexus.utils.WorldGuardUtils;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
-import net.minecraft.world.entity.Pose;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
-import org.bukkit.craftbukkit.v1_19_R1.entity.CraftWither;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Silverfish;
@@ -35,11 +31,15 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.util.Vector;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -285,104 +285,105 @@ public class CorruptedFight extends WitherFight {
 				}
 			}
 		},
-		SPIN_ATTACK {
-			@Override
-			public void execute(List<Player> players) {
-				Wither wither = WitherChallenge.currentFight.wither;
-				wither.setInvulnerable(true);
-
-				Location freezeLoc = wither.getLocation();
-				Player target = WitherChallenge.currentFight.getRandomAlivePlayer();
-
-				net.minecraft.world.entity.boss.wither.WitherBoss witherBoss = ((CraftWither) wither).getHandle();
-
-				SpinPacketListener listener = new SpinPacketListener();
-				Nexus.getProtocolManager().addPacketListener(listener);
-
-				ClientboundSetEntityDataPacket packet = new ClientboundSetEntityDataPacket(wither.getEntityId(), witherBoss.getEntityData(), true);
-				for (Player player : wither.getTrackedPlayers())
-					((CraftPlayer) player).getHandle().connection.send(packet);
-
-				AtomicInteger taskId1 = new AtomicInteger();
-				taskId1.set(Tasks.repeat(1, 1, () -> wither.lookAt(target)));
-
-				AtomicInteger taskId2 = new AtomicInteger();
-				taskId2.set(Tasks.repeat(1, 1, () -> {
-					wither.teleport(freezeLoc);
-					wither.setTarget(null);
-				}));
-
-				AtomicInteger taskId3 = new AtomicInteger();
-				taskId3.set(Tasks.repeat(TickTime.SECOND.x(2), 1, () -> {
-					Vector velocity = target.getLocation().toVector().subtract(wither.getLocation().toVector()).normalize().multiply(.5);
-					wither.setVelocity(velocity);
-
-					if (wither.isOnGround()) {
-
-						wither.getNearbyEntities(3, 3, 3).forEach(e -> {
-							if (!(e instanceof Player player))
-								return;
-							if (WitherChallenge.currentFight.getAlivePlayers().contains(player.getUniqueId()))
-								return;
-							double distance = player.getLocation().distance(wither.getLocation());
-							double damage = 10 - (distance * 2);
-							player.damage(damage, wither);
-						});
-
-						Tasks.cancel(taskId1.get());
-						Tasks.cancel(taskId2.get());
-						Tasks.cancel(taskId3.get());
-
-						Nexus.getProtocolManager().removePacketListener(listener);
-						ClientboundSetEntityDataPacket packet2 = new ClientboundSetEntityDataPacket(wither.getEntityId(), witherBoss.getEntityData(), true);
-						for (Player player : wither.getTrackedPlayers())
-							((CraftPlayer) player).getHandle().connection.send(packet2);
-
-						wither.setInvulnerable(false);
-					}
-				}));
-			}
-
-			static class SpinPacketListener implements PacketListener {
-
-				@Override
-				public void onPacketSending(PacketEvent event) {
-					if (!(event.getPacket().getHandle() instanceof ClientboundSetEntityDataPacket packet))
-						return;
-					if (packet.getId() != WitherChallenge.currentFight.wither.getEntityId())
-						return;
-					packet.getUnpackedData().forEach(item -> {
-						if (item.getAccessor().getId() == 6) {
-							item.setValue(cast(Pose.SPIN_ATTACK));
-						}
-					});
-				}
-
-				public static <S, T> T cast(S src) {
-					return (T) src;
-				}
-
-				@Override
-				public void onPacketReceiving(PacketEvent event) {
-				}
-
-				@Override
-				public ListeningWhitelist getSendingWhitelist() {
-					return ListeningWhitelist.newBuilder().types(PacketType.fromClass(ClientboundSetEntityDataPacket.class)).build();
-				}
-
-				@Override
-				public ListeningWhitelist getReceivingWhitelist() {
-					return null;
-				}
-
-				@Override
-				public Plugin getPlugin() {
-					return Nexus.getInstance();
-				}
-			}
-
-		};
+//		SPIN_ATTACK {
+//			@Override
+//			public void execute(List<Player> players) {
+//				Wither wither = WitherChallenge.currentFight.wither;
+//				wither.setInvulnerable(true);
+//
+//				Location freezeLoc = wither.getLocation();
+//				Player target = WitherChallenge.currentFight.getRandomAlivePlayer();
+//
+//				net.minecraft.world.entity.boss.wither.WitherBoss witherBoss = ((CraftWither) wither).getHandle();
+//
+//				SpinPacketListener listener = new SpinPacketListener();
+//				Nexus.getProtocolManager().addPacketListener(listener);
+//
+//				ClientboundSetEntityDataPacket packet = new ClientboundSetEntityDataPacket(wither.getEntityId(), witherBoss.getEntityData(), true);
+//				for (Player player : wither.getTrackedPlayers())
+//					((CraftPlayer) player).getHandle().connection.send(packet);
+//
+//				AtomicInteger taskId1 = new AtomicInteger();
+//				taskId1.set(Tasks.repeat(1, 1, () -> wither.lookAt(target)));
+//
+//				AtomicInteger taskId2 = new AtomicInteger();
+//				taskId2.set(Tasks.repeat(1, 1, () -> {
+//					wither.teleport(freezeLoc);
+//					wither.setTarget(null);
+//				}));
+//
+//				AtomicInteger taskId3 = new AtomicInteger();
+//				taskId3.set(Tasks.repeat(TickTime.SECOND.x(2), 1, () -> {
+//					Tasks.cancel(taskId2.get());
+//					Vector velocity = target.getLocation().toVector().subtract(wither.getLocation().toVector()).normalize().multiply(2);
+//					wither.setVelocity(velocity);
+//
+//					if (wither.isOnGround()) {
+//
+//						wither.getNearbyEntities(3, 3, 3).forEach(e -> {
+//							if (!(e instanceof Player player))
+//								return;
+//							if (WitherChallenge.currentFight.getAlivePlayers().contains(player.getUniqueId()))
+//								return;
+//							double distance = player.getLocation().distance(wither.getLocation());
+//							double damage = 10 - (distance * 2);
+//							player.damage(damage, wither);
+//						});
+//
+//						Tasks.cancel(taskId1.get());
+//						Tasks.cancel(taskId3.get());
+//
+//						Nexus.getProtocolManager().removePacketListener(listener);
+//						ClientboundSetEntityDataPacket packet2 = new ClientboundSetEntityDataPacket(wither.getEntityId(), witherBoss.getEntityData(), true);
+//						for (Player player : wither.getTrackedPlayers())
+//							((CraftPlayer) player).getHandle().connection.send(packet2);
+//
+//						wither.setInvulnerable(false);
+//					}
+//				}));
+//			}
+//
+//			static class SpinPacketListener implements PacketListener {
+//
+//				@Override
+//				public void onPacketSending(PacketEvent event) {
+//					if (!(event.getPacket().getHandle() instanceof ClientboundSetEntityDataPacket packet))
+//						return;
+//					if (packet.getId() != WitherChallenge.currentFight.wither.getEntityId())
+//						return;
+//					packet.getUnpackedData().forEach(item -> {
+//						if (item.getAccessor().getId() == 6) {
+//							item.setValue(cast(Pose.SPIN_ATTACK));
+//						}
+//					});
+//				}
+//
+//				public static <S, T> T cast(S src) {
+//					return (T) src;
+//				}
+//
+//				@Override
+//				public void onPacketReceiving(PacketEvent event) {
+//				}
+//
+//				@Override
+//				public ListeningWhitelist getSendingWhitelist() {
+//					return ListeningWhitelist.newBuilder().types(PacketType.fromClass(ClientboundSetEntityDataPacket.class)).build();
+//				}
+//
+//				@Override
+//				public ListeningWhitelist getReceivingWhitelist() {
+//					return null;
+//				}
+//
+//				@Override
+//				public Plugin getPlugin() {
+//					return Nexus.getInstance();
+//				}
+//			}
+//
+//		}
+		;
 
 		public void execute(List<Player> players) {
 			for (Player player : players)
