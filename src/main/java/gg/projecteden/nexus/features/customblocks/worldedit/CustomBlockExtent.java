e4ef06b8c46a6d4dc261e0300adde747d7e6dc32
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2022-07-13 14:14:29 -0500

    basic enum stuff

--- src/main/java/gg/projecteden/nexus/features/customblocks/worldedit/CustomBlockExtent.java
+++ src/main/java/gg/projecteden/nexus/features/customblocks/worldedit/CustomBlockExtent.java
@@ -7,9 +7,14 @@ import com.sk89q.worldedit.extent.Extent;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.block.BaseBlock;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
+import gg.projecteden.nexus.features.customblocks.CustomBlockUtils;
 import gg.projecteden.nexus.features.customblocks.models.CustomBlock;
-import gg.projecteden.nexus.features.customblocks.models.CustomBlock.CustomBlockType;
+import gg.projecteden.nexus.features.particles.effects.DotEffect;
+import gg.projecteden.nexus.utils.ColorType;
+import gg.projecteden.nexus.utils.PlayerUtils.Dev;
 import gg.projecteden.nexus.utils.WorldEditUtils;
+import lombok.AllArgsConstructor;
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -35,7 +40,7 @@ public class CustomBlockExtent extends AbstractDelegateExtent {
 	public <T extends BlockStateHolder<T>> boolean setBlock(int x, int y, int z, T block) throws WorldEditException {
 		debug("setBlock(x, y, z, block)");
 		Location location = new Location(world, x, y, z);
-		if (setBlock(location, block))
+		if (setBlock(location, block, EditType.PLACE))
 			return true;
 
 		return super.setBlock(x, y, z, block);
@@ -46,31 +51,51 @@ public class CustomBlockExtent extends AbstractDelegateExtent {
 		debug("setBlock(position, block)");
 		Location location = worldedit.toLocation(position);
 
-		if (setBlock(location, block))
+		if (setBlock(location, block, EditType.REMOVE))
 			return true;
 
 		return super.setBlock(position, block);
 	}
 
-	private boolean setBlock(Location location, BlockStateHolder<?> block) {
+
+	private boolean setBlock(Location location, BlockStateHolder<?> block, EditType editType) {
 		BaseBlock baseBlock = block.toBaseBlock();
 		Material material = worldedit.toMaterial(baseBlock);
 
 		org.bukkit.block.Block priorBlock = location.getBlock();
 		Material priorMaterial = priorBlock.getType();
+		DotEffect.debug(Dev.WAKKA.getPlayer(), location.toCenterLocation(), editType.getColor());
+
+		debug("Edit Type: " + editType.name());
 
 		// if setting to a handled material
-		if (CustomBlockType.getBlockMaterials().contains(material)) {
-			BlockData blockData = BukkitAdapter.adapt(block);
-			debug("BlockData: " + blockData);
-			CustomBlock _customBlock = CustomBlock.fromBlockData(blockData, priorBlock.getRelative(BlockFace.DOWN));
-			if (_customBlock != null) {
-				debug("Custom Block: " + _customBlock.name());
+//		if (CustomBlockType.getBlockMaterials().contains(material)) {
+		BlockData blockData = BukkitAdapter.adapt(block);
+//			debug("BlockData: " + blockData);
+		debug("Type: " + block.getBlockType());
+		CustomBlock _customBlock = CustomBlock.fromBlockData(blockData, priorBlock.getRelative(BlockFace.DOWN));
+		if (_customBlock != null) {
+			debug("Custom Block Data: " + CustomBlockUtils.getData(location));
 //				return true;
-			}
 		}
+//		}
+
+		debug("");
 
 		return false;
+	}
+
+	@AllArgsConstructor
+	private enum EditType {
+		PLACE(ColorType.BLUE),
+		REMOVE(ColorType.RED),
+		;
+
+		final ColorType colorType;
+
+		public Color getColor() {
+			return colorType.getBukkitColor();
+		}
 	}
 }
 
