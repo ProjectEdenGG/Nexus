7f2ecaad665d8fab12cb214cd26a4a806c5085bd
GriffinCodes <griffin@projecteden.gg>
2022-07-26 21:38:57 -0500

    Cleanup

--- src/main/java/gg/projecteden/nexus/features/crates/GemCommand.java
+++ src/main/java/gg/projecteden/nexus/features/crates/GemCommand.java
@@ -18,7 +18,6 @@ import gg.projecteden.nexus.utils.StringUtils;
 import gg.projecteden.nexus.utils.Utils;
 import lombok.NoArgsConstructor;
 import net.kyori.adventure.text.ComponentLike;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -31,7 +30,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import static gg.projecteden.nexus.utils.Nullables.isNullOrAir;
 
@@ -50,9 +48,10 @@ public class GemCommand extends CustomCommand implements Listener {
 
 	@Path("all")
 	void all() {
-		for (Enchantment enchantment : Arrays.stream(Enchantment.values()).filter(enchantment ->
-				!enchantment.equals(Enchantment.BINDING_CURSE) && !enchantment.equals(Enchantment.VANISHING_CURSE)).collect(Collectors.toList()))
-			PlayerUtils.giveItem(player(), makeGem(enchantment, 1));
+		Arrays.stream(Enchantment.values())
+			.filter(enchantment -> !enchantment.equals(Enchantment.BINDING_CURSE))
+			.filter(enchantment -> !enchantment.equals(Enchantment.VANISHING_CURSE))
+			.forEach(enchant -> PlayerUtils.giveItem(player(), makeGem(enchant, 1)));
 	}
 
 	@EventHandler
@@ -61,23 +60,27 @@ public class GemCommand extends CustomCommand implements Listener {
 			return;
 		if (event.getHand() != EquipmentSlot.HAND)
 			return;
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK)
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK && !isNullOrAir(event.getClickedBlock()))
 			if (MaterialTag.CONTAINERS.isTagged(event.getClickedBlock().getType()))
 				return;
 
 		Player player = event.getPlayer();
 		PlayerInventory inventory = player.getInventory();
+
+		final ItemStack gem;
+		final ItemStack tool;
 		if (isGem(inventory.getItemInMainHand())) {
-			ItemStack gem = inventory.getItemInMainHand();
-			ItemStack tool = inventory.getItemInOffHand();
-			addGemEnchantToTool(player, gem, tool);
-			event.setCancelled(true);
+			gem = inventory.getItemInMainHand();
+			tool = inventory.getItemInOffHand();
 		} else if (isGem(inventory.getItemInOffHand())) {
-			ItemStack gem = inventory.getItemInOffHand();
-			ItemStack tool = inventory.getItemInMainHand();
-			addGemEnchantToTool(player, gem, tool);
-			event.setCancelled(true);
+			gem = inventory.getItemInOffHand();
+			tool = inventory.getItemInMainHand();
+		} else {
+			return;
 		}
+
+		addGemEnchantToTool(player, gem, tool);
+		event.setCancelled(true);
 	}
 
 	public void addGemEnchantToTool(Player player, ItemStack gem, ItemStack tool) {
@@ -87,22 +90,23 @@ public class GemCommand extends CustomCommand implements Listener {
 			PlayerUtils.send(player, "&cYou must hold an item in your other hand to apply the enchantment");
 			return;
 		}
+
 		if (!ItemUtils.getApplicableEnchantments(tool).contains(enchantment)) {
 			PlayerUtils.send(player, "&cThe enchantment on this gem is not applicable to the tool you are holding");
 			return;
 		}
+
 		if (tool.getEnchantments().containsKey(enchantment)) {
 			if (enchantment == Enchant.GLOWING) {
 				level = tool.getEnchantmentLevel(Enchant.GLOWING) + 1;
-			}
-			else if (tool.getEnchantments().get(enchantment) > level) {
+			} else if (tool.getEnchantments().get(enchantment) > level) {
 				PlayerUtils.send(player, "&cThe tool you are holding already has that enchantment at a higher level");
 				return;
-			}
-			else if (tool.getEnchantments().get(enchantment) == level) {
+			} else if (tool.getEnchantments().get(enchantment) == level) {
 				level++;
 			}
 		}
+
 		ComponentLike displayName = gem.getItemMeta().displayName();
 		gem.setAmount(gem.getAmount() - 1);
 		tool.addUnsafeEnchantment(enchantment, level);
@@ -114,9 +118,15 @@ public class GemCommand extends CustomCommand implements Listener {
 	}
 
 	public boolean isGem(ItemStack item) {
-		if (isNullOrAir(item)) return false;
-		if (!item.getType().equals(CustomMaterial.GEM_SAPPHIRE.getMaterial())) return false;
-		if (item.getEnchantments().isEmpty()) return false;
+		if (isNullOrAir(item))
+			return false;
+
+		if (!item.getType().equals(CustomMaterial.GEM_SAPPHIRE.getMaterial()))
+			return false;
+
+		if (item.getEnchantments().isEmpty())
+			return false;
+
 		return ModelId.of(item) == CustomMaterial.GEM_SAPPHIRE.getModelId();
 	}
 
