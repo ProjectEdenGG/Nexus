c0479b8cf82c553b1e27ba095dc95a9bd6017057
GriffinCodes <griffin@projecteden.gg>
2022-08-02 12:35:16 -0500

    Cleanup CustomRecipes menus

---
+++ src/main/java/gg/projecteden/nexus/features/recipes/menu/CustomRecipeFolderMenu.java
@@ -0,0 +1,87 @@
+package gg.projecteden.nexus.features.recipes.menu;
+
+import gg.projecteden.nexus.features.menus.api.ClickableItem;
+import gg.projecteden.nexus.features.menus.api.annotations.Title;
+import gg.projecteden.nexus.features.recipes.menu.common.ICustomRecipeMenu;
+import gg.projecteden.nexus.features.recipes.menu.common.ICustomRecipesMenu;
+import gg.projecteden.nexus.features.recipes.models.NexusRecipe;
+import gg.projecteden.nexus.features.recipes.models.RecipeGroup;
+import gg.projecteden.nexus.features.recipes.models.RecipeType;
+import gg.projecteden.nexus.utils.ItemBuilder;
+import lombok.NoArgsConstructor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.FurnaceRecipe;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@NoArgsConstructor
+@Title("Custom Recipes")
+public class CustomRecipeFolderMenu extends ICustomRecipesMenu {
+	private RecipeType type;
+	private int page;
+
+	public CustomRecipeFolderMenu(RecipeType type, ICustomRecipesMenu previousMenu) {
+		super(previousMenu);
+		this.type = type;
+	}
+
+	@Override
+	public void open(Player player) {
+		super.open(player, page);
+	}
+
+	@Override
+	public void init() {
+		super.init();
+		this.page = contents.pagination().getPage();
+
+		final List<ClickableItem> items = new ArrayList<>();
+
+		if (type == null) {
+			for (RecipeType type : RecipeType.getEnabled()) {
+				if (type.isFolder())
+					items.add(ClickableItem.of(type.getItem(), e -> new CustomRecipeFolderMenu(type, this).open(player)));
+				else
+					items.add(ClickableItem.of(type.getItem(), e -> getMenu(type.getRecipes().get(0)).open(player)));
+			}
+		} else {
+			if (type.isFolder()) {
+				Set<RecipeGroup> uniqueValues = new HashSet<>();
+				for (NexusRecipe nexusRecipe : type.getRecipes()) {
+					if (nexusRecipe.getPermission() != null)
+						if (!player.hasPermission(nexusRecipe.getPermission()))
+							continue;
+
+					if (!nexusRecipe.isShowInMenu())
+						continue;
+
+					RecipeGroup group = nexusRecipe.getGroup();
+					if (group != null) {
+						if (uniqueValues.add(group))
+							items.add(ClickableItem.of(new ItemBuilder(group.getDisplayItem()).name(group.getDisplayName()), e -> getMenu(nexusRecipe).open(player)));
+					} else {
+						items.add(ClickableItem.of(nexusRecipe.getResult(), e -> getMenu(nexusRecipe).open(player)));
+					}
+				}
+			}
+		}
+
+		paginator()
+			.items(items)
+			.previousSlot(0, 2)
+			.nextSlot(0, 6)
+			.perPage(18)
+			.build();
+	}
+
+	public ICustomRecipeMenu getMenu(NexusRecipe recipe) {
+		if (recipe.getRecipe() instanceof FurnaceRecipe)
+			return new CustomFurnaceRecipeMenu(recipe, this);
+		else
+			return new CustomCraftingRecipeMenu(recipe, this);
+	}
+
+}
