c3d38ac447a21425a148315343cafba2975db569
GriffinCodes <griffin@projecteden.gg>
2022-08-04 23:25:31 -0500

    Use worldedit for /tree

--- src/main/java/gg/projecteden/nexus/features/commands/TreeCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/TreeCommand.java
@@ -1,15 +1,21 @@
 package gg.projecteden.nexus.features.commands;
 
+import com.fastasyncworldedit.core.configuration.Caption;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.bukkit.BukkitPlayer;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.math.Vector3;
+import com.sk89q.worldedit.util.TreeGenerator;
 import gg.projecteden.nexus.framework.commands.models.CustomCommand;
 import gg.projecteden.nexus.framework.commands.models.annotations.Arg;
 import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission.Group;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
+import gg.projecteden.nexus.utils.WorldEditUtils;
 import lombok.NonNull;
 import org.bukkit.Location;
-import org.bukkit.TreeType;
-import org.bukkit.block.Block;
 
 @Permission(Group.STAFF)
 public class TreeCommand extends CustomCommand {
@@ -19,17 +25,27 @@ public class TreeCommand extends CustomCommand {
 	}
 
 	@Path("[type]")
-	void run(@Arg("tree") TreeType treeType) {
-		Block target = getTargetBlockRequired();
+	void run(@Arg("tree") TreeGenerator.TreeType treeType) {
+		final WorldEditUtils worldedit = new WorldEditUtils(player());
+		final BukkitPlayer worldeditPlayer = worldedit.getPlayer(player());
+		final LocalSession session = worldeditPlayer.getSession();
 
-		Location location = target.getLocation().add(0, 1, 0);
-		if (location.getBlock().getType().isSolid())
-			error("Could not generate tree on " + camelCase(target.getType()));
+		try (EditSession editSession = session.createEditSession(worldeditPlayer)) {
+			Location location = getTargetBlockRequired().getLocation().add(0, 1, 0);
+			final BlockVector3 pos = Vector3.toBlockPoint(location.getX(), location.getY(), location.getZ());
+			boolean successful = false;
+			for (int i = 0; i < 10; i++) {
+				if (treeType.generate(editSession, pos)) {
+					successful = true;
+					break;
+				}
+			}
 
-		if (world().generateTree(target.getLocation(), treeType))
-			send(PREFIX + "Generated " + camelCase(treeType));
-		else
-			send(PREFIX + "Tree generation failed");
+			if (!successful)
+				worldeditPlayer.print(Caption.of("worldedit.tool.tree.obstructed"));
+
+			session.remember(editSession);
+		}
 	}
 
 }
