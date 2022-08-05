f7a355d672057829813aa7679d21ba6a2ea1aa63
GriffinCodes <griffin@projecteden.gg>
2022-08-05 00:52:00 -0500

    Fix custom maps not loading

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("dev.dbassett:skullcreator:3.0.1")
     implementation("com.vdurmont:emoji-java:5.1.1")
     implementation("com.github.mpkorstanje:simmetrics-core:4.1.1")
+    compileOnly("fr.moribus:ImageOnMap:4.2.2-EDEN")
     compileOnly("net.citizensnpcs:citizens-main:2.0.28-SNAPSHOT")
     compileOnly("me.lucko:helper:5.6.10")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
