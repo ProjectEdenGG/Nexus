bf5a1f2b2b952db11de0e779d630975457c9bf73
GriffinCodes <griffin@projecteden.gg>
2022-08-08 17:09:17 -0500

    Fix NPE

--- src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/DecorationListener.java
+++ src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/DecorationListener.java
@@ -194,7 +194,7 @@ public class DecorationListener implements Listener {
 			.tool(tool)
 			.build();
 
-		if (data.getDecoration().getRotation() == null)
+		if (data.getDecoration() == null || data.getDecoration().getRotation() == null)
 			return;
 
 		boolean cancel = false;
