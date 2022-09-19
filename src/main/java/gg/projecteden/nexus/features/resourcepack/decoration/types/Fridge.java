119a57bf78b92e7dcd288e2ea658f40b34a3a4f0
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2022-09-19 02:59:36 -0500

    Decor: add the hitboxes

---
+++ src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/types/Fridge.java
@@ -0,0 +1,38 @@
+package gg.projecteden.nexus.features.resourcepack.decoration.types;
+
+import gg.projecteden.nexus.features.resourcepack.decoration.common.Hitbox;
+import gg.projecteden.nexus.features.resourcepack.decoration.types.surfaces.FloorThing;
+import gg.projecteden.nexus.features.resourcepack.models.CustomMaterial;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+
+import java.util.List;
+
+public class Fridge extends FloorThing {
+	FridgeSize size;
+
+	public Fridge(String name, CustomMaterial material, FridgeSize size) {
+		super(name, material);
+		this.size = size;
+		this.hitboxes = size.getHitboxes();
+	}
+
+	@AllArgsConstructor
+	public enum FridgeSize {
+		MINI(
+			Hitbox.single(Material.BARRIER)),
+		STANDARD(List.of(
+			Hitbox.origin(Material.BARRIER),
+			Hitbox.offset(Material.BARRIER, BlockFace.UP, 1))),
+		TALL(List.of(
+			Hitbox.origin(Material.BARRIER),
+			Hitbox.offset(Material.BARRIER, BlockFace.UP, 1),
+			Hitbox.offset(Material.BARRIER, BlockFace.UP, 2))),
+		;
+
+		@Getter
+		final List<Hitbox> hitboxes;
+	}
+}
