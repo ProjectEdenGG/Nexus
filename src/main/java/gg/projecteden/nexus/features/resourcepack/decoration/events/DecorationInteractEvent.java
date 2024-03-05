b6e54652eab3bb15b91d34272ee1ae340dae7dd3
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2024-03-05 13:20:37 -0600

    Decor: adjust curtain sound location

--- src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/events/DecorationInteractEvent.java
+++ src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/events/DecorationInteractEvent.java
@@ -2,15 +2,19 @@ package gg.projecteden.nexus.features.resourcepack.decoration.events;
 
 import gg.projecteden.nexus.features.resourcepack.decoration.common.Decoration;
 import lombok.Getter;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class DecorationInteractEvent extends DecorationEvent {
 	@Getter
 	private final InteractType interactType;
+	@Getter
+	private final Block clickedBlock;
 
-	public DecorationInteractEvent(Player player, Decoration decoration, InteractType type) {
+	public DecorationInteractEvent(Player player, Decoration decoration, Block block, InteractType type) {
 		super(player, decoration);
 		this.interactType = type;
+		this.clickedBlock = block;
 	}
 
 	public enum InteractType {
