d48685cde97a0b8316359c06206761fc76b35f26
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2024-06-24 18:02:00 -0500

    Decor: fix catalogs not spawning items

--- src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/catalog/Catalog.java
+++ src/main/java/gg/projecteden/nexus/features/resourcepack/decoration/catalog/Catalog.java
@@ -227,9 +227,23 @@ public class Catalog implements Listener {
 
 		itemStack = config.getItem();
 
-		if (tryBuyItem(viewer, itemStack, transactionCause, config, shopGroup))
+		if (DecorationUtils.hasBypass(viewer)) {
+			DecorationUtils.getSoundBuilder(Sound.ENTITY_ITEM_PICKUP).category(SoundCategory.PLAYERS).volume(0.3).receiver(viewer).play();
+			PlayerUtils.giveItem(viewer, itemStack);
+			return;
+		}
+
+		Double price = config.getCatalogPrice();
+		if (price == null)
 			return;
 
+		BankerService bankerService = new BankerService();
+		if (!bankerService.has(viewer, price, shopGroup)) {
+			DecorationError.LACKING_FUNDS.send(viewer);
+			return;
+		}
+
+		bankerService.withdraw(transactionCause.of(null, viewer, BigDecimal.valueOf(-price), shopGroup, config.getName()));
 		PlayerUtils.mailItem(viewer, itemStack, null, worldGroup, eventName);
 	}
 
@@ -240,37 +254,31 @@ public class Catalog implements Listener {
 
 		itemStack = config.getItem();
 
+		if (DecorationUtils.hasBypass(viewer)) {
+			DecorationUtils.getSoundBuilder(Sound.ENTITY_ITEM_PICKUP).category(SoundCategory.PLAYERS).volume(0.3).receiver(viewer).play();
+			PlayerUtils.giveItem(viewer, itemStack);
+			return;
+		}
+
+		Double price = config.getCatalogPrice();
+		if (price == null)
+			return;
+
 		if (!WorldGroup.of(viewer).equals(WorldGroup.SURVIVAL))
 			return;
 
-		if (tryBuyItem(viewer, itemStack, transactionCause, config, ShopGroup.SURVIVAL))
+		ShopGroup shopGroup = ShopGroup.SURVIVAL;
+		BankerService bankerService = new BankerService();
+		if (!bankerService.has(viewer, price, shopGroup)) {
+			DecorationError.LACKING_FUNDS.send(viewer);
 			return;
+		}
+
+		bankerService.withdraw(transactionCause.of(null, viewer, BigDecimal.valueOf(-price), shopGroup, config.getName()));
 
 		if (PlayerUtils.hasRoomFor(viewer, itemStack))
 			DecorationUtils.getSoundBuilder(Sound.ENTITY_ITEM_PICKUP).category(SoundCategory.PLAYERS).volume(0.3).receiver(viewer).play();
 
 		PlayerUtils.giveItemAndMailExcess(viewer, itemStack, WorldGroup.SURVIVAL);
 	}
-
-	private static boolean tryBuyItem(Player viewer, ItemStack itemStack, TransactionCause transactionCause, DecorationConfig config, ShopGroup shopGroup) {
-		if (DecorationUtils.hasBypass(viewer)) {
-			DecorationUtils.getSoundBuilder(Sound.ENTITY_ITEM_PICKUP).category(SoundCategory.PLAYERS).volume(0.3).receiver(viewer).play();
-			PlayerUtils.giveItem(viewer, itemStack);
-			return true;
-		}
-
-		Double price = config.getCatalogPrice();
-		if (price == null)
-			return true;
-
-		BankerService bankerService = new BankerService();
-		if (!bankerService.has(viewer, price, shopGroup)) {
-			DecorationError.LACKING_FUNDS.send(viewer);
-			return true;
-		}
-
-		bankerService.withdraw(transactionCause.of(null, viewer, BigDecimal.valueOf(-price), shopGroup, config.getName()));
-		return false;
-	}
-
 }
