155b794c144b6fa895fbaa0f634832cc37c8bb67
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2024-07-02 17:51:01 -0500

    Pugmas24: add block animations

--- src/main/java/gg/projecteden/nexus/features/events/y2024/pugmas24/models/Geyser.java
+++ src/main/java/gg/projecteden/nexus/features/events/y2024/pugmas24/models/Geyser.java
@@ -1,8 +1,6 @@
 package gg.projecteden.nexus.features.events.y2024.pugmas24.models;
 
 import com.destroystokyo.paper.ParticleBuilder;
-import com.sk89q.worldedit.regions.Polygonal2DRegion;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import gg.projecteden.api.common.utils.RandomUtils;
 import gg.projecteden.api.common.utils.TimeUtils.TickTime;
 import gg.projecteden.nexus.Nexus;
@@ -23,10 +21,10 @@ import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.Levelled;
 import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -36,6 +34,11 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
+/*
+	TODO:
+		- GEYSER ERUPTING GIVES BETTER CHANCES TO FISHING?
+		- FINAL LOCATIONS
+ */
 public class Geyser implements Listener {
 
 	@Getter
@@ -44,13 +47,20 @@ public class Geyser implements Listener {
 	private static boolean hurtPlayers = false;
 	private static GeyserStatus status = GeyserStatus.INACTIVE;
 	private static List<Location> base;
+	private static final WorldEditUtils worldedit = Pugmas24.get().worldedit();
+	private static final WorldGuardUtils worldguard = Pugmas24.get().worldguard();
 	//
 	public static final Location geyserOrigin = Pugmas24.get().location(-501, 93, -3046); // TODO: FINAL LOC, NORTH WEST CORNER
+	public static final Location geyserPaste = Pugmas24.get().location(-505, 93, -3049); // TODO: FINAL LOC, NORTH WEST CORNER
 	private static final int maxHeight = 8;
-	private static final Levelled splashData = (Levelled) Material.WATER.createBlockData();
+	private static final int frameSpeed = 2;
 	private static final String geyserPoolsRegion = Pugmas24.get().getRegionName() + "_geyser";
 	private static final String geyserInsideRegion = Pugmas24.get().getRegionName() + "_geyser_inside";
 	private static final String geyserColumnRegion = Pugmas24.get().getRegionName() + "_geyser_column";
+	private static final String schemReset = "pugmas24/geyser/empty";
+	private static final String schemRunning = "pugmas24/geyser/running_";
+	private static final String schemIntro = "pugmas24/geyser/intro_";
+	private static final String schemOutro = "pugmas24/geyser/outro_";
 	//
 	private static final SoundBuilder geyserSound = new SoundBuilder(CustomSound.AMBIENT_GEYSER).volume(2).category(SoundCategory.AMBIENT).location(geyserOrigin.clone());
 	private static final SoundBuilder rumbleSound = new SoundBuilder(CustomSound.AMBIENT_GROUND_RUMBLE).volume(4).pitch(2).category(SoundCategory.AMBIENT).location(geyserOrigin.clone().subtract(0, 2, 0));
@@ -63,7 +73,7 @@ public class Geyser implements Listener {
 		hurtPlayers = false;
 		smokeFailChance = 99;
 		animating = false;
-		// TODO: delete any leftover blocks
+		worldedit.paster().file(schemReset).at(geyserPaste).pasteAsync();
 	}
 
 	public static void animate() {
@@ -79,7 +89,6 @@ public class Geyser implements Listener {
 		erupting = false;
 		hurtPlayers = false;
 		status = GeyserStatus.START_INTRO;
-		splashData.setLevel(7);
 		players();
 
 		Tasks.async(Geyser::incrementGeyser);
@@ -114,15 +123,19 @@ public class Geyser implements Listener {
 				players.addAll(Pugmas24.get().getPlayersIn(geyserColumnRegion));
 
 				Location geyserPushLoc = geyserOrigin.clone();
+				geyserPushLoc.add(0.5, 0, 0.5);
 				for (Player player : players) {
 					if (!isWater(player.getLocation()))
 						continue;
 
 					var fromLoc = geyserPushLoc.clone();
 					if (player.getLocation().getBlockY() <= geyserPushLoc.getBlockY())
-						fromLoc.setY(player.getLocation().getY() - 1);
+						fromLoc.setY(player.getLocation().getY() - 2);
 
-					player.setVelocity(EntityUtils.getForcefieldVelocity(player, fromLoc, 1.5));
+					Vector launchVector = EntityUtils.getForcefieldVelocity(player, fromLoc, 2.5);
+					Vector randomDir = new Vector(RandomUtils.randomDouble(-0.5, 0.5), RandomUtils.randomDouble(0.2, 0.6), RandomUtils.randomDouble(-0.5, 0.5));
+					launchVector.add(randomDir);
+					player.setVelocity(launchVector);
 				}
 			}
 		});
@@ -166,9 +179,16 @@ public class Geyser implements Listener {
 			wait += TickTime.SECOND.x(3);
 		}
 
-		int speedTicks = 2;
 		Tasks.wait(wait - TickTime.SECOND.x(1), () -> erupting = true);
-		wait = geyserRaise(wait, speedTicks, maxHeight, false);
+		wait = geyserRaise(wait, frameSpeed, maxHeight, false);
+
+		for (int frame = 1; frame <= 5; frame++) {
+			int finalFrame = frame;
+			Tasks.waitAsync(wait, () ->
+					worldedit.paster().file(schemIntro + finalFrame).at(geyserPaste).pasteAsync());
+
+			wait += frameSpeed;
+		}
 
 		Tasks.wait(wait, () -> {
 			status = GeyserStatus.START_RUNNING;
@@ -180,9 +200,18 @@ public class Geyser implements Listener {
 		status = GeyserStatus.ANIMATING;
 		erupting = true;
 
-		long wait = TickTime.SECOND.x(10);
+		long wait = 0;
 
-		// TODO: ANIMATION
+		int loops = RandomUtils.randomInt(10, 30);
+		for (int i = 0; i < loops; i++) {
+			for (int frame = 1; frame <= 7; frame++) {
+				int finalFrame = frame;
+				Tasks.waitAsync(wait, () ->
+						worldedit.paster().file(schemRunning + finalFrame).at(geyserPaste).pasteAsync());
+
+				wait += frameSpeed;
+			}
+		}
 
 		Tasks.wait(wait, () -> {
 			status = GeyserStatus.START_OUTRO;
@@ -193,10 +222,18 @@ public class Geyser implements Listener {
 	private static void startOutro() {
 		status = GeyserStatus.ANIMATING;
 		erupting = true;
-		int speedTicks = 5;
 
 		long wait = 0;
-		wait = geyserLower(wait, speedTicks, maxHeight);
+		for (int frame = 1; frame <= 13; frame++) {
+			int finalFrame = frame;
+			Tasks.waitAsync(wait, () ->
+					worldedit.paster().file(schemOutro + finalFrame).at(geyserPaste).pasteAsync());
+
+			wait += frameSpeed;
+		}
+
+		Tasks.waitAsync(wait, () ->
+				worldedit.paster().file(schemReset).at(geyserPaste).pasteAsync());
 
 		Tasks.wait(wait - TickTime.SECOND.x(2), () -> erupting = false);
 		Tasks.wait(wait, () -> {
@@ -206,31 +243,36 @@ public class Geyser implements Listener {
 	}
 
 	private static void incrementGeyser() {
-		if (status == GeyserStatus.ANIMATING)
-			return;
+		switch (status) {
+			case ANIMATING, INACTIVE -> {
+				return;
+			}
 
-		if (status == GeyserStatus.START_INTRO) {
-			startIntro();
-			return;
-		}
+			case START_INTRO -> {
+				startIntro();
+				return;
+			}
 
-		if (status == GeyserStatus.START_RUNNING) {
-			startRunning();
-			return;
-		}
+			case START_RUNNING -> {
+				startRunning();
+				return;
+			}
 
-		if (status == GeyserStatus.START_OUTRO) {
-			startOutro();
-			return;
-		}
+			case START_OUTRO -> {
+				startOutro();
+				return;
+			}
 
-		if (status == GeyserStatus.ENDING) {
-			reset();
-			return;
+			case ENDING -> {
+				reset();
+				return;
+			}
 		}
 
 		Tasks.async(Geyser::incrementGeyser);
 	}
+
+	//
 
 	private enum GeyserStatus {
 		START_INTRO,
@@ -242,14 +284,12 @@ public class Geyser implements Listener {
 		;
 	}
 
-	//
-
 	private static Set<Location> waterLocations = new HashSet<>();
 	private static int smokeFailChance = 99;
 
 	public static void animateSmoke() {
 		if (Nullables.isNullOrEmpty(waterLocations)) {
-			waterLocations = getWaterLocations();
+			waterLocations = getSmokeLocations();
 		}
 
 		for (Location location : waterLocations) {
@@ -319,15 +359,8 @@ public class Geyser implements Listener {
 		return Geyser.geyserOrigin.getBlock().getRelative(blockFace).getLocation();
 	}
 
-	private static Set<Location> getWaterLocations() {
-		WorldEditUtils worldedit = Pugmas24.get().worldedit();
-		WorldGuardUtils worldguard = Pugmas24.get().worldguard();
-
-		ProtectedRegion protectedRegion = worldguard.getProtectedRegion(geyserPoolsRegion);
-		Polygonal2DRegion polyRegion = (Polygonal2DRegion) worldguard.convert(protectedRegion);
-
-		return worldedit.getBlocks(polyRegion, block -> polyRegion.contains(worldedit.toBlockVector3(block.getLocation()))).stream()
-				.filter(block -> !Nullables.isNullOrAir(block))
+	private static Set<Location> getSmokeLocations() {
+		return worldedit.getBlocksPoly(worldguard.getRegion(geyserPoolsRegion)).stream()
 				.filter(block -> block.getType() == Material.WATER)
 				.filter(block -> block.getRelative(BlockFace.UP).getType() == Material.AIR)
 				.map(Block::getLocation)
