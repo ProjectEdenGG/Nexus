6355800d6c4d1ea74832f06fe5d50d11ab3a1db1
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2024-07-28 13:55:28 -0500

    Pugmas: add fishing rods

---
+++ src/main/java/gg/projecteden/nexus/features/events/y2024/pugmas24/models/Fishing.java
@@ -0,0 +1,18 @@
+package gg.projecteden.nexus.features.events.y2024.pugmas24.models;
+
+import gg.projecteden.nexus.features.resourcepack.models.CustomMaterial;
+import gg.projecteden.nexus.utils.ItemBuilder;
+
+public class Fishing {
+
+	private static final ItemBuilder ROD_WOOD = new ItemBuilder(CustomMaterial.FISHING_ROD_WOOD)
+			.name("Wood Fishing Rod");
+
+	private static final ItemBuilder ROD_REINFORCED = new ItemBuilder(CustomMaterial.FISHING_ROD_REINFORCED)
+			.name("Reinforced Fishing Rod");
+
+	private static final ItemBuilder ROD_GOLDEN = new ItemBuilder(CustomMaterial.FISHING_ROD_GOLDEN)
+			.name("Golden Fishing Rod");
+
+
+}
