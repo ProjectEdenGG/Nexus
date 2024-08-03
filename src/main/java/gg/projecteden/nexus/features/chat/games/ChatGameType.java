31f9ede016629ede36e34f41072defadd4f196b5
GriffinCodes <griffin@projecteden.gg>
2024-08-02 20:22:11 -0500

    Add countries and biomes to chat games

--- src/main/java/gg/projecteden/nexus/features/chat/games/ChatGameType.java
+++ src/main/java/gg/projecteden/nexus/features/chat/games/ChatGameType.java
@@ -9,6 +9,7 @@ import gg.projecteden.nexus.utils.StringUtils;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.Material;
+import org.bukkit.block.Biome;
 import org.bukkit.entity.EntityType;
 import org.simmetrics.metrics.StringMetrics;
 
@@ -101,6 +102,34 @@ public enum ChatGameType {
 	private static final Set<String> WORDS = new HashSet<>() {{
 		addAll(enumWordFormatter.apply(Arrays.stream(Material.values()).filter(Material::isItem)));
 		addAll(enumWordFormatter.apply(Arrays.stream(EntityType.values()).filter(type -> type.isAlive() && type != EntityType.PLAYER)));
+		addAll(enumWordFormatter.apply(Arrays.stream(Biome.values())));
+		// https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population - removed countries without number
+		addAll(List.of(
+			"China", "India", "United States", "Indonesia", "Pakistan", "Nigeria", "Brazil", "Bangladesh", "Russia",
+			"Mexico", "Japan", "Philippines", "Ethiopia", "Egypt", "Vietnam", "Democratic Republic of the Congo",
+			"Turkey", "Iran", "Germany", "France", "United Kingdom", "Thailand", "Tanzania", "South Africa", "Italy",
+			"Myanmar", "Colombia", "Kenya", "South Korea", "Spain", "Argentina", "Algeria", "Uganda", "Iraq", "Sudan",
+			"Canada", "Poland", "Morocco", "Uzbekistan", "Ukraine", "Angola", "Afghanistan", "Peru", "Malaysia",
+			"Mozambique", "Saudi Arabia", "Yemen", "Ghana", "Madagascar", "Ivory Coast", "Nepal", "Cameroon",
+			"Venezuela", "Australia", "Niger", "North Korea", "Burkina Faso", "Syria", "Mali", "Malawi", "Sri Lanka",
+			"Kazakhstan", "Chile", "Zambia", "Romania", "Chad", "Somalia", "Senegal", "Netherlands", "Guatemala",
+			"Cambodia", "Ecuador", "Zimbabwe", "South Sudan", "Guinea", "Rwanda", "Benin", "Burundi", "Bolivia",
+			"Tunisia", "Belgium", "Papua New Guinea", "Haiti", "Jordan", "Cuba", "Czech Republic", "Dominican Republic",
+			"Portugal", "Sweden", "Greece", "United Arab Emirates", "Tajikistan", "Azerbaijan", "Israel", "Honduras",
+			"Hungary", "Austria", "Belarus", "Switzerland", "Sierra Leone", "Togo", "Laos", "Libya", "Kyrgyzstan",
+			"Turkmenistan", "El Salvador", "Nicaragua", "Serbia", "Bulgaria", "Republic of the Congo", "Paraguay",
+			"Denmark", "Singapore", "Central African Republic", "Finland", "Norway", "Lebanon", "Palestine", "Slovakia",
+			"New Zealand", "Costa Rica", "Ireland", "Liberia", "Oman", "Kuwait", "Mauritania", "Panama", "Croatia",
+			"Eritrea", "Georgia", "Mongolia", "Uruguay", "Bosnia and Herzegovina", "Armenia", "Namibia", "Lithuania",
+			"Qatar", "Jamaica", "Albania", "Moldova", "Gambia", "Botswana", "Gabon", "Lesotho", "Slovenia", "Latvia",
+			"North Macedonia", "Guinea-Bissau", "Bahrain", "Equatorial Guinea", "Estonia", "East Timor",
+			"Trinidad and Tobago", "Mauritius", "Eswatini", "Djibouti", "Cyprus", "Fiji", "Bhutan", "Guyana",
+			"Comoros", "Solomon Islands", "Luxembourg", "Montenegro", "Suriname", "Malta", "Maldives", "Cape Verde",
+			"Brunei", "Belize", "Bahamas", "Iceland", "Vanuatu", "Barbados", "Sao Tome and Principe", "Samoa",
+			"Saint Lucia", "Kiribati", "Seychelles", "Grenada", "Saint Vincent and the Grenadines", "Micronesia",
+			"Antigua and Barbuda", "Tonga", "Andorra", "Dominica", "Saint Kitts and Nevis", "Marshall Islands",
+			"Liechtenstein", "Monaco", "San Marino", "Palau", "Nauru", "Tuvalu", "Vatican City"
+		));
 	}};
 
 }
