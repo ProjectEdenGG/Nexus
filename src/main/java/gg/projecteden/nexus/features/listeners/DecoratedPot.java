945e22dba904b654f885acee0cb916c2d1516d25
WakkaFlocka239 <WakkaFlocka239@gmail.com>
2024-08-04 19:07:07 -0500

    refactor

--- src/main/java/gg/projecteden/nexus/features/listeners/DecoratedPot.java
+++ src/main/java/gg/projecteden/nexus/features/listeners/DecoratedPot.java
@@ -1,28 +1,94 @@
 package gg.projecteden.nexus.features.listeners;
 
 import com.destroystokyo.paper.ParticleBuilder;
+import gg.projecteden.nexus.models.nerd.Rank;
 import gg.projecteden.nexus.utils.ItemUtils;
+import gg.projecteden.nexus.utils.LocationUtils.CardinalDirection;
+import gg.projecteden.nexus.utils.MaterialTag;
 import gg.projecteden.nexus.utils.Nullables;
 import gg.projecteden.nexus.utils.SoundBuilder;
 import gg.projecteden.nexus.utils.Tasks;
 import org.bukkit.Color;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.block.DecoratedPot.Side;
+import org.bukkit.block.data.Directional;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-/*
-	Allows players to "fill" decorated pots with water, and "empty" them as well
-	When a pot breaks, if the pot contains a water bucket, the water will be placed at the location of the pot
- */
+import static gg.projecteden.nexus.utils.Nullables.isNullOrAir;
+
 public class DecoratedPot implements Listener {
 
+	/*
+		Allows players to change decorated pot designs by crouch clicking the pot with the sherd
+	 */
+	@EventHandler
+	public void onPotInteractWithSherd(PlayerInteractEvent event) {
+		var block = event.getClickedBlock();
+		var sherd = event.getItem();
+		var player = event.getPlayer();
+
+		if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
+			return;
+
+		if (isNullOrAir(block))
+			return;
+
+		if (!(block.getBlockData() instanceof Directional directional))
+			return;
+
+		if (!(block.getState() instanceof org.bukkit.block.DecoratedPot pot))
+			return;
+
+		if (!MaterialTag.POTTERY_SHERDS.isTagged(sherd))
+			return;
+
+		if (!player.isSneaking())
+			return;
+
+		if (!Rank.of(player).isStaff())
+			return;
+
+		if (player.getGameMode() != GameMode.CREATIVE)
+			return;
+
+		var blockFace = event.getBlockFace();
+		var facing = directional.getFacing().getOppositeFace();
+
+		Side side = null;
+		if (facing == blockFace)
+			side = Side.FRONT;
+
+		if (CardinalDirection.of(facing).turnLeft().toBlockFace() == blockFace)
+			side = Side.RIGHT;
+
+		if (CardinalDirection.of(facing).turnRight().toBlockFace() == blockFace)
+			side = Side.LEFT;
+
+		if (facing.getOppositeFace() == blockFace)
+			side = Side.BACK;
+
+		if (side == null)
+			return;
+
+		event.setCancelled(true);
+		pot.setSherd(side, sherd.getType());
+		pot.update();
+	}
+
+	/*
+		Allows players to "fill" decorated pots with water, and "empty" them as well
+		When a pot breaks, if the pot contains a water bucket, the water will be placed at the location of the pot
+ 	*/
 	@EventHandler
 	public void on(PlayerInteractEvent event) {
 		Block block = event.getClickedBlock();
