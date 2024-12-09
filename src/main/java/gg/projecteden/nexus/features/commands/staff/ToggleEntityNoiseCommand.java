5af8acb000c87faca79d13539a74a23ae3b1380d
Blast-MC <cjblanton2@gmail.com>
2024-12-09 15:05:58 -0500

    allow non living entities in ToggleEntityNoiseCommand

--- src/main/java/gg/projecteden/nexus/features/commands/staff/ToggleEntityNoiseCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/staff/ToggleEntityNoiseCommand.java
@@ -6,7 +6,9 @@ import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission.Group;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 
 @Permission(Group.STAFF)
 public class ToggleEntityNoiseCommand extends CustomCommand {
@@ -18,7 +20,9 @@ public class ToggleEntityNoiseCommand extends CustomCommand {
 	@Path
 	@Description("Make an entity silent")
 	void run() {
-		LivingEntity entity = getTargetLivingEntityRequired();
+		Entity entity = getTargetEntityRequired();
+		if (entity instanceof Player)
+			error("You cannot make a player silent");
 		boolean isSilent = entity.isSilent();
 		entity.setSilent(!isSilent);
 		send(PREFIX + entity.getClass().getSimpleName() + " is now " + (isSilent ? "unmuted" : "muted"));
