f110f83600f163d26b298051ec2953d2a6cbb85b
GriffinCodes <griffin@projecteden.gg>
2025-01-18 18:52:44 -0600

    Add support for endpoint variables

--- src/main/java/gg/projecteden/nexus/features/api/SimpleHttpServer.java
+++ src/main/java/gg/projecteden/nexus/features/api/SimpleHttpServer.java
@@ -18,6 +18,8 @@ import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 @Environments(Env.PROD)
 public class SimpleHttpServer extends Feature {
@@ -60,12 +62,13 @@ public class SimpleHttpServer extends Feature {
 		public void handle(HttpExchange exchange) {
 			try {
 				String path = exchange.getRequestURI().getPath();
-				Nexus.log("[API] " + exchange.getRequestMethod() + " " + path);
+				Nexus.debug("[API] " + exchange.getRequestMethod() + " " + path);
 
 				Object response = null;
 
 				var httpMethod = HttpMethod.valueOf(exchange.getRequestMethod());
 
+				endpoints:
 				for (Method method : CONTROLLER.getClass().getDeclaredMethods()) {
 					method.setAccessible(true);
 					if (!method.isAnnotationPresent(httpMethod.getAnnotation()))
@@ -73,14 +76,44 @@ public class SimpleHttpServer extends Feature {
 
 					Annotation annotation = method.getAnnotation(httpMethod.getAnnotation());
 					String controllerPath = (String) annotation.getClass().getMethod("value").invoke(annotation);
-					if (!path.equals(controllerPath))
-						continue;
 
-					response = method.invoke(CONTROLLER);
+					var requestSplit = path.split("/");
+					var controllerSplit = controllerPath.split("/");
+					List<String> arguments = new ArrayList<>();
+
+					Nexus.debug("[API] method: " + method.getName());
+					Nexus.debug("[API] path: " + path);
+					Nexus.debug("[API] controllerPath: " + controllerPath);
+					Nexus.debug("[API] requestSplit: " + String.join(", ", requestSplit));
+					Nexus.debug("[API] controllerSplit: " + String.join(", ", controllerSplit));
+
+					if (requestSplit.length != controllerSplit.length) {
+						Nexus.debug("[API] Argument length mismatch, continuing");
+						continue;
+					}
+
+					for (int i = 0; i < controllerSplit.length; i++) {
+						var requestStep = requestSplit[i];
+						var controllerStep = controllerSplit[i];
+						Nexus.debug("[API] requestStep: " + requestStep);
+						Nexus.debug("[API] controllerStep: " + controllerStep);
+
+						if (controllerStep.startsWith("{") && controllerStep.endsWith("}")) {
+							arguments.add(requestStep);
+						} else if (!requestStep.equals(controllerStep)) {
+							Nexus.debug("[API] Step mismatch, continuing");
+							continue endpoints;
+						}
+					}
+
+					Nexus.debug("[API] Method: " + method.getName());
+					Nexus.debug("[API] Arguments: " + arguments);
+					response = method.invoke(CONTROLLER, arguments.toArray());
 				}
 
 				if (response == null) {
 					exchange.sendResponseHeaders(404, 0);
+					exchange.close();
 					return;
 				}
 
