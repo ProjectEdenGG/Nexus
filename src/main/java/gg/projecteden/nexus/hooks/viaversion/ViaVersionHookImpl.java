250532c0727de12eb7c8c4a7f74c33791032e259
GriffinCodes <griffin@projecteden.gg>
2025-01-23 20:45:18 -0600

    Simplify

--- src/main/java/gg/projecteden/nexus/hooks/viaversion/ViaVersionHookImpl.java
+++ src/main/java/gg/projecteden/nexus/hooks/viaversion/ViaVersionHookImpl.java
@@ -4,23 +4,17 @@ import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import org.bukkit.entity.Player;
 
-import static gg.projecteden.api.common.utils.Nullables.isNullOrEmpty;
-
 public class ViaVersionHookImpl extends ViaVersionHook {
 
 	@Override
 	public String getPlayerVersion(Player player) {
 		try {
-			var version = Via.getManager().getPlatform().getApi().getPlayerVersion(player.getUniqueId());
-			var versions = ProtocolVersion.getProtocols().stream()
-				.filter(protocolVersion -> protocolVersion.getVersion() == version)
-				.map(ProtocolVersion::getName)
-				.toList();
+			ProtocolVersion version = Via.getManager().getPlatform().getApi().getPlayerProtocolVersion(player.getUniqueId());
 
-			if (isNullOrEmpty(versions))
+			if (!version.isKnown())
 				return "Unknown (" + version + ")";
 
-			return String.join(", ", versions);
+			return version.getName();
 		} catch (IllegalArgumentException ex) {
 			ex.printStackTrace();
 			return "Unknown (ViaVersion error)";
