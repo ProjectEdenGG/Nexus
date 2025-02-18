235a08365efa52dc185cae42907b92db0158604a
Blast-MC <cjblanton2@gmail.com>
2025-02-18 12:30:06 -0500

    incorrect listener import

--- src/main/java/gg/projecteden/nexus/features/commands/staff/MultiCommandCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/staff/MultiCommandCommand.java
@@ -18,9 +18,9 @@ import lombok.NonNull;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.net.http.WebSocket.Listener;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
