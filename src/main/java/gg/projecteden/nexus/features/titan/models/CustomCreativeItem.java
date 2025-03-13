545020bf5960cdb563b61ac32ed7abe9aea4ee07
Blast-MC <cjblanton2@gmail.com>
2025-03-12 23:36:11 -0400

    cleanup creative item api

--- src/main/java/gg/projecteden/nexus/features/titan/models/CustomCreativeItem.java
+++ src/main/java/gg/projecteden/nexus/features/titan/models/CustomCreativeItem.java
@@ -1,9 +1,7 @@
 package gg.projecteden.nexus.features.titan.models;
 
 import gg.projecteden.nexus.features.resourcepack.customblocks.models.CustomBlock;
-import gg.projecteden.nexus.features.resourcepack.customblocks.models.common.ICustomBlock;
 import gg.projecteden.nexus.features.resourcepack.decoration.DecorationType;
-import gg.projecteden.nexus.features.resourcepack.decoration.types.Dyeable;
 import gg.projecteden.nexus.utils.ItemBuilder;
 import gg.projecteden.nexus.utils.SerializationUtils.NBT;
 import gg.projecteden.nexus.utils.StringUtils;
@@ -14,35 +12,20 @@ import lombok.Data;
 @AllArgsConstructor
 public class CustomCreativeItem {
 
-	String material;
-	String displayName;
-	String model;
-	String hex;
 	String category;
 	String item;
 
 	public CustomCreativeItem(CustomBlock customBlock) {
-		material = ICustomBlock.itemMaterial.name().toLowerCase();
-		displayName = customBlock.get().getItemName();
-		model = customBlock.get().getModel();
 		item = NBT.serializeItemStack(customBlock.get().getItemStack());
 		category = "Custom Blocks";
 	}
 
 	public CustomCreativeItem(DecorationType decorationType) {
-		material = decorationType.getConfig().getMaterial().name().toLowerCase();
-		displayName = decorationType.getConfig().getName();
-		model = decorationType.getConfig().getModel();
 		category = "Decorations: " + StringUtils.camelCase(decorationType.getTypeConfig().theme());
-		if (decorationType.getConfig() instanceof Dyeable dyeable)
-			hex = dyeable.getColor().asHexString();
 		item = NBT.serializeItemStack(decorationType.getConfig().getItem());
 	}
 
 	public CustomCreativeItem(ItemBuilder item, String category) {
-		material = item.material().name().toLowerCase();
-		displayName = item.name();
-		model = item.model();
 		this.category = category;
 		this.item = NBT.serializeItemStack(item.build());
 	}
