ca9ce9a18ceaa2580eae0f3ac30c9552a7d0ad48
Wakka <wakkaflocka239@gmail.com>
2025-03-19 13:19:20 -0500

    CustomBlocks: add crafting recipe to neon block

--- src/main/java/gg/projecteden/nexus/features/resourcepack/customblocks/models/noteblocks/neon/INeonBlock.java
+++ src/main/java/gg/projecteden/nexus/features/resourcepack/customblocks/models/noteblocks/neon/INeonBlock.java
@@ -1,10 +1,15 @@
 package gg.projecteden.nexus.features.resourcepack.customblocks.models.noteblocks.neon;
 
+import com.mojang.datafixers.util.Pair;
+import gg.projecteden.nexus.features.recipes.models.builders.RecipeBuilder;
+import gg.projecteden.nexus.features.resourcepack.customblocks.models.noteblocks.common.ICraftableNoteBlock;
 import gg.projecteden.nexus.features.resourcepack.customblocks.models.noteblocks.common.ICustomNoteBlock;
 import gg.projecteden.nexus.features.resourcepack.customblocks.models.noteblocks.common.ILightableNoteBlock;
+import gg.projecteden.nexus.utils.MaterialTag;
 import org.bukkit.Material;
+import org.jetbrains.annotations.Nullable;
 
-public interface INeonBlock extends ICustomNoteBlock, ILightableNoteBlock {
+public interface INeonBlock extends ILightableNoteBlock, ICraftableNoteBlock {
 
 	@Override
 	default double getBlockHardness() {
@@ -14,5 +19,10 @@ public interface INeonBlock extends ICustomNoteBlock, ILightableNoteBlock {
 	@Override
 	default Material getMinimumPreferredTool() {
 		return Material.WOODEN_PICKAXE;
+	}
+
+	@Override
+	default @Nullable Pair<RecipeBuilder<?>, Integer> getCraftRecipe() {
+		return getSurroundRecipe(Material.TORCH, MaterialTag.CONCRETES);
 	}
 }
