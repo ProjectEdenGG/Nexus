4c490e58b4b9a33b094f049b2349e54e5cfa00d9
Wakka <wakkaflocka239@gmail.com>
2025-05-21 20:33:13 -0500

    kick if exploit

--- src/main/java/gg/projecteden/nexus/features/listeners/Restrictions.java
+++ src/main/java/gg/projecteden/nexus/features/listeners/Restrictions.java
@@ -2,6 +2,7 @@ package gg.projecteden.nexus.features.listeners;
 
 import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
 import gg.projecteden.api.common.utils.TimeUtils.TickTime;
+import gg.projecteden.api.common.utils.UUIDUtils;
 import gg.projecteden.api.interfaces.HasUniqueId;
 import gg.projecteden.nexus.features.chat.Censor;
 import gg.projecteden.nexus.features.chat.Chat.Broadcast;
@@ -12,10 +13,14 @@ import gg.projecteden.nexus.models.cooldown.CooldownService;
 import gg.projecteden.nexus.models.nerd.Nerd;
 import gg.projecteden.nexus.models.nerd.Rank;
 import gg.projecteden.nexus.models.nickname.Nickname;
+import gg.projecteden.nexus.models.punishments.Punishment;
+import gg.projecteden.nexus.models.punishments.PunishmentType;
+import gg.projecteden.nexus.models.punishments.Punishments;
 import gg.projecteden.nexus.utils.CitizensUtils;
 import gg.projecteden.nexus.utils.MaterialTag;
 import gg.projecteden.nexus.utils.Nullables;
 import gg.projecteden.nexus.utils.PlayerUtils;
+import gg.projecteden.nexus.utils.PlayerUtils.OnlinePlayers;
 import gg.projecteden.nexus.utils.StringUtils;
 import gg.projecteden.nexus.utils.Tasks;
 import gg.projecteden.nexus.utils.WorldGuardUtils;
@@ -97,10 +102,22 @@ public class Restrictions implements Listener {
 			return;
 
 		event.setCancelled(true);
+		String playerReason = "";
+
+		// get nearest player within 5 blocks who is in creative, does not include staff
+		List<Player> players = OnlinePlayers.where().world(entity.getWorld()).radius(entity.getLocation(), 5)
+			.filter(player -> !Rank.of(player).isStaff())
+			.filter(player -> player.getGameMode() == GameMode.CREATIVE)
+			.get();
+		if (players.size() == 1) {
+			Player player = players.getFirst();
+			Punishments.of(player).add(Punishment.ofType(PunishmentType.KICK).punisher(UUIDUtils.UUID0).input("Attempting to spawn mobs while in creative mode illegally"));
+			playerReason = "&c (Player: &e" + player.getName() + "&c)";
+		}
+
 		String entityType = StringUtils.camelCase(entity.getType());
 		String location = StringUtils.getLocationString(entity.getLocation());
-		String spawnReason = StringUtils.camelCase(event.getEntity().getEntitySpawnReason());
-		String message = "&cPrevented " + entityType + " spawning at " + location + "&c (Reason: &e" + spawnReason + "&c)";
+		String message = "&cPrevented " + entityType + " spawning at " + location + playerReason;
 
 		Broadcast.staffIngame().prefix("Restrictions").message(message).send();
 		Broadcast.staffDiscord().prefix("Restrictions").message(message).send();
