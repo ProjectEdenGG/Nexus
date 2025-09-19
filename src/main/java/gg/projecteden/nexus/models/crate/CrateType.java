3a2ddb25adcf86e74143f4c2f682343e07d251ee
GriffinCodes <griffin@projecteden.gg>
2025-09-18 21:43:19 -0500

    Add halloween crate type

--- src/main/java/gg/projecteden/nexus/models/crate/CrateType.java
+++ src/main/java/gg/projecteden/nexus/models/crate/CrateType.java
@@ -26,7 +26,8 @@ public enum CrateType {
 	MYSTERY(ItemModelType.CRATE_KEY_MYSTERY, "笯", true),
 	WEEKLY_WAKKA(ItemModelType.CRATE_KEY_WAKKA, "清",true),
 	MINIGAMES(ItemModelType.CRATE_KEY_MINIGAMES, "禘", true),
-	ONE_BLOCK(null, "皂")
+	ONE_BLOCK(null, "皂"),
+	HALLOWEEN(null, null)
 	;
 
 	final ItemModelType itemModelType;
