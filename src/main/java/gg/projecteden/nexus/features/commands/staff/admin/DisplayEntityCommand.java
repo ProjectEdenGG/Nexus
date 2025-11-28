464ccc5cf85b1e77d6864764bb1d3ab5d2b75328
GriffinCodes <griffin@projecteden.gg>
2025-11-28 12:55:12 -0600

    Add some display entity commands

--- src/main/java/gg/projecteden/nexus/features/commands/staff/admin/DisplayEntityCommand.java
+++ src/main/java/gg/projecteden/nexus/features/commands/staff/admin/DisplayEntityCommand.java
@@ -1,20 +1,26 @@
 package gg.projecteden.nexus.features.commands.staff.admin;
 
+import gg.projecteden.nexus.features.resourcepack.models.font.CustomFont;
 import gg.projecteden.nexus.framework.commands.models.CustomCommand;
+import gg.projecteden.nexus.framework.commands.models.annotations.Arg;
 import gg.projecteden.nexus.framework.commands.models.annotations.Description;
 import gg.projecteden.nexus.framework.commands.models.annotations.Path;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission;
 import gg.projecteden.nexus.framework.commands.models.annotations.Permission.Group;
+import gg.projecteden.nexus.framework.commands.models.annotations.Switch;
 import gg.projecteden.nexus.framework.commands.models.events.CommandEvent;
 import gg.projecteden.nexus.framework.exceptions.postconfigured.InvalidInputException;
 import gg.projecteden.nexus.utils.ColorType;
+import gg.projecteden.nexus.utils.JsonBuilder;
 import lombok.NonNull;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.BlockDisplay;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.TextDisplay;
+import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.NotNull;
+import org.joml.Vector3f;
 
 @Permission(Group.ADMIN)
 public class DisplayEntityCommand extends CustomCommand {
@@ -37,9 +43,34 @@ public class DisplayEntityCommand extends CustomCommand {
 	}
 
 	@Path("edit text background <color>")
-	@Description("Get the text background color on the nearest text display")
+	@Description("Set the text background color on the nearest text display")
 	void edit_text_background(ChatColor color) {
 		textDisplay().setBackgroundColor(ColorType.toBukkitColor(color));
+	}
+
+	@Path("edit text text <text...> [--font]")
+	@Description("Set the text background color on the nearest text display")
+	void edit_text_text(String text, @Switch @Arg("default") CustomFont font) {
+		textDisplay().text(new JsonBuilder(text).font(font).build());
+	}
+
+	@Path("get text scale")
+	@Description("Get the scale of the nearest text display")
+	void get_text_scale() {
+		Transformation transformation = textDisplay().getTransformation();
+		send(PREFIX + "Scale: " + transformation.getScale());
+	}
+
+	@Path("edit text scale <scale>")
+	@Description("Set the scale of the nearest text display")
+	void edit_text_scale(float scale) {
+		Transformation transformation = textDisplay().getTransformation();
+		textDisplay().setTransformation(new Transformation(
+			transformation.getTranslation(),
+			transformation.getLeftRotation(),
+			new Vector3f(scale, scale, scale),
+			transformation.getRightRotation()
+		));
 	}
 
 	@NotNull
