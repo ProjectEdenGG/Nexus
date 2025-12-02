4a5df265c18ae0bd2effc1dbb615858327375a3a
GriffinCodes <griffin@projecteden.gg>
2025-12-02 02:18:35 -0600

    Cache recipes

--- src/main/java/gg/projecteden/nexus/features/virtualinventories/VirtualInventoryUtils.java
+++ src/main/java/gg/projecteden/nexus/features/virtualinventories/VirtualInventoryUtils.java
@@ -2,6 +2,7 @@ package gg.projecteden.nexus.features.virtualinventories;
 
 import gg.projecteden.nexus.features.menus.api.TemporaryMenuListener.CustomInventoryHolder;
 import gg.projecteden.nexus.features.virtualinventories.models.inventories.VirtualInventory;
+import gg.projecteden.nexus.utils.ItemUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.bukkit.Bukkit;
@@ -9,32 +10,43 @@ import org.bukkit.inventory.CookingRecipe;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class VirtualInventoryUtils {
+
+	private static final Map<Class<? extends CookingRecipe<?>>, Map<ItemStack, CookingRecipe<?>>> CACHE = new HashMap<>();
+
+	public static CookingRecipe<?> getCookingRecipes(ItemStack ingredient, Class<? extends CookingRecipe<?>> recipeType) {
+		var cache = CACHE.computeIfAbsent(recipeType, $ -> new HashMap<>());
+		for (Entry<ItemStack, CookingRecipe<?>> entry : cache.entrySet())
+			if (ItemUtils.isFuzzyMatch(entry.getKey(), ingredient))
+				return entry.getValue();
+
+		return cache.computeIfAbsent(ingredient, $ -> {
+			Iterator<Recipe> iterator = Bukkit.recipeIterator();
+			while (iterator.hasNext()) {
+				Recipe recipe = iterator.next();
+				if (!(recipe instanceof CookingRecipe<?> cookingRecipe))
+					continue;
+
+				if (!recipeType.isAssignableFrom(recipe.getClass()))
+					continue;
+
+				if (!cookingRecipe.getInputChoice().test(ingredient))
+					continue;
+
+				return cookingRecipe;
+			}
+			return null;
+		});
+	}
 
 	@Data
 	@AllArgsConstructor
 	public static class VirtualInventoryHolder extends CustomInventoryHolder {
 		private final VirtualInventory<?> virtualInventory;
-	}
-
-	public static CookingRecipe<?> getCookingRecipes(ItemStack ingredient, Class<? extends CookingRecipe<?>> recipeType) {
-		Iterator<Recipe> iterator = Bukkit.recipeIterator();
-		while (iterator.hasNext()) {
-			Recipe recipe = iterator.next();
-			if (!(recipe instanceof CookingRecipe<?> cookingRecipe))
-				continue;
-
-			if (!recipeType.isAssignableFrom(recipe.getClass()))
-				continue;
-
-			if (!cookingRecipe.getInputChoice().test(ingredient))
-				continue;
-
-			return cookingRecipe;
-		}
-
-		return null;
 	}
 }
--- src/main/java/gg/projecteden/nexus/features/virtualinventories/managers/VirtualPersonalInventoryManager.java
+++ src/main/java/gg/projecteden/nexus/features/virtualinventories/managers/VirtualPersonalInventoryManager.java
@@ -2,11 +2,13 @@ package gg.projecteden.nexus.features.virtualinventories.managers;
 
 import gg.projecteden.api.common.utils.TimeUtils.TickTime;
 import gg.projecteden.nexus.Nexus;
+import gg.projecteden.nexus.features.recipes.CustomRecipes;
 import gg.projecteden.nexus.features.virtualinventories.events.VirtualInventoryConstructEvent;
 import gg.projecteden.nexus.features.virtualinventories.models.inventories.TickableVirtualInventory;
 import gg.projecteden.nexus.features.virtualinventories.models.inventories.VirtualInventory;
 import gg.projecteden.nexus.features.virtualinventories.models.inventories.VirtualInventoryType;
 import gg.projecteden.nexus.features.virtualinventories.models.inventories.VirtualPersonalInventory;
+import gg.projecteden.nexus.framework.features.Depends;
 import gg.projecteden.nexus.framework.features.Feature;
 import gg.projecteden.nexus.models.nickname.Nickname;
 import gg.projecteden.nexus.models.virtualinventories.VirtualInventoriesConfigService;
@@ -21,8 +23,9 @@ import java.util.List;
 /* TODO
 	- handle breaking blocks
 */
+@Depends(CustomRecipes.class)
 public class VirtualPersonalInventoryManager extends Feature {
-	private static int taskId;
+	private static int TASK_ID;
 
 	public static <T extends VirtualInventory<?>> T getOrCreate(Location location, Player player, VirtualInventoryType type) {
 		var inv = new VirtualInventoriesConfigService().get0().getPersonalInventories()
@@ -45,7 +48,7 @@ public class VirtualPersonalInventoryManager extends Feature {
 
 		service.get0().initializeLocations();
 
-		taskId = Tasks.repeat(TickTime.TICK.x(5), TickTime.TICK, () -> {
+		TASK_ID = Tasks.repeat(TickTime.TICK.x(5), TickTime.TICK, () -> {
 			if (!VirtualInventoryManager.isTicking())
 				return;
 
