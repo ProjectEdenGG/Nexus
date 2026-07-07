47129306941a667290963526e7a98592bcbc41e2
GriffinCodes <griffin@projecteden.gg>
2026-07-06 23:45:54 -0500

    Manually resend recipes after finishing

--- src/main/java/gg/projecteden/nexus/features/recipes/CustomRecipes.java
+++ src/main/java/gg/projecteden/nexus/features/recipes/CustomRecipes.java
@@ -24,7 +24,6 @@ import gg.projecteden.nexus.framework.features.Feature;
 import gg.projecteden.nexus.utils.ColorType;
 import gg.projecteden.nexus.utils.CopperState;
 import gg.projecteden.nexus.utils.CopperState.CopperBlockType;
-import gg.projecteden.nexus.utils.Debug;
 import gg.projecteden.nexus.utils.ItemBuilder;
 import gg.projecteden.nexus.utils.ItemBuilder.Model;
 import gg.projecteden.nexus.utils.ItemUtils;
@@ -36,6 +35,7 @@ import gg.projecteden.nexus.utils.Tasks;
 import gg.projecteden.nexus.utils.WoodType;
 import lombok.Getter;
 import lombok.NonNull;
+import net.minecraft.server.MinecraftServer;
 import org.bukkit.Bukkit;
 import org.bukkit.Keyed;
 import org.bukkit.Material;
@@ -70,8 +70,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
-
-import static gg.projecteden.nexus.utils.Debug.DebugType.RECIPES;
 
 @Depends({ResourcePack.class, CustomEnchants.class})
 public class CustomRecipes extends Feature implements Listener {
@@ -138,23 +136,24 @@ public class CustomRecipes extends Feature implements Listener {
 					}
 				});
 		});
-
 	}
+
+	private static int updateRecipesTaskId;
 
 	public static void register(NexusRecipe recipe) {
 		if (recipe == null)
 			return;
 
 		try {
-			for (Recipe recipe1 : Bukkit.getServer().getRecipesFor(recipe.getResult()))
-				if (RecipeUtils.areEqual(recipe.getRecipe(), recipe1)) {
-					Debug.log(RECIPES, recipe.getKey().getKey() + " == " + ((Keyed) recipe1).getKey().getKey());
-					return;
-				}
-
 			Tasks.sync(() -> {
 				try {
-					Bukkit.addRecipe(recipe.getRecipe());
+					Bukkit.addRecipe(recipe.getRecipe(), false);
+					Tasks.cancel(updateRecipesTaskId);
+					updateRecipesTaskId = Tasks.wait(10, () -> {
+						Nexus.log("[CustomRecipes] Sending recipe updates to players");
+						Bukkit.getServer().updateRecipes();
+						MinecraftServer.getServer().getPlayerList().reloadResources();
+					});
 				} catch (IllegalStateException duplicate) {
 					Nexus.log(duplicate.getMessage());
 				} catch (Exception ex) {
